86614ffe29ce10d42ee4b5209439b3d692ca3c63
Yehuda Katz <wycats@yehuda-katzs-macbookpro41.local>
2009-07-06 23:47:21 -0700

    Remove some dead code

--- lib/bundler.rb
+++ lib/bundler.rb
@@ -2,31 +2,6 @@ require "rubygems"
 require "rubygems/remote_fetcher"
 require "pp"
 
-# module Bundler
-#   class LazySourceIndex
-#     
-#     def initialize
-#       @cache = Hash.new
-#       @cache_two = Hash.new do |h,(gem_info,uri)|
-#         puts "FETCHING: #{gem_info}"
-#         h[[gem_info,uri]] = Gem::SpecFetcher.fetcher.fetch_spec(gem_info, URI.parse(uri))
-#       end
-#     end
-#     
-#     def search(gem_pattern, platform_only = false)
-#       start = Time.now
-#       retval = @cache[gem_pattern.hash] ||= begin
-#         Gem::SpecFetcher.fetcher.find_matching(gem_pattern, true, true).map do |result|
-#           @cache_two[result]
-#         end
-#       end
-#       $time += Time.now - start
-#       retval
-#     end
-#     
-#   end
-# end
-
 class FasterSourceIndex
   def initialize(index)
     @index = index
@@ -62,10 +37,6 @@ list = {
   "merb-core" => ">= 0",
   "merb-haml" => ">= 0",
   "merb_datamapper" => ">= 0"
-
-  # "merb-core" => "1.0.12",
-  # "merb-haml" => "1.0.12",
-  # "merb_datamapper" => "1.0.12"
 }.map {|k,v| Gem::Dependency.new(k, v)}
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "gem_resolver", "lib", "gem_resolver"))
@@ -73,36 +44,9 @@ pp GemResolver.resolve(list, index).all_specs.map {|x| x.full_name }
 
 puts "TOTAL: #{Time.now - t}"
 
-# index = Bundler::LazySourceIndex.new
-# pp index.search(Gem::Dependency.new("merb-core", "> 0"))
-
-# time = Time.now
 # deflated = Gem::RemoteFetcher.fetcher.fetch_path("#{Gem.sources.first}/Marshal.4.8.Z"); nil
 # inflated = Gem.inflate deflated; nil
 # index    = Marshal.load(inflated); nil
 # File.open("dumped", "w") do |f|
 #   f.puts inflated
 # end
-# puts "FINISHED INFLATING: #{Time.now - time}s"
-
-
-
-
-# require "ruby-prof"
-# 
-# RubyProf.start
-# 
-# resolved = GemResolver.resolve([Gem::Dependency.new("rails", "> 0")], index)
-# 
-# result = RubyProf.stop
-# 
-# printer = RubyProf::GraphPrinter.new(result)
-# printer.print
-# 
-# pp resolved.all_specs.map {|x| [x.name, x.version]}
-
-# Gem::SpecFetcher.fetcher.find_matching(dependency) returns
-#   [[["merb-core", #<Gem::Version "1.0.12">, "ruby"], "http://gems.rubyforge.org/"]]
-#
-# Gem::SpecFetcher.fetcher.fetch_spec(["merb-core", Gem::Version.new("0.9.2"), "ruby"], URI.parse("http://gems.rubyforge.org/"))
-#   Gem::Specification
