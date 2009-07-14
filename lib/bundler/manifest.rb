7aded78696e003d3bdd8523bf44cadc7d81dc7c4
Yehuda Katz + Carl Lerche <ykatz+clerche@engineyard.com>
2009-07-14 14:16:43 -0700

    Got a rough first version of the Manifest object working

--- lib/bundler/manifest.rb
+++ lib/bundler/manifest.rb
@@ -1,9 +1,45 @@
 module Bundler
+  class VersionConflict < StandardError; end
+
   class Manifest
     attr_reader :sources, :dependencies
-    
-    def initialize(sources, dependencies)
-      @sources, @dependencies = sources, dependencies
+
+    def initialize(sources, dependencies, path)
+      sources.map! {|s| s.is_a?(URI) ? s : URI.parse(s) }
+      @sources, @dependencies, @path = sources, dependencies, path
+    end
+
+    def fetch
+      return if all_gems_installed?
+
+      finder = Finder.new(*sources)
+      unless bundle = finder.resolve(*dependencies)
+        gems = @dependencies.map {|d| "  #{d.to_s}" }.join("\n")
+        raise VersionConflict, "No compatible versions could be found for:\n#{gems}"
+      end
+
+      bundle.download(@path)
+    end
+
+    def install
+      fetch
+      installer = Installer.new(@path)
+      installer.install  # options come here
+    end
+
+  private
+    def all_gems_installed?
+      gem_versions = {}
+
+      Dir[File.join(@path, "cache", "*.gem")].each do |file|
+        file =~ /\/([^\/]+)-([\d\.]+)\.gem$/
+        name, version = $1, $2
+        gem_versions[name] = Gem::Version.new(version)
+      end
+
+      ret = @dependencies.all? do |dep|
+        dep.version_requirements.satisfied_by?(gem_versions[dep.name])
+      end
     end
   end
 end
--- spec/bundler/manifest_spec.rb
+++ spec/bundler/manifest_spec.rb
@@ -6,19 +6,87 @@ describe "Bundler::Manifest" do
     Bundler::Dependency.new(name, options)
   end
 
-  it "has a list of sources and dependencies" do
-    sources = %w(http://gems.rubyforge.org)
-    deps = []
-    deps << dep("rails")
-    deps << dep("will_paginate")
-    
-    manifest = Bundler::Manifest.new(sources, deps)
-    manifest.sources.should == sources
-    manifest.dependencies.should == deps
+  before(:each) do
+    @sources = %W(file://#{gem_repo1} file://#{gem_repo2})
+    @deps = []
+    @deps << build_dep("rails", "2.3.2")
+    @deps << build_dep("rack", "0.9.1")
+
+    FileUtils.rm_rf(tmp_dir)
+    FileUtils.mkdir_p(tmp_dir)
+    @manifest = Bundler::Manifest.new(@sources, @deps, tmp_dir)
   end
 
+  it "has a list of sources and dependencies" do
+    @manifest.sources.should == @sources
+    @manifest.dependencies.should == @deps
+  end
+
+  def have_cached_gems(*gems)
+    simple_matcher("have cached gems") do |given, matcher|
+      gems.all? do |name|
+        matcher.failure_message = "Gem #{name} was not cached"
+        File.exists?(File.join(given, "cache", "#{name}.gem"))
+      end
+    end
+  end
+
+  alias have_cached_gem have_cached_gems
+
+  def have_installed_gems(*gems)
+    simple_matcher("have installed gems") do |given, matcher|
+      gems.all? do |name|
+        matcher.failure_message = "Gem #{name} was not installed"
+        File.exists?(File.join(given, "specifications", "#{name}.gemspec")) &&
+        File.directory?(File.join(given, "gems", "#{name}"))
+      end
+    end
+  end
+
+  alias have_installed_gem have_installed_gems
+
   it "bundles itself (running all of the steps)" do
-    pending
+    @manifest.install
+
+    gems = %w(rack-0.9.1 actionmailer-2.3.2
+      activerecord-2.3.2 activesupport-2.3.2
+      rake-0.8.7 actionpack-2.3.2
+      activeresource-2.3.2 rails-2.3.2)
+
+    tmp_dir.should have_cached_gems(*gems)
+    tmp_dir.should have_installed_gems(*gems)
+  end
+
+  it "skips fetching the source index if all gems are present" do
+    @manifest.install
+    Bundler::Finder.should_not_receive(:new)
+    @manifest.install
+  end
+
+  it "does the full fetching if a gem in the cache does not match the manifest" do
+    @manifest.install
+
+    deps = []
+    deps << build_dep("rails", "2.3.2")
+    deps << build_dep("rack", "1.0.0")
+
+    manifest = Bundler::Manifest.new(@sources,deps, tmp_dir)
+    manifest.install
+
+    gems = %w(rack-1.0.0 actionmailer-2.3.2
+      activerecord-2.3.2 activesupport-2.3.2
+      rake-0.8.7 actionpack-2.3.2
+      activeresource-2.3.2 rails-2.3.2)
+
+    tmp_dir.should have_cached_gems(*gems)
+    tmp_dir.should have_installed_gems(*gems)
+  end
+
+  it "raises a friendly exception if the manifest doesn't resolve" do
+    @manifest.dependencies << build_dep("active_support", "2.0")
+
+    lambda { @manifest.install }.should raise_error(Bundler::VersionConflict,
+      /rails \(= 2\.3\.2.*rack \(= 0\.9\.1.*active_support \(= 2\.0/m)
   end
 
 end
