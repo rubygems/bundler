03a575f493c89f0724dcbdf97fc922ac97573f01
Carl Lerche <carllerche@mac.com>
2010-01-13 12:09:27 -0800

    Add bbl init

--- lib/bubble/cli.rb
+++ lib/bubble/cli.rb
@@ -8,6 +8,16 @@ Gem.configuration
 
 module Bubble
   class CLI < Thor
+    desc "init", "Generates a Gemfile into the current working directory"
+    def init
+      if File.exist?("Gemfile")
+        puts "Gemfile already exists at `#{Dir.pwd}/Gemfile`"
+      else
+        puts "Writing new Gemfile to `#{Dir.pwd}/Gemfile`"
+        FileUtils.cp(File.expand_path('../templates/Gemfile', __FILE__), 'Gemfile')
+      end
+    end
+
     desc "install", "Install the current environment to the system"
     def install
       Installer.install(Bubble.definition)
---
+++ spec/other/init_spec.rb
@@ -0,0 +1,22 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe "bbl init" do
+  before :each do
+    in_app_root
+  end
+
+  it "generates a Gemfile" do
+    bbl :init
+    bundled_app("Gemfile").should exist
+  end
+
+  it "does not change existing Gemfiles" do
+    gemfile <<-G
+      gem "rails"
+    G
+
+    lambda {
+      bbl :init
+    }.should_not change { File.read(bundled_app("Gemfile")) }
+  end
+end
