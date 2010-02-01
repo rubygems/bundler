0df5a119235327046bb3fd2c36dbd5e9ff33ce1f
Carl Lerche <carllerche@mac.com>
2010-02-01 13:18:06 -0800

    Add a spec for a runtime path bug

--- spec/runtime/setup_spec.rb
+++ spec/runtime/setup_spec.rb
@@ -1,8 +1,23 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
 describe "Bundler.setup" do
+  describe "with paths" do
+    it "activates the gems in the path source" do
+      pending
+      system_gems "rack-1.0.0"
 
-  it "uses gems from pinned git sources even" do
-    pending
+      build_lib "rack", "1.0.0" do |s|
+        s.write "lib/rack.rb", "puts 'WIN'"
+      end
+
+      gemfile <<-G
+        path "#{lib_path('rack-1.0.0')}"
+        source "file://#{gem_repo1}"
+        gem "rack"
+      G
+
+      run "require 'rack'"
+      out.should == "WIN"
+    end
   end
 end
