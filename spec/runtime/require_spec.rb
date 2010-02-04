f50ccd9e12006731383a1ed6c791e6a371693529
Jeremy Kemper <jeremy@bitsweat.net>
2010-02-03 22:24:04 -0800

    Test :default group and multiple groups
    Signed-off-by: Carl Lerche <carllerche@mac.com>

--- spec/runtime/require_spec.rb
+++ spec/runtime/require_spec.rb
@@ -3,38 +3,41 @@ require File.expand_path('../../spec_helper', __FILE__)
 describe "Bundler.require" do
   before :each do
     build_lib "one", "1.0.0" do |s|
-      s.write "lib/baz.rb", "puts 'WIN'"
-      s.write "lib/qux.rb", "puts 'WIN'"
+      s.write "lib/baz.rb", "puts 'baz'"
+      s.write "lib/qux.rb", "puts 'qux'"
     end
 
     build_lib "two", "1.0.0" do |s|
-      s.write "lib/two.rb", "puts 'WIN'"
+      s.write "lib/two.rb", "puts 'two'"
     end
 
     build_lib "three", "1.0.0" do |s|
-      s.write "lib/three.rb", "puts 'LOSE'"
+      s.write "lib/three.rb", "puts 'three'"
     end
 
     gemfile <<-G
       path "#{lib_path('one-1.0.0')}"
       path "#{lib_path('two-1.0.0')}"
       path "#{lib_path('three-1.0.0')}"
-      gem "one", :group => "bar", :require => %w(baz qux)
-      gem "two", :group => "bar"
-      gem "three", :group => "not"
+      gem "one", :group => :bar, :require => %w(baz qux)
+      gem "two"
+      gem "three", :group => :not
     G
   end
 
   it "requires the gems" do
-    run "Bundler.require('bar')"
-    out.should == "WIN\nWIN\nWIN"
+    run "Bundler.require(:bar)"
+    out.should == "baz\nqux"
+
+    run "Bundler.require(:default, :bar)"
+    out.should == "two\nbaz\nqux"
   end
 
   it "requires the locked gems" do
     bundle :lock
 
     env = bundled_app(".bundle/environment.rb")
-    out = ruby("require '#{env}'; Bundler.setup('bar'); Bundler.require('bar')")
-    out.should == "WIN\nWIN\nWIN"
+    out = ruby("require '#{env}'; Bundler.setup(:default, :bar); Bundler.require(:default, :bar)")
+    out.should == "two\nbaz\nqux"
   end
 end
