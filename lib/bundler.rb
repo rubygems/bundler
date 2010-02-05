27e509d2509e3b9df7aec77e5eaeb32093ebbec1
Carl Lerche <carllerche@mac.com>
2010-02-05 14:26:22 -0800

    Bump the version

--- lib/bundler.rb
+++ lib/bundler.rb
@@ -4,7 +4,7 @@ require 'yaml'
 require 'bundler/rubygems'
 
 module Bundler
-  VERSION = "0.9.3.pre"
+  VERSION = "0.9.3"
 
   autoload :Definition,          'bundler/definition'
   autoload :Dependency,          'bundler/dependency'
