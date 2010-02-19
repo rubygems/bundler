4aeb903eb4d375984ea23eb1dbc3033ad6909a7c
Andre Arko <andre@arko.net>
2010-02-19 00:19:55 -0800

    Bump to 0.9.8

--- lib/bundler.rb
+++ lib/bundler.rb
@@ -4,7 +4,7 @@ require 'yaml'
 require 'bundler/rubygems_ext'
 
 module Bundler
-  VERSION = "0.9.7"
+  VERSION = "0.9.8"
 
   autoload :Definition,          'bundler/definition'
   autoload :Dependency,          'bundler/dependency'
