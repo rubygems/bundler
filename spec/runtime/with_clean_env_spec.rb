33154fd6865d5006a91381c92b7b83ba1c3b59cd
Joshua Hull <joshbuddy@gmail.com>
2010-03-26 16:32:51 -0400

    Add Bundler.with_clean_env for blocks of ruby without a bundled env
    e.g. if you are in a bundled Rakefile but need to shell out to
    something on your system that uses ruby like Homebrew
    
    Fixes #225

---
+++ spec/runtime/with_clean_env_spec.rb
@@ -0,0 +1,15 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe "Bundler.with_clean_env" do
+
+  it "should reset and restore the environment" do
+    gem_path = ENV['GEM_PATH']
+
+    Bundler.with_clean_env do
+      `echo $GEM_PATH`.strip.should_not == gem_path
+    end
+
+    ENV['GEM_PATH'].should == gem_path
+  end
+
+end
