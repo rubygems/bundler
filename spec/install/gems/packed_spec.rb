2c96fa737f86bf1970d2abcbda7f105dd2849f28
Andre Arko <andre@arko.net>
2010-04-19 18:43:35 -0700

    Ignore cached gems with the wrong platform
    Closes #288

--- spec/install/gems/packed_spec.rb
+++ spec/install/gems/packed_spec.rb
@@ -9,45 +9,64 @@ describe "bundle install with gem sources" do
         gem "rack"
       G
 
-      bundle :lock
-      bundle :cache
-
-      system_gems []
+      bundle :pack
+      simulate_new_machine
       FileUtils.rm_rf gem_repo2
 
       bundle :install
       should_be_installed "rack 1.0.0"
     end
 
-    it "does not constantly reinstall the gems" do
+    it "does not reinstall already-installed gems" do
       install_gemfile <<-G
         source "file://#{gem_repo1}"
         gem "rack"
       G
-
-      bundle "pack"
+      bundle :pack
 
       build_gem "rack", "1.0.0", :path => bundled_app('vendor/cache') do |s|
         s.write "lib/rack.rb", "raise 'omg'"
       end
 
-      bundle "install"
-
+      bundle :install
       err.should be_empty
       should_be_installed "rack 1.0"
     end
+  end
 
-    it "updates the cache if it exists" do
+  describe "when cached" do
+    it "ignores cached gems for the wrong platform" do
+      install_gemfile <<-G
+        Gem.platforms = [#{java}]
+        source "file://#{gem_repo1}"
+        gem "platform_specific"
+      G
+      bundle :cache
+      simulate_new_machine
+
+      install_gemfile <<-G
+        Gem.platforms = [#{rb}]
+        source "file://#{gem_repo1}"
+        gem "platform_specific"
+      G
+      bundle :install
+      run "require 'platform_specific' ; puts PLATFORM_SPECIFIC"
+      out.should == "1.0.0 RUBY"
+    end
+
+    it "updates the cache during later installs" do
+      cached_gem = bundled_app("vendor/cache/rack-1.0.0.gem")
       install_gemfile <<-G
         source "file://#{gem_repo1}"
         gem "rack"
       G
 
-      cached_gem = bundled_app("vendor/cache/rack-1.0.0.gem")
-      bundle "pack"
-      FileUtils.rm_rf(cached_gem)
-      bundle "install"
+      bundle :cache
+      cached_gem.should exist
 
+      FileUtils.rm_rf(cached_gem)
+
+      bundle :install
       out.should include("Copying .gem files into vendor/cache")
       cached_gem.should exist
     end
