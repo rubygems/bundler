3e1563427a9d033e07a948da24b2c365d9ed8618
Andre Arko <andre@arko.net>
2010-04-22 11:32:41 -0700

    Fix exec to work with GEM_LOADED

--- lib/bundler/setup.rb
+++ lib/bundler/setup.rb
@@ -6,7 +6,7 @@ if Bundler::SharedHelpers.in_bundle?
   env_file = Bundler::SharedHelpers.env_file
   if env_file.exist?
     require env_file
-    Bundler.setup if Bundler::GEM_LOADED
+    Bundler.setup if defined?(Bundler::GEM_LOADED)
   else
     require 'bundler'
     begin
