a6097c588b4e068d6968132b20d30fda50536b5c
Carl Lerche <carllerche@mac.com>
2010-04-26 12:36:34 -0700

    Removing a dead file

--- lib/bundler/flex.rb
+++
@@ -1,9 +0,0 @@
-module Bundler
-  module Flex
-    autoload :Definition,     'bundler/flex/definition'
-    autoload :Environment,    'bundler/flex/environments'
-    autoload :Installer,      'bundler/flex/environments'
-    autoload :LockfileParser, 'bundler/flex/lockfile_parser'
-    autoload :Runtime,        'bundler/flex/environments'
-  end
-end
