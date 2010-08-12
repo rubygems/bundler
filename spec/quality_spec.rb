947c1f5d80416c7a057cbdb24f44bc055f73ab13
Carl Lerche <carllerche@mac.com>
2010-08-11 19:10:11 -0700

    Let's write a spec that ensures that gem build bundler.gemspec still works.

--- spec/quality_spec.rb
+++ spec/quality_spec.rb
@@ -1,3 +1,5 @@
+require "spec_helper"
+
 describe "The library itself" do
   def check_for_tab_characters(filename)
     failing_lines = []
@@ -42,5 +44,12 @@ describe "The library itself" do
       end
     end
     error_messages.compact.should be_well_formed
+  end
+
+  it "can still be built" do
+    Dir.chdir(root) do
+      `gem build bundler.gemspec`
+      $?.should == 0
+    end
   end
 end
