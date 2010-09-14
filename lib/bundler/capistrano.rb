f37ca576f49a2724761b57c89e65881d3438c706
dbloete <mail@dennisbloete.de>
2010-09-14 22:44:38 +0200

    Further refactoring of the deployment recipes

--- lib/bundler/capistrano.rb
+++ lib/bundler/capistrano.rb
@@ -6,25 +6,6 @@ require 'bundler/deployment'
 
 Capistrano::Configuration.instance(:must_exist).load do
   after "deploy:update_code", "bundle:install"
-
-  namespace :bundle do
-    desc <<-DESC
-      Install the current Bundler environment. By default, gems will be \
-      installed to the shared/bundle path. Gems in the development and \
-      test group will not be installed. The install command is executed \
-      with the --deployment and --quiet flags. You can override any of \
-      these defaults by setting the variables shown below. If capistrano \
-      can not find the 'bundle' cmd then you can override the bundle_cmd \
-      variable to specifiy which one it should use.
-
-        set :bundle_gemfile,      "Gemfile"
-        set :bundle_dir,          File.join(fetch(:shared_path), 'bundle')
-        set :bundle_flags,       "--deployment --quiet"
-        set :bundle_without,      [:development, :test]
-        set :bundle_cmd,          "bundle" # e.g. change to "/opt/ruby/bin/bundle"
-    DESC
-    task :install, :except => { :no_release => true } do
-      Bundler::Deployment.install_bundle(self)
-    end
-  end
+  
+  Bundler::Deployment.define_task(self, :task, :except => { :no_release => true })
 end
--- lib/bundler/vlad.rb
+++ lib/bundler/vlad.rb
@@ -5,24 +5,5 @@
 require 'bundler/deployment'
 
 namespace :vlad do
-  namespace :bundle do
-    desc <<-DESC
-      Install the current Bundler environment. By default, gems will be \
-      installed to the shared/bundle path. Gems in the development and \
-      test group will not be installed. The install command is executed \
-      with the --deployment and --quiet flags. You can override any of \
-      these defaults by setting the variables shown below. If Vlad \
-      can not find the 'bundle' cmd then you can override the bundle_cmd \
-      variable to specifiy which one it should use.
-
-        set :bundle_gemfile,      "Gemfile"
-        set :bundle_dir,          File.join(fetch(:shared_path), 'bundle')
-        set :bundle_flags,        "--deployment --quiet"
-        set :bundle_without,      [:development, :test]
-        set :bundle_cmd,          "bundle" # e.g. change to "/opt/ruby/bin/bundle"
-    DESC
-    remote_task :install, :roles => :app do
-      Bundler::Deployment.install_bundle(Rake::RemoteTask)
-    end
-  end
+  Bundler::Deployment.define_task(Rake::RemoteTask, :remote_task, :roles => :app)
 end
