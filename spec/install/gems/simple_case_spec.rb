1c312f62721a6c1fd97905577af411ed6389c203
Terence Lee <hone02@gmail.com>
2010-10-08 21:51:02 -0500

    bundle lock is gone, move tests to simple_case_spec

--- spec/install/gems/simple_case_spec.rb
+++ spec/install/gems/simple_case_spec.rb
@@ -706,4 +706,36 @@ describe "bundle install with gem sources" do
     end
   end
 
+  describe "when locked and installed with --without" do
+    before(:each) do
+      build_repo2
+      system_gems "rack-0.9.1" do
+        install_gemfile <<-G, :without => :rack
+          source "file://#{gem_repo2}"
+          gem "rack"
+
+          group :rack do
+            gem "rack_middleware"
+          end
+        G
+      end
+    end
+
+    it "uses the correct versions even if --without was used on the original" do
+      should_be_installed "rack 0.9.1"
+      should_not_be_installed "rack_middleware 1.0"
+      simulate_new_machine
+
+      bundle :install
+
+      should_be_installed "rack 0.9.1"
+      should_be_installed "rack_middleware 1.0"
+    end
+
+    it "does not hit the remote a second time" do
+      FileUtils.rm_rf gem_repo2
+      bundle "install --without rack"
+      err.should be_empty
+    end
+  end
 end
