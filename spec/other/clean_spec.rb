9cb7c624caf8b3f97269d3cb9b8f2925df969104
Terence Lee <hone02@gmail.com>
2010-10-19 09:55:52 -0500

    bundle clean spec for without groups

--- spec/other/clean_spec.rb
+++ spec/other/clean_spec.rb
@@ -93,7 +93,29 @@ describe "bundle clean" do
     vendored_gems("specifications/rack-1.0.0.gemspec").should_not exist
   end
 
-  it "remove gems in bundle without groups"
+  it "remove gems in bundle without groups" do
+    install_gemfile <<-G
+      source "file://#{gem_repo1}"
+
+      gem "foo"
+
+      group :test_group do
+        gem "rack", "1.0.0"
+      end
+    G
+
+    bundle "install --path vendor"
+    bundle "install --without test_group"
+    bundle :clean
+
+    out.should == "Removing rack (1.0.0)"
+
+    vendored_gems("gems/foo-1.0").should exist
+    vendored_gems("gems/rack-1.0.0").should_not exist
+
+    vendored_gems("specifications/foo-1.0.gemspec").should exist
+    vendored_gems("specifications/rack-1.0.0.gemspec").should_not exist
+  end
 
   it "removes out of date git gems"
 
