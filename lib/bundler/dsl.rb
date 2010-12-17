03378109d5b589b635429f7dbb63016156182b62
wycats <wycats@gmail.com>
2010-12-17 10:42:31 -0800

    Make it possible to override a .gemspec dependency's source in the Gemfile

--- lib/bundler/dsl.rb
+++ lib/bundler/dsl.rb
@@ -32,9 +32,6 @@ module Bundler
         spec = Gem::Specification.load(gemspecs.first)
         raise InvalidOption, "There was an error loading the gemspec at #{gemspecs.first}." unless spec
         gem spec.name, :path => path
-        spec.runtime_dependencies.each do |dep|
-          gem dep.name, *dep.requirement.as_list
-        end
         group(development_group) do
           spec.development_dependencies.each do |dep|
             gem dep.name, *dep.requirement.as_list
