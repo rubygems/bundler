2bf0911d3eeed1cd255008379245d0d2b5ea2025
Sam Umbach <sam@thinkrelevance.com>
2011-01-16 01:02:45 -0500

    Use #load_gemspec to evaluate gemspec files specified in Gemfile
      - #load_gemspec already wraps the eval in chdir to resolve issue #947
      - also loads gemspec files in YAML format
      - also provides better error messages than Gem::Specification
    
    Closes #947

--- lib/bundler/dsl.rb
+++ lib/bundler/dsl.rb
@@ -29,7 +29,7 @@ module Bundler
 
       case gemspecs.size
       when 1
-        spec = Gem::Specification.load(gemspecs.first)
+        spec = Bundler.load_gemspec(gemspecs.first)
         raise InvalidOption, "There was an error loading the gemspec at #{gemspecs.first}." unless spec
         gem spec.name, :path => path
         group(development_group) do
