36f1594dac2a5705fc5b7aa6f9293ac5ba9c60ee
Andre Arko <andre@arko.net>
2011-01-18 16:40:37 -0800

    add test for bundle install --no-cache, fix --no-cache option

--- spec/install/gems/packed_spec.rb
+++ spec/install/gems/packed_spec.rb
@@ -68,5 +68,17 @@ describe "bundle install with gem sources" do
         out.should == "1.0.0 RUBY"
       end
     end
+
+    fit "does not update the cache if --no-cache is passed" do
+      gemfile <<-G
+        source "file://#{gem_repo1}"
+        gem "rack"
+      G
+      bundled_app("vendor/cache").mkpath
+      bundled_app("vendor/cache").children.should be_empty
+
+      bundle "install --no-cache"
+      bundled_app("vendor/cache").children.should be_empty
+    end
   end
 end
