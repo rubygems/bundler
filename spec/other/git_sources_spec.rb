cf47f4e13108af9932f343c687f60de77a5fd6da
Hedgehog <hedgehogshiatus@gmail.com>
2011-02-09 10:04:05 +1100

    [spec] reorganize git sources uri specs
    Signed-off-by: Hedgehog <hedgehogshiatus@gmail.com>

---
+++ spec/other/git_sources_spec.rb
@@ -0,0 +1,58 @@
+require "spec_helper"
+
+describe "bundle with git sources" do
+  it "base_name should parse scp style URI syntax" do
+    source  = Bundler::Source::Git.new("uri" => "git@github.com:repo.git")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with SSH URI syntax" do
+    source  = Bundler::Source::Git.new("uri" => "ssh://user@host.xz:port/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with Git URI syntax" do
+    source  = Bundler::Source::Git.new("uri" => "git://host.xz:port/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with HTTP URI syntax" do
+    source  = Bundler::Source::Git.new("uri" => "http://host.xz:port/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with HTTPS URI syntax" do
+    source  = Bundler::Source::Git.new("uri" => "https://host.xz:port/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with FTP URI syntax" do
+    source  = Bundler::Source::Git.new("uri" => "ftp://host.xz:port/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with FTPS URI syntax" do
+    source  = Bundler::Source::Git.new("uri" => "ftps://host.xz:port/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with RSYNC URI syntax" do
+    source  = Bundler::Source::Git.new("uri" => "rsync://host.xz/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with SSH URI syntax with username expansion" do
+    source  = Bundler::Source::Git.new("uri" => "ssh://user@host.xz:port/~user/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with Git URI syntax with username expansion" do
+    source  = Bundler::Source::Git.new("uri" => "git://host.xz:port/~user/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with SCP URI syntax with username expansion" do
+    source  = Bundler::Source::Git.new("uri" => "user@host.xz:/~user/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with File URI syntax with hostname" do
+    source  = Bundler::Source::Git.new("uri" => "file://host.name.org/path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+  it "base_name should parse a Git repository location described with File URI syntax without hostname" do
+    source  = Bundler::Source::Git.new("uri" => "file:///path/to/repo.git/")
+    source.send(:base_name).should == "repo"
+  end
+end
+
+
