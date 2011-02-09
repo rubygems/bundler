052d630984112ff632c6f02c19b463cd76d0236b
Hedgehog <hedgehogshiatus@gmail.com>
2011-02-09 12:52:41 +1100

    [spec] specs for git gem cache
    Signed-off-by: Hedgehog <hedgehogshiatus@gmail.com>

--- spec/cache/git_spec.rb
+++ spec/cache/git_spec.rb
@@ -2,14 +2,38 @@ require "spec_helper"
 describe "bundle cache" do
   describe "with git sources" do
     it "is silent when the path is within the bundle" do
-      build_lib "foo", :path => bundled_app("lib/foo")
+      build_git "foo", :path => "file://#{lib_path('foo')}/.git"
 
       install_gemfile <<-G
-        gem "foo", :path => '#{bundled_app("lib/foo")}'
+        gem "foo", :git => "file://#{lib_path('foo')}/.git"
+      G
+      bundle "install"
+      bundle "cache"
+      out.should match /Updating .gem files in vendor\/cache/
+    end
+
+    it "locks the gemfile" do
+      build_git "foo", :path => "file://#{lib_path('foo')}/.git"
+
+      install_gemfile <<-G
+        gem "foo", :git => "file://#{lib_path('foo')}/.git"
       G
 
       bundle "cache"
-      out.should match /Updating .gem files in vendor\/cache/
+      bundled_app("Gemfile.lock").should exist
+    end
+
+    it "caches the gems" do
+      build_git "foo", :path => "file://#{lib_path('foo')}/.git"
+
+      install_gemfile <<-G
+        gem "foo", :git => "file://#{lib_path('foo')}/.git"
+      G
+
+      bundle "cache"
+      pending do
+        bundled_app("vendor/cache/foo-1.0.0.gem").should exist
+      end
     end
   end
 end
