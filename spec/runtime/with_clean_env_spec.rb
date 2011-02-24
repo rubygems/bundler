9bd7cfde53c01526593ff9126bb4563f7353743c
Jonathan Dance <jd@wuputah.com>
2011-02-23 22:29:11 -0600

    with_clean_env clears bundler env vars
    old behavior is at Bundler.with_original_env

--- spec/runtime/with_clean_env_spec.rb
+++ spec/runtime/with_clean_env_spec.rb
@@ -1,7 +1,6 @@
 require "spec_helper"
 
-describe "Bundler.with_clean_env" do
-
+shared_examples_for "Bundler.with_*_env" do
   it "should reset and restore the environment" do
     gem_path = ENV['GEM_PATH']
 
@@ -10,6 +9,39 @@ describe "Bundler.with_clean_env" do
     end
 
     ENV['GEM_PATH'].should == gem_path
+  end
+end
+
+describe "Bundler.with_clean_env" do
+
+  it_should_behave_like "Bundler.with_*_env"
+
+  it "should not pass any bundler environment variables" do
+    bundle_path = Bundler::ORIGINAL_ENV['BUNDLE_PATH']
+    Bundler::ORIGINAL_ENV['BUNDLE_PATH'] = "./Gemfile"
+
+    Bundler.with_clean_env do
+      `echo $BUNDLE_PATH`.strip.should_not == './Gemfile'
+    end
+
+    Bundler::ORIGINAL_ENV['BUNDLE_PATH'] = bundle_path
+  end
+
+end
+
+describe "Bundler.with_original_env" do
+
+  it_should_behave_like "Bundler.with_*_env"
+
+  it "should pass bundler environment variables set before Bundler was run" do
+    bundle_path = Bundler::ORIGINAL_ENV['BUNDLE_PATH']
+    Bundler::ORIGINAL_ENV['BUNDLE_PATH'] = "./Gemfile"
+
+    Bundler.with_original_env do
+      `echo $BUNDLE_PATH`.strip.should == './Gemfile'
+    end
+
+    Bundler::ORIGINAL_ENV['BUNDLE_PATH'] = bundle_path
   end
 
 end
