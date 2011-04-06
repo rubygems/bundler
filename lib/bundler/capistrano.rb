d3c48dcd422da94d7151dfd8531f7eae48aebe71
Alex Arnell <alex@bravenet.com>
2011-04-06 12:50:23 -0700

    should just use fetch directly

--- lib/bundler/capistrano.rb
+++ lib/bundler/capistrano.rb
@@ -7,6 +7,5 @@ require 'bundler/deployment'
 Capistrano::Configuration.instance(:must_exist).load do
   after "deploy:update_code", "bundle:install"
   Bundler::Deployment.define_task(self, :task, :except => { :no_release => true })
-  bundle_cmd     = context.fetch(:bundle_cmd, "bundle")
-  set :rake, "#{bundle_cmd} exec rake"
+  set :rake, "#{fetch(:bundle_cmd, "bundle")} exec rake"
 end
