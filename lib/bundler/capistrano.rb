c9c0d1757fc49077dea629954c0a3ab40f24a8ab
Ryan Briones <ryan.briones@brionesandco.com>
2011-04-21 10:25:45 -0500

    Use specified gemfile when running bundle exec'd rake

--- lib/bundler/capistrano.rb
+++ lib/bundler/capistrano.rb
@@ -7,5 +7,5 @@ require 'bundler/deployment'
 Capistrano::Configuration.instance(:must_exist).load do
   after "deploy:update_code", "bundle:install"
   Bundler::Deployment.define_task(self, :task, :except => { :no_release => true })
-  set :rake, lambda { "#{fetch(:bundle_cmd, "bundle")} exec rake" }
+  set :rake, lambda { "#{fetch(:bundle_cmd, "bundle")} --gemfile #{File.join(context.fetch(:current_release), bundle_gemfile)} exec rake" }
 end
