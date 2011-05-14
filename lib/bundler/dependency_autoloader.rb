c2fd025fbb21e84f54a35e2c28d39d155826c3e6
Ian MacLeod <ian@nevir.net>
2011-05-14 11:05:34 -0700

    Refactor the DependencyAutoloader to be a bit more readable

--- lib/bundler/dependency_autoloader.rb
+++ lib/bundler/dependency_autoloader.rb
@@ -18,30 +18,38 @@ module Bundler
         inject_hooks!
       end
       
+      # Handles the loading of a given symbol's dependencies
+      def load_symbol(symbol)
+        if registered_symbols.include? symbol
+          registered_symbols[symbol].each do |runtime, dependency|
+            runtime.require_dependency(dependency)
+          end
+          
+          # If we made it this far, the require was successful.  Now we need to return the
+          # constant to fulfill our contract.
+          return Object.const_get(symbol) if Object.const_defined?(symbol)
+        end
+        
+        return nil
+      end
+      
       # Unfortunately, we can't take advantage of Kernel::autoload, as it only supports loading a
       # single file per symbol.  Gotta build our own, which makes this a little fragile.
       # 
-      # We only support top-level constants, as we can't predict whether an intermediary constant
-      # will be a class/module, and what it might inherit from.
+      # We only support top-level constants to avoid conflicts with other modules/classes that
+      # define their own const_missing hooks
       def inject_hooks!
         return if @injected_hooks
         
         ::Object.class_eval do
           class << self
+            
             orig_const_missing = instance_method :const_missing
             define_method(:const_missing) do |sym|
-              if ::Bundler::DependencyAutoloader.registered_symbols.include? sym
-                ::Bundler::DependencyAutoloader.registered_symbols[sym].each do |runtime, dependency|
-                  runtime.require_dependency(dependency)
-                end
-                
-                # If we made it this far, the require was successful.  Now we need to return the
-                # constant to fulfill our contract.
-                return Object.const_get(sym) if Object.const_defined?(sym)
-              end
-              
-              orig_const_missing.bind(self).call(sym)
+              loaded = ::Bundler::DependencyAutoloader.load_symbol(sym)
+              return loaded || orig_const_missing.bind(self).call(sym)
             end
+            
           end
         end
         
