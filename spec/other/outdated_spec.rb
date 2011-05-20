029bee1fe8b1303543bf9c2bf81c35bba5ba19e0
Joel Moss <joel@developwithstyle.com>
2011-05-20 15:13:35 +0100

    Ignoring pre-release gems in outdated command, and added --all option to included pre-releases

--- spec/other/outdated_spec.rb
+++ spec/other/outdated_spec.rb
@@ -20,32 +20,56 @@ describe "bundle outdated" do
         build_gem "activesupport", "3.0"
         update_git "foo", :path => lib_path("foo")
       end
-
+  
       bundle "outdated"
       out.should include("activesupport (3.0 > 2.3.5)")
       out.should include("foo (1.0")
     end
   end
-
+  
   describe "with --local option" do
     it "doesn't hit repo2" do
       FileUtils.rm_rf(gem_repo2)
-
+  
       bundle "outdated --local"
       out.should_not match(/Fetching source index/)
     end
   end
-
+  
   describe "with specified gems" do
     it "returns list of outdated gems" do
       update_repo2 do
         build_gem "activesupport", "3.0"
         update_git "foo", :path => lib_path("foo")
       end
-
+  
       bundle "outdated foo"
       out.should_not include("activesupport (3.0 > 2.3.5)")
       out.should include("foo (1.0")
+    end
+  end
+  
+  describe "pre-release gems" do
+    context "without the --all option" do
+      it "ignores pre-release versions" do
+        update_repo2 do
+          build_gem "activesupport", "3.0.0.beta"
+        end
+
+        bundle "outdated"
+        out.should_not include("activesupport (3.0.0.beta > 2.3.5)")
+      end
+    end
+  
+    describe "with the --all option" do
+      it "includes pre-release versions" do
+        update_repo2 do
+          build_gem "activesupport", "3.0.0.beta"
+        end
+
+        bundle "outdated --all"
+        out.should include("activesupport (3.0.0.beta > 2.3.5)")
+      end
     end
   end
 
