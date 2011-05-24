509c0ed1301b01edfa805245864bbdb497a02cd6
Andre Arko <andre@arko.net>
2011-05-24 08:20:31 -0700

    Change outdated --all to --pre for symmetry with rubygems

--- lib/bundler/cli.rb
+++ lib/bundler/cli.rb
@@ -279,12 +279,13 @@ module Bundler
     end
     map %w(list) => "show"
 
-    desc "outdated", "Returns a list of installed gems that are outdated."
+    desc "outdated [GEM]", "list installed gems with newer versions available"
     long_desc <<-D
-      Outdated lists the names and versions of all gems that are outdated when compared to the source.
-      Calling outdated with [GEM [GEM]] will check only the given gems.
+      Outdated lists the names and versions of gems that have a newer version available
+      in the given source. Calling outdated with [GEM [GEM]] will only check for newer
+      versions of the given gems. By default, available prerelease gems will be ignored.
     D
-    method_option "all", :type => :boolean, :banner => "Check for and include pre-release gems"
+    method_option "pre", :type => :boolean, :banner => "Check for newer pre-release gems"
     method_option "source", :type => :array, :banner => "Check against a specific source"
     method_option "local", :type => :boolean, :banner =>
       "Do not attempt to fetch gems remotely and use the gem cache instead"
@@ -312,7 +313,7 @@ module Bundler
           current = spec
           spec = definition.index[current.name].sort_by{|b| b.version }
 
-          if !options[:all] && spec.size > 1
+          if !options[:pre] && spec.size > 1
             spec = spec.delete_if{|b| b.respond_to?(:version) && b.version.prerelease? }
           end
 
