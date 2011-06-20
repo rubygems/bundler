43fa713b0817106c10fdba3501da1943fc1d0ac4
Matthew Smith <matthew.smith2@extendhealth.com>
2011-06-20 09:27:54 -0600

    Added test for git uri as a network share.

--- spec/cache/git_spec.rb
+++ spec/cache/git_spec.rb
@@ -6,4 +6,11 @@ describe "bundle cache with git" do
   end
 end
 
+describe "bundle cache with network share git" do
+  it "base_name should be undefined if repo uri is a network share path" do
+    source = Bundler::Source::Git.new("uri" => "//MachineName/ShareFolder")
+	source.send(:base_name).should == "Undetermined"
+  end
+ end
+
 
