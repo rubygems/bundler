5dcb70b7430d5a911f20761d5986ecc36559f0fa
Chris Griego <cgriego@gmail.com>
2011-07-12 21:06:14 -0500

    Run the bundle install earlier in a Capistrano deployment to give other tasks a stable, common hook to use.
    closes #1300

--- lib/bundler/capistrano.rb
+++ lib/bundler/capistrano.rb
@@ -5,7 +5,7 @@
 require 'bundler/deployment'
 
 Capistrano::Configuration.instance(:must_exist).load do
-  after "deploy:update_code", "bundle:install"
+  after "deploy:finalize_update", "bundle:install"
   Bundler::Deployment.define_task(self, :task, :except => { :no_release => true })
   set :rake, lambda { "#{fetch(:bundle_cmd, "bundle")} exec rake" }
 end
