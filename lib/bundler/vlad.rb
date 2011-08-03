e45135efaafa08c95d7f847b20894d740718c927
Matteo Latini <mtylty@gmail.com>
2011-08-03 11:05:13 +0300

    fixes rake 0.9.2 WARNING
    closes #1333

--- lib/bundler/vlad.rb
+++ lib/bundler/vlad.rb
@@ -4,6 +4,8 @@
 # include the vlad:bundle:install task in your vlad:deploy task.
 require 'bundler/deployment'
 
+include Rake::DSL if defined? Rake::DSL
+
 namespace :vlad do
   Bundler::Deployment.define_task(Rake::RemoteTask, :remote_task, :roles => :app)
 end
