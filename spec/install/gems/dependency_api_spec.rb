32978b04098580ea58b98808abdb1ef3c2194423
Terence Lee <hone02@gmail.com>
2011-08-05 02:29:53 -0500

    test when hitting FORCE_MODERN_INDEX_LIMIT

--- spec/install/gems/dependency_api_spec.rb
+++ spec/install/gems/dependency_api_spec.rb
@@ -171,4 +171,28 @@ describe "gemcutter's dependency API" do
     bundle :install, :artifice => "endpoint_extra"
     should_be_installed "back_deps 1.0"
   end
+
+  it "does not fetch every specs if the index of gems is large when doing back deps" do
+    build_repo2 do
+      build_gem "back_deps" do |s|
+        s.add_dependency "foo"
+      end
+      build_gem "missing"
+      # need to hit the limit
+      1.upto(Bundler::Source::Rubygems::FORCE_MODERN_INDEX_LIMIT) do |i|
+        build_gem "gem#{i}"
+      end
+
+      FileUtils.rm_rf Dir[gem_repo2("gems/foo-*.gem")]
+    end
+
+    gemfile <<-G
+      source "#{source_uri}"
+      source "#{source_uri}/extra"
+      gem "back_deps"
+    G
+
+    bundle :install, :artifice => "endpoint_extra_missing"
+    should_be_installed "back_deps 1.0"
+  end
 end
