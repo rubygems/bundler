165dbfd58b8abe3057e0bdd2e8b434b34c240464
Andre Arko <andre@arko.net>
2011-10-02 19:25:36 -0700

    hardcode linecache version

--- spec/realworld/edgecases_spec.rb
+++ spec/realworld/edgecases_spec.rb
@@ -2,7 +2,7 @@ describe "real world edgecases", :realworld => true do
   it "ignores extra gems with bad platforms" do
     install_gemfile <<-G
       source :rubygems
-      gem "linecache"
+      gem "linecache", "0.46"
     G
     err.should eq("")
   end
