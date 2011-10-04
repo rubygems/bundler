f05eba7a3796859fb7e3fc42525fdc828b3e2c19
Terence Lee <hone02@gmail.com>
2011-10-03 20:00:35 -0500

    this test only applies to 1.8

--- spec/realworld/edgecases_spec.rb
+++ spec/realworld/edgecases_spec.rb
@@ -1,9 +1,12 @@
 describe "real world edgecases", :realworld => true do
-  it "ignores extra gems with bad platforms" do
-    install_gemfile <<-G
-      source :rubygems
-      gem "linecache", "0.46"
-    G
-    err.should eq("")
+  if RUBY_VERSION < "1.9"
+    # there is no rbx-relative-require gem that will install on 1.9
+    it "ignores extra gems with bad platforms" do
+      install_gemfile <<-G
+        source :rubygems
+        gem "linecache", "0.46"
+      G
+      err.should eq("")
+    end
   end
 end
