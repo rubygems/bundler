a86fbb03f31418fb5cf7eb2466844f8d8a34f812
Andre Arko <andre@arko.net>
2011-12-22 18:46:10 -0800

    make Gemfile errors more sensible

--- lib/bundler/dsl.rb
+++ lib/bundler/dsl.rb
@@ -6,6 +6,11 @@ module Bundler
       builder = new
       builder.instance_eval(Bundler.read_file(gemfile.to_s), gemfile.to_s, 1)
       builder.to_definition(lockfile, unlock)
+    rescue StandardError, ScriptError => e
+      e.backtrace[0] = "#{e.backtrace[0]}: #{e.message} (#{e.class})"
+      Bundler.ui.info e.backtrace.join("\n       ")
+      raise GemfileError, "There was an error in your Gemfile," \
+        " and Bundler cannot continue."
     end
 
     VALID_PLATFORMS = Bundler::Dependency::PLATFORM_MAP.keys.freeze
