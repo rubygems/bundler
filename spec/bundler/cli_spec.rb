0c897fe5235b36b0bf716ae2407dce429e2c2477
Tim Miller <tim.miller.0@gmail.com>
2012-05-01 19:55:46 -0700

    fix two benign typos.

--- spec/bundler/cli_spec.rb
+++ spec/bundler/cli_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe 'bundle executable' do
   it 'returns non-zero exit status when passed unrecognized options' do
-    bundle '--inalid_argument', :exitstatus => true
+    bundle '--invalid_argument', :exitstatus => true
     exitstatus.should_not == 0
   end
 end
