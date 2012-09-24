c03c85dbaf6d1211ece20cb7c2c25b79afde75d9
Marcus Oladell <mafcocinco@gmail.com>
2012-09-24 10:34:36 -0500

    removed files without .tt extension

--- lib/bundler/templates/newgem/spec/newgem_spec.rb
+++
@@ -1,9 +0,0 @@
-describe <%= conifg[:constant_name] %> do
-  it 'should have a version number' do
-    <%= config[:constant_name] %>::VERSION.should_not be_nil
-  end
-
-  it 'should do something useful' do
-    false.should be_true
-  end
-end
