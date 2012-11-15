7268495a5a4708519fcfcfbe48940096672ea6a0
Andre Arko <andre@arko.net>
2012-11-14 19:01:29 -0800

    refactor psyched_yaml

--- lib/bundler/psyched_yaml.rb
+++ lib/bundler/psyched_yaml.rb
@@ -1,24 +1,20 @@
-# Psych could be a gem
 begin
-  gem 'psych'
-rescue Gem::LoadError
-end if defined?(Gem)
+  # Psych could be a gem, so set up the load path
+  gem 'psych' if defined?(gem)
 
-# Psych could be a stdlib
-begin
-  # it's too late if Syck is already loaded
+  # Psych could just be in the stdlib
+  # but it's too late if Syck is already loaded
   require 'psych' unless defined?(Syck)
 rescue LoadError
+  # apparently Psych wasn't available. Oh well.
+ensure
+  # at least load the YAML stdlib, whatever that may be
+  require 'yaml'
 end
 
-# Psych might NOT EXIST AT ALL
-require 'yaml'
-
-# if the file is not valid YAML:
-# * Syck raises ArgumentError
-# * Psych raises Psych::SyntaxError
-begin
-  YamlSyntaxError = Psych::SyntaxError
-rescue NameError
-  YamlSyntaxError = ArgumentError
+module Bundler
+  # On encountering invalid YAML,
+  # Psych raises Psych::SyntaxError
+  # Syck raises ArgumentError
+  YamlSyntaxError = defined?(Psych) ? Psych::SyntaxError : ArgumentError
 end
--- spec/bundler/psyched_yaml_spec.rb
+++ spec/bundler/psyched_yaml_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-describe YamlSyntaxError do
+describe Bundler::YamlSyntaxError do
   it "is raised on YAML parse errors" do
-    expect{ YAML.parse "{foo" }.to raise_error(YamlSyntaxError)
+    expect{ YAML.parse "{foo" }.to raise_error(Bundler::YamlSyntaxError)
   end
 end
