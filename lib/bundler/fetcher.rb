c227a40c4839be8a13094b5533bf45d5a832a7a3
Andre Arko <andre@arko.net>
2012-11-14 18:04:57 -0800

    handle broken OpenSSL more generically

--- lib/bundler/fetcher.rb
+++ lib/bundler/fetcher.rb
@@ -107,16 +107,6 @@ module Bundler
       end
 
       index
-    rescue LoadError => e
-      if e.message.include?("cannot load such file -- openssl")
-        raise InstallError,
-          "\nCould not load OpenSSL." \
-          "\nYou must recompile Ruby with OpenSSL support or change the sources in your" \
-          "\nGemfile from 'https' to 'http'. Instructions for compiling with OpenSSL" \
-          "\nusing RVM are available at rvm.io/packages/openssl."
-      else
-        raise e
-      end
     end
 
     # fetch index
--- lib/bundler/friendly_errors.rb
+++ lib/bundler/friendly_errors.rb
@@ -12,6 +12,19 @@ module Bundler
       exit 1
     rescue SystemExit => e
       exit e.status
+    rescue LoadError => e
+      case e.message
+      when /cannot load such file -- openssl/, /openssl.so/, /libcrypto.so/
+        Bundler.ui.error "\nCould not load OpenSSL."
+        Bundler.ui.warn "You must recompile Ruby with OpenSSL support or change the sources in your" \
+          "\nGemfile from 'https' to 'http'. Instructions for compiling with OpenSSL" \
+          "\nusing RVM are available at rvm.io/packages/openssl."
+        Bundler.ui.debug "#{e.class}: #{e.message}"
+        Bundler.ui.debug e.backtrace.join("\n")
+        exit 1
+      else
+        raise e
+      end
     rescue Exception => e
       Bundler.ui.error(
         "Unfortunately, a fatal error has occurred. Please see the Bundler \n" \
