rubygems/bundler

commit aabdb3387e889396a5f2020e1a00e57b91928917

DslError duplicates GemfileError needlessly

lib/bundler.rb
@@ -55,12 +55,10 @@ module Bundler
   class GitError             < BundlerError; status_code(11) ; end
   class DeprecatedError      < BundlerError; status_code(12) ; end
   class GemspecError         < BundlerError; status_code(14) ; end
-  class DslError             < BundlerError; status_code(15) ; end
+  class InvalidOption        < BundlerError; status_code(2)  ; end
   class ProductionError      < BundlerError; status_code(16) ; end
-  class InvalidOption        < DslError                      ; end
   class HTTPError            < BundlerError; status_code(17) ; end
   class RubyVersionMismatch  < BundlerError; status_code(18) ; end
-
 
   WINDOWS = RbConfig::CONFIG["host_os"] =~ %r!(msdos|mswin|djgpp|mingw)!
   FREEBSD = RbConfig::CONFIG["host_os"] =~ /bsd/

lib/bundler/dsl.rb
@@ -80,7 +80,7 @@ module Bundler
           elsif dep.type == :development
             return
           else
-            raise DslError, "You cannot specify the same gem twice with different version requirements. \n" \
+            raise GemfileEror, "You cannot specify the same gem twice with different version requirements. \n" \
                             "You specified: #{current.name} (#{current.requirement}) and " \
                             "#{dep.name} (#{dep.requirement})\n"
           end
@@ -92,7 +92,7 @@ module Bundler
           elsif dep.type == :development
             return
           else
-            raise DslError, "You cannot specify the same gem twice coming from different sources.\n" \
+            raise GemfileEror, "You cannot specify the same gem twice coming from different sources.\n" \
                             "You specified that #{dep.name} (#{dep.requirement}) should come from " \
                             "#{current.source || 'an unspecified source'} and #{dep.source}\n"
           end
@@ -227,7 +227,7 @@ module Bundler
       platforms.map! { |p| p.to_sym }
       platforms.each do |p|
         next if VALID_PLATFORMS.include?(p)
-        raise DslError, "`#{p}` is not a valid platform. The available options are: #{VALID_PLATFORMS.inspect}"
+        raise GemfileError, "`#{p}` is not a valid platform. The available options are: #{VALID_PLATFORMS.inspect}"
       end
 
       if github = opts.delete("github")

commit 4205f39c4dedad24124ea2ad19ed068ea8713f8c

Fix the build
Specs were failing because of typo and inconsistency introduced in
aabdb3387e.

lib/bundler.rb
@@ -55,7 +55,7 @@ module Bundler
   class GitError             < BundlerError; status_code(11) ; end
   class DeprecatedError      < BundlerError; status_code(12) ; end
   class GemspecError         < BundlerError; status_code(14) ; end
-  class InvalidOption        < BundlerError; status_code(2)  ; end
+  class InvalidOption        < BundlerError; status_code(15) ; end
   class ProductionError      < BundlerError; status_code(16) ; end
   class HTTPError            < BundlerError; status_code(17) ; end
   class RubyVersionMismatch  < BundlerError; status_code(18) ; end

lib/bundler/dsl.rb
@@ -80,7 +80,7 @@ module Bundler
           elsif dep.type == :development
             return
           else
-            raise GemfileEror, "You cannot specify the same gem twice with different version requirements. \n" \
+            raise GemfileError, "You cannot specify the same gem twice with different version requirements. \n" \
                             "You specified: #{current.name} (#{current.requirement}) and " \
                             "#{dep.name} (#{dep.requirement})\n"
           end
@@ -92,7 +92,7 @@ module Bundler
           elsif dep.type == :development
             return
           else
-            raise GemfileEror, "You cannot specify the same gem twice coming from different sources.\n" \
+            raise GemfileError, "You cannot specify the same gem twice coming from different sources.\n" \
                             "You specified that #{dep.name} (#{dep.requirement}) should come from " \
                             "#{current.source || 'an unspecified source'} and #{dep.source}\n"
           end