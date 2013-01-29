ab34cd87de618776188d1a55d4566240a9f2835e
Andre Arko <andre@arko.net>
2013-01-29 15:58:20 -0800

    need a subclass error for status_code

--- lib/bundler/vendored_persistent.rb
+++ lib/bundler/vendored_persistent.rb
@@ -3,7 +3,7 @@ begin
   # ensure OpenSSL is loaded
   OpenSSL
 rescue LoadError, NameError => e
-  raise Bundler::BundlerError, "\nCould not load OpenSSL." \
+  raise Bundler::HTTPError, "\nCould not load OpenSSL." \
     "\nYou must recompile Ruby with OpenSSL support or change the sources in your" \
     "\nGemfile from 'https' to 'http'. Instructions for compiling with OpenSSL" \
     "\nusing RVM are available at rvm.io/packages/openssl."
