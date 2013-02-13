d30026e9c8fc6c98478120866a47ca5b619251b8
Andre Arko <andre@arko.net>
2013-02-13 12:28:44 -0800

    warn on deprecated symbol source

--- lib/bundler/dsl.rb
+++ lib/bundler/dsl.rb
@@ -107,6 +107,9 @@ module Bundler
     def source(source, options = {})
       case source
       when :gemcutter, :rubygems, :rubyforge then
+        Bundler.ui.warn "The source :#{source} is deprecated because HTTP " \
+          "requests are insecure.\nPlease change your source to 'https://" \
+          "rubygems.org' if possible, or 'http://rubygems.org' if not."
         @rubygems_source.add_remote "https://rubygems.org"
         return
       when String
