617bed3c7bc89a3a5a7e3c604ce175ca522dfbbd
Andre Arko <andre@arko.net>
2013-03-02 00:16:07 -0800

    re-handle rubygems requiring openssl

--- lib/bundler/friendly_errors.rb
+++ lib/bundler/friendly_errors.rb
@@ -5,6 +5,14 @@ module Bundler
     Bundler.ui.error e.message, :wrap => true
     Bundler.ui.trace e
     exit e.status_code
+  rescue LoadError => e
+    raise e unless e.message =~ /cannot load such file -- openssl|openssl.so|libcrypto.so/
+    Bundler.ui.error "\nCould not load OpenSSL."
+    Bundler.ui.warn "You must recompile Ruby with OpenSSL support or change the sources in your" \
+      "\nGemfile from 'https' to 'http'. Instructions for compiling with OpenSSL" \
+      "\nusing RVM are available at rvm.io/packages/openssl."
+    Bundler.ui.trace e
+    exit 1
   rescue Interrupt => e
     Bundler.ui.error "\nQuitting..."
     Bundler.ui.trace e
