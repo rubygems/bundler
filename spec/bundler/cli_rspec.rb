19efe8a2f01ac54fa7c66a56dd36a2a93931c555
Pieter Joost van de Sande <pj@born2code.net>
2013-05-19 17:17:49 +0200

    Add spec for exitstatus for unrecognized tasks

--- spec/bundler/cli_rspec.rb
+++ spec/bundler/cli_rspec.rb
@@ -6,4 +6,9 @@ describe 'bundle executable' do
     bundle '--invalid_argument', :exitstatus => true
     expect(exitstatus).to_not be_zero
   end
+
+  it 'returns non-zero exit status when passed unrecognized task' do
+    bundle 'unrecognized-tast', :exitstatus => true
+    expect(exitstatus).to_not be_zero
+  end
 end
