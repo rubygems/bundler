4785e0d66d4c3ca91d4893aadc81caeca5acd4dc
Michael Pearson <mipearson@gmail.com>
2013-06-12 09:19:52 +1000

    Discover new false-positive in cached gemspec test.
    Retry by omitting gem creation entirely and only writing gemspec, not gem.

--- spec/install/gems/simple_case_spec.rb
+++ spec/install/gems/simple_case_spec.rb
@@ -815,23 +815,21 @@ describe "bundle install with gem sources" do
   end
 
   it "should use gemspecs in the system cache when available" do
-    build_gem "wibble", "1.0.0", :to_system => true
-    build_gem "wobble", "1.0.0", :to_system => true
-
     gemfile <<-G
       source "http://localtestserver.gem"
-      gem 'wibble'
+      gem 'rack'
     G
 
-    File.open("#{tmp}/gems/system/specifications/wibble-1.0.0.gemspec", 'w+') do |f|
+    FileUtils.mkdir_p "#{tmp}/gems/system/specifications"
+    File.open("#{tmp}/gems/system/specifications/rack-1.0.0.gemspec", 'w+') do |f|
       spec = Gem::Specification.new do |s|
-        s.name = 'wibble'
+        s.name = 'rack'
         s.version = '1.0.0'
-        s.add_runtime_dependency 'wobble', '1.0.0'
+        s.add_runtime_dependency 'activesupport', '2.3.2'
       end
       f.write spec.to_ruby
     end
     bundle :install, :artifice => 'endpoint_marshal_fail' # force gemspec load
-    should_be_installed "wobble 1.0.0"
+    should_be_installed "activesupport 2.3.2"
   end
 end
