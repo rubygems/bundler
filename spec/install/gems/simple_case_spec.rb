44d84c9dcaa42233416e720bfced7c1f56a80e95
Michael Pearson <mipearson@gmail.com>
2013-06-11 21:10:40 +1000

    Spec to ensure cached gemspecs are used when available

--- spec/install/gems/simple_case_spec.rb
+++ spec/install/gems/simple_case_spec.rb
@@ -814,4 +814,21 @@ describe "bundle install with gem sources" do
     end
   end
 
+  it "should use gemspecs in the system cache when available" do
+    build_gem "rack", "1.0.0", :to_system => true
+    gemfile <<-G
+      source "http://localtestserver.gem"
+      gem 'rack'
+    G
+    spec = Gem::Specification.new do |s|
+      s.name = 'rack'
+      s.version = '1.0.0'
+      s.add_runtime_dependency 'activesupport', '2.3.2'
+    end
+    File.open("#{tmp}/gems/system/specifications/rack-1.0.0.gemspec", 'w+') do |f|
+      f.write spec.to_ruby
+    end
+    bundle :install, :verbose => true, :artifice => 'endpoint_marshal_fail' # force gemspec load
+    should_be_installed "activesupport 2.3.2"
+  end
 end
