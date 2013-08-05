f8211e923f13d33daf429758123ab29065285113
Jessica Lynn Suttles <jlsuttles@gmail.com>
2013-08-05 14:52:12 -0700

    updates bundle command names spec

--- spec/other/cli_dispatch_spec.rb
+++ spec/other/cli_dispatch_spec.rb
@@ -4,18 +4,18 @@ describe "bundle command names" do
   it "work when given fully" do
     bundle "install"
     expect(err).to eq("")
-    expect(out).to match(/Bundler::GemfileNotFound/)
+    expect(out).not_to match(/Ambiguous command/)
   end
 
   it "work when not ambiguous" do
     bundle "ins"
     expect(err).to eq("")
-    expect(out).to match(/Bundler::GemfileNotFound/)
+    expect(out).not_to match(/Ambiguous command/)
   end
 
   it "print a friendly error when ambiguous" do
     bundle "i"
     expect(err).to eq("")
-    expect(out).to match(/Ambiguous command i matches \[init, inject, install\]/)
+    expect(out).to match(/Ambiguous command/)
   end
 end
