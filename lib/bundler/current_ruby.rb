c8797067660b648f3d68d1142f126d52b892a27f
Tony Miller <mcfiredrill@gmail.com>
2013-08-24 10:00:52 -0700

    add jruby_18 and jruby_19 to platforms

--- lib/bundler/current_ruby.rb
+++ lib/bundler/current_ruby.rb
@@ -60,6 +60,14 @@ module Bundler
       defined?(RUBY_ENGINE) && RUBY_ENGINE == "jruby"
     end
 
+    def jruby_18?
+      jruby? && on_18?
+    end
+
+    def jruby_19?
+      jruby? && on_19?
+    end
+
     def maglev?
       defined?(RUBY_ENGINE) && RUBY_ENGINE == "maglev"
     end
