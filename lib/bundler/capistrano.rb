2ffd162f5be6cd88d32013330b620379f9f325c7
sanemat <o.gata.ken@gmail.com>
2013-08-30 17:46:41 +0900

    Require version file for checking capistrano version
    bundler-1.4.0.pre.1 works fine, but bundler-1.4.0.pre.2 shows error below:
    ```
    $ bundle exec cap production deploy
    /path/to/bundler-1.4.0.pre.2/lib/bundler/capistrano.rb:7:in `<top (required)>': uninitialized constant Capistrano::Version (NameError)
    ```
    
    This commit brings this error, so I add require version file.
    1d7a7893dfeb85afa25a3a526354b31c85f05dbf
    
    bundler: 1.4.0.pre.2
    capistrano: 2.14.2

--- lib/bundler/capistrano.rb
+++ lib/bundler/capistrano.rb
@@ -3,6 +3,7 @@
 # Just add "require 'bundler/capistrano'" in your Capistrano deploy.rb, and
 # Bundler will be activated after each new deployment.
 require 'bundler/deployment'
+require 'capistrano/version'
 
 if Gem::Version.new(Capistrano::Version).release >= Gem::Version.new("3.0")
   raise "For Capistrano 3.x integration, please use http://github.com/capistrano/bundler"
