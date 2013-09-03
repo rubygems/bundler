541aafe3f68c2052e1893a58c9e0332ec75129b9
Hemant Kumar <gethemant@gmail.com>
2013-09-03 11:05:20 +0530

    Handle unhandled exceptions for thread workers and enque a wrapped exception

--- lib/bundler/parallel_workers/thread_worker.rb
+++ lib/bundler/parallel_workers/thread_worker.rb
@@ -15,7 +15,11 @@ module Bundler
             loop do
               obj = @request_queue.deq
               break if obj.equal? POISON
-              @response_queue.enq func.call(obj)
+              begin
+                @response_queue.enq func.call(obj)
+              rescue Exception => e
+                @response_queue.enq(WrappedException.new(e))
+              end
             end
           end
         end
