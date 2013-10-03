39f64b962361316f6028d2bb60b06355054f0936
Andre Arko <andre@arko.net>
2013-10-02 23:07:39 -0700

    allow exceptions to be passed to Retry

--- spec/bundler/retry_spec.rb
+++ spec/bundler/retry_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "bundle retry" do
   it "return successful result if no errors" do
     attempts = 0
-    result = Bundler::Retry.new(nil, 3).attempt do
+    result = Bundler::Retry.new(nil, nil, 3).attempt do
       attempts += 1
       :success
     end
@@ -11,10 +11,21 @@ describe "bundle retry" do
     expect(attempts).to eq(1)
   end
 
+  it "defaults to retrying twice" do
+    attempts = 0
+    expect {
+      Bundler::Retry.new(nil).attempt do
+        attempts += 1
+        raise "nope"
+      end
+    }.to raise_error
+    expect(attempts).to eq(3)
+  end
+
   it "returns the first valid result" do
     jobs = [Proc.new{ raise "foo" }, Proc.new{ :bar }, Proc.new{ raise "foo" }]
     attempts = 0
-    result = Bundler::Retry.new(nil, 3).attempt do
+    result = Bundler::Retry.new(nil, nil, 3).attempt do
       attempts += 1
       job = jobs.shift
       job.call
@@ -27,11 +38,23 @@ describe "bundle retry" do
     error    = Bundler::GemfileNotFound
     attempts = 0
     expect {
-      Bundler::Retry.new(nil, 3).attempt do
+      Bundler::Retry.new(nil, nil, 3).attempt do
         attempts += 1
         raise error
       end
     }.to raise_error(error)
     expect(attempts).to eq(4)
+  end
+
+  it "raises exceptions" do
+    error = Bundler::GemfileNotFound
+    attempts = 0
+    expect {
+      Bundler::Retry.new(nil, error).attempt do
+        attempts += 1
+        raise error
+      end
+    }.to raise_error(error)
+    expect(attempts).to eq(1)
   end
 end
