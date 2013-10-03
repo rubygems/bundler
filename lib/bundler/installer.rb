db8d55ee49692612fdd1a8e55be7ae280b58277e
Andre Arko <andre@arko.net>
2013-10-02 23:21:11 -0700

    stop retrying the resolver

--- lib/bundler/installer.rb
+++ lib/bundler/installer.rb
@@ -79,13 +79,7 @@ module Bundler
       # Since we are installing, we can resolve the definition
       # using remote specs
       unless local
-        if options["local"]
-          @definition.resolve_with_cache!
-        else
-          Bundler::Retry.new("source fetch").attempts do
-            @definition.resolve_remotely!
-          end
-        end
+        options["local"] ? @definition.resolve_with_cache! : @definition.resolve_remotely!
       end
       # Must install gems in the order that the resolver provides
       # as dependencies might actually affect the installation of
