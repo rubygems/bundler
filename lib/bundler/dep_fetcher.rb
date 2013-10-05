02a59e6ae6e0509ec7943e8b1ae9d94f2584668d
Andre Arko <andre@arko.net>
2013-10-05 22:04:50 +0000

    download new index files from the server \o/

--- lib/bundler/dep_fetcher.rb
+++ lib/bundler/dep_fetcher.rb
@@ -1,15 +1,18 @@
+require 'bundler/vendored_persistent'
+
 module Bundler
   class DepFetcher
 
     def initialize(source_uri)
       @source_uri = source_uri
+      @http = Net::HTTP::Persistent.new("bundler")
     end
 
     def fetch(names)
       Bundler.bundle_path.join("deps").mkpath
 
       names.each do |name|
-        deps = File.read File.expand_path("~/src/bundler/new-index/deps/#{name}")
+        deps = @http.request(@source_uri + "/api/v2/deps/#{name}").body
         Bundler.bundle_path.join("deps/#{name}").open("w"){|f| f.write(deps) }
       end
     end
