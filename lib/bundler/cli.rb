bb31c05760ec9395379fd1918f667417bcbb867e
Trent Ogren <tedwardo2@gmail.com>
2013-10-16 19:52:30 -0500

    `--local` option for `bundle gem` command
    Gems created only to be used locally by one app need less code
    generated.

--- lib/bundler/cli.rb
+++ lib/bundler/cli.rb
@@ -705,6 +705,7 @@ module Bundler
     desc "gem GEM", "Creates a skeleton for creating a rubygem"
     method_option :bin, :type => :boolean, :default => false, :aliases => '-b', :banner => "Generate a binary for your library."
     method_option :test, :type => :string, :lazy_default => 'rspec', :aliases => '-t', :banner => "Generate a test directory for your library: 'rspec' is the default, but 'minitest' is also supported."
+    method_option :local, :type => :boolean, :default => false, :aliases => '-l', :banner => "Generate only what's needed for a gem that will be local to a single application"
     method_option :edit, :type => :string, :aliases => "-e",
                   :lazy_default => [ENV['BUNDLER_EDITOR'], ENV['VISUAL'], ENV['EDITOR']].find{|e| !e.nil? && !e.empty? },
                   :required => false, :banner => "/path/to/your/editor",
@@ -726,17 +727,18 @@ module Bundler
         :constant_array  => constant_array,
         :author          => git_user_name.empty? ? "TODO: Write your name" : git_user_name,
         :email           => git_user_email.empty? ? "TODO: Write your email address" : git_user_email,
+        :local           => options[:local],
         :test            => options[:test]
       }
       gemspec_dest = File.join(target, "#{name}.gemspec")
       template(File.join("newgem/Gemfile.tt"),               File.join(target, "Gemfile"),                             opts)
       template(File.join("newgem/Rakefile.tt"),              File.join(target, "Rakefile"),                            opts)
-      template(File.join("newgem/LICENSE.txt.tt"),           File.join(target, "LICENSE.txt"),                         opts)
-      template(File.join("newgem/README.md.tt"),             File.join(target, "README.md"),                           opts)
+      template(File.join("newgem/LICENSE.txt.tt"),           File.join(target, "LICENSE.txt"),                         opts) unless options[:local]
+      template(File.join("newgem/README.md.tt"),             File.join(target, "README.md"),                           opts) unless options[:local]
       template(File.join("newgem/gitignore.tt"),             File.join(target, ".gitignore"),                          opts)
       template(File.join("newgem/newgem.gemspec.tt"),        gemspec_dest,                                             opts)
       template(File.join("newgem/lib/newgem.rb.tt"),         File.join(target, "lib/#{namespaced_path}.rb"),           opts)
-      template(File.join("newgem/lib/newgem/version.rb.tt"), File.join(target, "lib/#{namespaced_path}/version.rb"),   opts)
+      template(File.join("newgem/lib/newgem/version.rb.tt"), File.join(target, "lib/#{namespaced_path}/version.rb"),   opts) unless options[:local]
       if options[:bin]
         template(File.join("newgem/bin/newgem.tt"),          File.join(target, 'bin', name),                           opts)
       end
@@ -749,11 +751,13 @@ module Bundler
         template(File.join("newgem/test/minitest_helper.rb.tt"), File.join(target, "test/minitest_helper.rb"),         opts)
         template(File.join("newgem/test/test_newgem.rb.tt"),     File.join(target, "test/test_#{namespaced_path}.rb"), opts)
       end
-      if options[:test]
+      if options[:test] && !options[:local]
         template(File.join("newgem/.travis.yml.tt"),         File.join(target, ".travis.yml"),            opts)
       end
-      Bundler.ui.info "Initializing git repo in #{target}"
-      Dir.chdir(target) { `git init`; `git add .` }
+      unless options[:local]
+        Bundler.ui.info "Initializing git repo in #{target}"
+        Dir.chdir(target) { `git init`; `git add .` }
+      end
 
       if options[:edit]
         run("#{options["edit"]} \"#{gemspec_dest}\"")  # Open gemspec in editor
