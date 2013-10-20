5e294f645cd9cc19c089bbd76c1118a6f9f99df2
Andre Arko <andre@arko.net>
2013-10-19 18:00:06 -0700

    1.8.7 can't URI a URI

--- lib/bundler/source.rb
+++ lib/bundler/source.rb
@@ -5,11 +5,13 @@ module Bundler
     autoload :Git,      'bundler/source/git'
 
     def self.mirror_for(uri)
+      uri = URI(uri.to_s) unless uri.is_a?(URI)
+
       # Settings keys are all downcased
       mirrors = Bundler.settings.gem_mirrors
       normalized_key = URI(uri.to_s.downcase)
 
-      mirrors[normalized_key] || URI(uri)
+      mirrors[normalized_key] || uri
     end
 
   end
