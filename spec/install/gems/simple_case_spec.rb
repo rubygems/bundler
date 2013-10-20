49df122bffbafcfaa669567149394ea99501f51e
Andre Arko <andre@arko.net>
2013-10-19 17:06:32 -0700

    prereleases spec

--- spec/install/gems/simple_case_spec.rb
+++ spec/install/gems/simple_case_spec.rb
@@ -362,44 +362,6 @@ describe "bundle install with gem sources" do
     end
   end
 
-  describe "when prerelease gems are available" do
-    it "finds prereleases" do
-      install_gemfile <<-G
-        source "file://#{gem_repo1}"
-        gem "not_released"
-      G
-      should_be_installed "not_released 1.0.pre"
-    end
-
-    it "uses regular releases if available" do
-      install_gemfile <<-G
-        source "file://#{gem_repo1}"
-        gem "has_prerelease"
-      G
-      should_be_installed "has_prerelease 1.0"
-    end
-
-    it "uses prereleases if requested" do
-      install_gemfile <<-G
-        source "file://#{gem_repo1}"
-        gem "has_prerelease", "1.1.pre"
-      G
-      should_be_installed "has_prerelease 1.1.pre"
-    end
-  end
-
-  describe "when prerelease gems are not available" do
-    it "still works" do
-      build_repo3
-      install_gemfile <<-G
-        source "file://#{gem_repo3}"
-        gem "rack"
-      G
-
-      should_be_installed "rack 1.0"
-    end
-  end
-
   describe "when BUNDLE_PATH or the global path config is set" do
     before :each do
       build_lib "rack", "1.0.0", :to_system => true do |s|
