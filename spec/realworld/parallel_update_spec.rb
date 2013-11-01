c21741dd08cb95dff6e0215d03e1d8ae9aa38cf8
Kenta Murata <mrkn@mrkn.jp>
2013-11-02 05:55:14 +0900

    Add tests for parallel update

---
+++ spec/realworld/parallel_update_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "updating dependencies parallely", :realworld => true do
+  before :each do
+    install_gemfile <<-G
+      source "https://rubygems.org"
+      gem 'activesupport', '~> 3.2.13'
+      gem 'faker', '~> 1.1.2'
+    G
+  end
+
+  it "installs gems parallely" do
+    gemfile <<-G
+      source "https://rubygems.org"
+      gem 'activesupport', '~> 4.0.0'
+      gem 'faker', '~> 1.1.2'
+    G
+
+    bundle :update, :jobs => 4
+
+    bundle "show activesupport"
+    expect(out).to match(/activesupport-4\.0\.\d+/)
+
+    bundle "show faker"
+    expect(out).to match(/faker/)
+
+    bundle "config jobs"
+    expect(out).to match(/: "4"/)
+  end
+end
