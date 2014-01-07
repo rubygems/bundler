b508f5f830894947560f05d8d7781057ebd8d190
Smit Shah <who828@gmail.com>
2014-01-07 08:52:01 +0530

    Refactored exec method in cli

--- lib/bundler/cli.rb
+++ lib/bundler/cli.rb
@@ -249,29 +249,8 @@ module Bundler
       into the system wide Rubygems repository.
     D
     def exec(*args)
-      Bundler.definition.validate_ruby!
-      Bundler.load.setup_environment
-
-      begin
-        if RUBY_VERSION >= "2.0"
-          args << { :close_others => !options.keep_file_descriptors? }
-        elsif options.keep_file_descriptors?
-          Bundler.ui.warn "Ruby version #{RUBY_VERSION} defaults to keeping non-standard file descriptors on Kernel#exec."
-        end
-
-        # Run
-        Kernel.exec(*args)
-      rescue Errno::EACCES
-        Bundler.ui.error "bundler: not executable: #{args.first}"
-        exit 126
-      rescue Errno::ENOENT
-        Bundler.ui.error "bundler: command not found: #{args.first}"
-        Bundler.ui.warn  "Install missing gem executables with `bundle install`"
-        exit 127
-      rescue ArgumentError
-        Bundler.ui.error "bundler: exec needs a command to run"
-        exit 128
-      end
+      require 'bundler/cli/exec'
+      Exec.new(options, args).run
     end
 
     desc "config NAME [VALUE]", "retrieve or set a configuration value"
