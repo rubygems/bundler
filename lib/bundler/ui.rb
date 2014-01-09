7a647149df05acff2c3e6d3fe2c6e60725c74671
Andre Arko <andre@arko.net>
2014-01-09 06:03:59 -0800

    realign public interfaces of UI and Shell

--- lib/bundler/ui.rb
+++ lib/bundler/ui.rb
@@ -3,33 +3,39 @@ module Bundler
     autoload :RGProxy, 'bundler/ui/rg_proxy'
     autoload :Shell,   'bundler/ui/shell'
 
-    def warn(message, newline = nil)
-    end
-
-    def debug(message, newline = nil)
-    end
-
-    def trace(message, newline = nil)
-    end
-
-    def error(message, newline = nil)
-    end
-
     def info(message, newline = nil)
     end
 
     def confirm(message, newline = nil)
     end
 
-    def quiet?
-      false
+    def warn(message, newline = nil)
+    end
+
+    def error(message, newline = nil)
+    end
+
+    def debug(message, newline = nil)
     end
 
     def debug?
       false
     end
 
+    def quiet?
+      false
+    end
+
     def ask(message)
+    end
+
+    def level=(name)
+    end
+
+    def level(name = nil)
+    end
+
+    def trace(message, newline = nil)
     end
 
     def silence
