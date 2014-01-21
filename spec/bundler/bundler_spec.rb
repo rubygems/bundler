2e7b9096bab188b0efa87c9dd91748b1fab3ed82
Kamil Kieliszczyk <kamil@kieliszczyk.net>
2014-01-21 11:11:27 +0100

    Use subject(:gemspec) in favor of instance variables

--- spec/bundler/bundler_spec.rb
+++ spec/bundler/bundler_spec.rb
@@ -4,71 +4,75 @@ require 'bundler'
 
 describe Bundler do
   describe "#load_gemspec_uncached" do
+    subject(:gemspec) { tmp("test.gemspec") }
 
-    before do
-      @gemspec = tmp("test.gemspec")
-      @gemspec.open('wb') do |f|
-        f.write strip_whitespace(<<-GEMSPEC)
-          ---
-            {:!00 ao=gu\g1= 7~f
-        GEMSPEC
-      end
-    end
-
-    describe "on Ruby 1.8", :ruby => "1.8" do
-      it "should catch YAML syntax errors" do
-        expect { Bundler.load_gemspec_uncached(@gemspec) }.
-          to raise_error(Bundler::GemspecError)
-      end
-    end
-
-    context "on Ruby 1.9", :ruby => "1.9" do
-      context "with Syck as YAML::Engine" do
-        it "raises a GemspecError after YAML load throws ArgumentError" do
-          orig_yamler, YAML::ENGINE.yamler = YAML::ENGINE.yamler, 'syck'
-
-          expect { Bundler.load_gemspec_uncached(@gemspec) }.
-            to raise_error(Bundler::GemspecError)
-
-          YAML::ENGINE.yamler = orig_yamler
+    context "with incorrect YAML file" do
+      before do
+        File.open(gemspec, "wb") do |f|
+          f.write strip_whitespace(<<-GEMSPEC)
+            ---
+              {:!00 ao=gu\g1= 7~f
+          GEMSPEC
         end
       end
 
-      context "with Psych as YAML::Engine" do
-        it "raises a GemspecError after YAML load throws Psych::SyntaxError" do
-          orig_yamler, YAML::ENGINE.yamler = YAML::ENGINE.yamler, 'psych'
-
-          expect { Bundler.load_gemspec_uncached(@gemspec) }.
+      context "on Ruby 1.8", :ruby => "1.8" do
+        it "should catch YAML syntax errors" do
+          expect { Bundler.load_gemspec_uncached(gemspec) }.
             to raise_error(Bundler::GemspecError)
-
-          YAML::ENGINE.yamler = orig_yamler
         end
       end
-    end
 
-    it "can load a gemspec with unicode characters with default ruby encoding" do
-      # spec_helper forces the external encoding to UTF-8 but that's not the
-      # ruby default.
-      encoding = nil
+      context "on Ruby 1.9", :ruby => "1.9" do
+        context "with Syck as YAML::Engine" do
+          it "raises a GemspecError after YAML load throws ArgumentError" do
+            orig_yamler, YAML::ENGINE.yamler = YAML::ENGINE.yamler, 'syck'
 
-      if defined?(Encoding)
-        encoding = Encoding.default_external
-        Encoding.default_external = "ASCII"
-      end
+            expect { Bundler.load_gemspec_uncached(gemspec) }.
+              to raise_error(Bundler::GemspecError)
 
-      File.open(tmp("test.gemspec"), "wb") do |file|
-        file.puts <<-G.gsub(/^\s+/, '')
-          # -*- encoding: utf-8 -*-
-          Gem::Specification.new do |gem|
-            gem.author = "André the Giant"
+            YAML::ENGINE.yamler = orig_yamler
           end
-        G
+        end
+
+        context "with Psych as YAML::Engine" do
+          it "raises a GemspecError after YAML load throws Psych::SyntaxError" do
+            orig_yamler, YAML::ENGINE.yamler = YAML::ENGINE.yamler, 'psych'
+
+            expect { Bundler.load_gemspec_uncached(gemspec) }.
+              to raise_error(Bundler::GemspecError)
+
+            YAML::ENGINE.yamler = orig_yamler
+          end
+        end
       end
+    end
 
-      gemspec = Bundler.load_gemspec_uncached(tmp("test.gemspec"))
-      expect(gemspec.author).to eq("André the Giant")
+    context "with correct YAML file" do
+      it "can load a gemspec with unicode characters with default ruby encoding" do
+        # spec_helper forces the external encoding to UTF-8 but that's not the
+        # ruby default.
+        encoding = nil
 
-      Encoding.default_external = encoding if defined?(Encoding)
+        if defined?(Encoding)
+          encoding = Encoding.default_external
+          Encoding.default_external = "ASCII"
+        end
+
+        File.open(gemspec, "wb") do |file|
+          file.puts <<-GEMSPEC.gsub(/^\s+/, '')
+            # -*- encoding: utf-8 -*-
+            Gem::Specification.new do |gem|
+              gem.author = "André the Giant"
+            end
+          GEMSPEC
+        end
+
+        expect(Bundler.load_gemspec_uncached(gemspec).author).
+          to eq("André the Giant")
+
+        Encoding.default_external = encoding if defined?(Encoding)
+      end
     end
 
   end
