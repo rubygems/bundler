17f4a87ce64e15f0920e7c7db692e51a3ae152a6
Andre Arko <andre@arko.net>
2014-04-07 17:20:20 +0900

    root warn spec to sudo specs

--- spec/bundler/cli_spec.rb
+++ spec/bundler/cli_spec.rb
@@ -4,30 +4,6 @@ require 'bundler/cli'
 describe "bundle executable" do
   let(:source_uri) { "http://localgemserver.test" }
 
-  context "#warn_if_root" do
-    it "warns the user when install is run as root" do
-      expect(Process).to receive(:uid).and_return(0)
-
-      gemfile <<-G
-        source "#{source_uri}"
-      G
-
-      warning = <<-W
-
-WARNING ****************************************************************
-Running bundler with sudo will likely have unintended consequences.
-If bundler requires you to run a command with sudo it will let you know.
-************************************************************************
-
-      W
-
-      output = capture_output {
-        Bundler::CLI.new.warn_if_root
-      }
-      expect(output).to include(warning)
-    end
-  end
-
   it "returns non-zero exit status when passed unrecognized options" do
     bundle '--invalid_argument', :exitstatus => true
     expect(exitstatus).to_not be_zero
