cc0d33a8cce9c3303bd6d8c7e29164d5d1ceebde
Andre Arko <andre@arko.net>
2014-04-28 05:30:36 -0700

    editor might need shell parsing

--- lib/bundler/cli/open.rb
+++ lib/bundler/cli/open.rb
@@ -1,4 +1,5 @@
 require 'bundler/cli/common'
+require 'shellwords'
 
 module Bundler
   class CLI::Open
@@ -11,11 +12,9 @@ module Bundler
     def run
       editor = [ENV['BUNDLER_EDITOR'], ENV['VISUAL'], ENV['EDITOR']].find{|e| !e.nil? && !e.empty? }
       return Bundler.ui.info("To open a bundled gem, set $EDITOR or $BUNDLER_EDITOR") unless editor
-      spec = Bundler::CLI::Common.select_spec(name, :regex_match)
-      return unless spec
-      full_gem_path = spec.full_gem_path
-      Dir.chdir(full_gem_path) do
-        command = [editor, full_gem_path]
+      path = Bundler::CLI::Common.select_spec(name, :regex_match).full_gem_path
+      Dir.chdir(path) do
+        command = [*Shellwords.split(editor), path]
         system(*command) || Bundler.ui.info("Could not run '#{command.join(' ')}'")
       end
     end
