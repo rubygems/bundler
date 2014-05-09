40f71c8d7a33d929accfdf7434df048d8b2cd8db
Matijs van Zuijlen <matijs@matijs.net>
2014-05-09 07:16:29 +0200

    Ensure Gemspec.lock and Gemfile match in multi-platform cache spec
    The activerecord 2.3.3 gem needs activesupport 2.3.2, which was not
    included in the Gemfile.lock or cache. This caused bundle update to
    fail. This change simplifies the set of needed gems, thereby returning
    to an older version of this spec. By adding the mri platforms to the
    block for 'rack' we keep the check for allowed platforms.

--- spec/cache/platform_spec.rb
+++ spec/cache/platform_spec.rb
@@ -5,16 +5,13 @@ describe "bundle cache with multiple platforms" do
     gemfile <<-G
       source "file://#{gem_repo1}"
 
-      platforms :ruby, :ruby_18, :ruby_19, :ruby_20, :ruby_21 do
+      platforms :ruby, :ruby_18, :ruby_19, :ruby_20, :ruby_21,
+                :mri, :mri_18, :mri_19, :mri_20, :mri_21 do
         gem "rack", "1.0.0"
       end
 
       platforms :jruby do
         gem "activesupport", "2.3.5"
-      end
-
-      platforms :mri, :mri_18, :mri_19, :mri_20, :mri_21 do
-        gem "activerecord", "2.3.2"
       end
     G
 
@@ -24,7 +21,6 @@ describe "bundle cache with multiple platforms" do
         specs:
           rack (1.0.0)
           activesupport (2.3.5)
-          activerecord (2.3.2)
 
       PLATFORMS
         ruby
@@ -33,10 +29,9 @@ describe "bundle cache with multiple platforms" do
       DEPENDENCIES
         rack (1.0.0)
         activesupport (2.3.5)
-        activerecord (2.3.2)
     G
 
-    cache_gems "rack-1.0.0", "activesupport-2.3.5", "activerecord-2.3.2"
+    cache_gems "rack-1.0.0", "activesupport-2.3.5"
   end
 
   it "ensures that a succesful bundle install does not delete gems for other platforms" do
@@ -46,7 +41,6 @@ describe "bundle cache with multiple platforms" do
 
     expect(bundled_app("vendor/cache/rack-1.0.0.gem")).to exist
     expect(bundled_app("vendor/cache/activesupport-2.3.5.gem")).to exist
-    expect(bundled_app("vendor/cache/activerecord-2.3.2.gem")).to exist
   end
 
   it "ensures that a succesful bundle update does not delete gems for other platforms" do
@@ -56,6 +50,5 @@ describe "bundle cache with multiple platforms" do
 
     expect(bundled_app("vendor/cache/rack-1.0.0.gem")).to exist
     expect(bundled_app("vendor/cache/activesupport-2.3.5.gem")).to exist
-    expect(bundled_app("vendor/cache/activerecord-2.3.2.gem")).to exist
   end
 end
