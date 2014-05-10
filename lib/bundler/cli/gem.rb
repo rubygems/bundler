4fdb5a7d8139aa26a7d6eefa8ff5821f014a8eb2
Andre Arko <andre@arko.net>
2014-05-10 14:35:22 -0700

    hey let's copy and paste less code

--- lib/bundler/cli/gem.rb
+++ lib/bundler/cli/gem.rb
@@ -1,23 +1,29 @@
+require 'pathname'
+
 module Bundler
   class CLI::Gem
-    attr_reader :options, :gem_name, :thor
+    attr_reader :options, :gem_name, :thor, :name, :target
+
     def initialize(options, gem_name, thor)
       @options = options
       @gem_name = gem_name
       @thor = thor
+
+      @name = gem_name.chomp("/") # remove trailing slash if present
+      @target = Pathname.pwd.join(name)
+
       validate_ext_name if options[:ext]
     end
 
     def run
-      name = gem_name.chomp("/") # remove trailing slash if present
       underscored_name = name.tr('-', '_')
       namespaced_path = name.tr('-', '/')
-      target = File.join(Dir.pwd, name)
       constant_name = name.split('_').map{|p| p[0..0].upcase + p[1..-1] }.join
       constant_name = constant_name.split('-').map{|q| q[0..0].upcase + q[1..-1] }.join('::') if constant_name =~ /-/
       constant_array = constant_name.split('::')
       git_user_name = `git config user.name`.chomp
       git_user_email = `git config user.email`.chomp
+
       opts = {
         :name             => name,
         :underscored_name => underscored_name,
@@ -30,41 +36,54 @@ module Bundler
         :test             => options[:test],
         :ext              => options[:ext]
       }
-      gemspec_dest = File.join(target, "#{name}.gemspec")
-      thor.template(File.join("newgem/Gemfile.tt"),               File.join(target, "Gemfile"),                             opts)
-      thor.template(File.join("newgem/Rakefile.tt"),              File.join(target, "Rakefile"),                            opts)
-      thor.template(File.join("newgem/LICENSE.txt.tt"),           File.join(target, "LICENSE.txt"),                         opts)
-      thor.template(File.join("newgem/README.md.tt"),             File.join(target, "README.md"),                           opts)
-      thor.template(File.join("newgem/gitignore.tt"),             File.join(target, ".gitignore"),                          opts)
-      thor.template(File.join("newgem/consolerc.tt"),             File.join(target, ".consolerc"),                          opts)
-      thor.template(File.join("newgem/newgem.gemspec.tt"),        gemspec_dest,                                             opts)
-      thor.template(File.join("newgem/lib/newgem.rb.tt"),         File.join(target, "lib/#{namespaced_path}.rb"),           opts)
-      thor.template(File.join("newgem/lib/newgem/version.rb.tt"), File.join(target, "lib/#{namespaced_path}/version.rb"),   opts)
-      if options[:bin]
-        thor.template(File.join("newgem/bin/newgem.tt"),          File.join(target, 'bin', name),                           opts)
-      end
+
+      templates = {
+        "Gemfile.tt" => "Gemfile",
+        "gitignore.tt" => ".gitignore",
+        "lib/newgem.rb.tt" => "lib/#{namespaced_path}.rb",
+        "lib/newgem/version.rb.tt" => "lib/#{namespaced_path}/version.rb"
+        "LICENSE.txt.tt" => "LICENSE.txt",
+        "newgem.gemspec.tt" => "#{name}.gemspec",
+        "newgem/consolerc.tt" => ".consolerc",
+        "Rakefile.tt" => "Rakefile",
+        "README.md.tt" => "README.md",
+      }
+
+      templates.merge!("bin/newgem.tt" => "bin/#{name}") if options[:bin]
+      templates.merge!(".travis.yml.tt" => ".travis.yml") if options[:test]
+
       case options[:test]
       when 'rspec'
-        thor.template(File.join("newgem/rspec.tt"),               File.join(target, ".rspec"),                              opts)
-        thor.template(File.join("newgem/spec/spec_helper.rb.tt"), File.join(target, "spec/spec_helper.rb"),                 opts)
-        thor.template(File.join("newgem/spec/newgem_spec.rb.tt"), File.join(target, "spec/#{namespaced_path}_spec.rb"),     opts)
+        templates.merge!(
+          "rspec.tt" => ".rspec",
+          "spec/spec_helper.rb.tt" => "spec/spec_helper.rb",
+          "spec/newgem_spec.rb.tt" => "spec/#{namespaced_path}_spec.rb"
+        )
       when 'minitest'
-        thor.template(File.join("newgem/test/minitest_helper.rb.tt"), File.join(target, "test/minitest_helper.rb"),         opts)
-        thor.template(File.join("newgem/test/test_newgem.rb.tt"),     File.join(target, "test/test_#{namespaced_path}.rb"), opts)
+        templates.merge!(
+          "test/minitest_helper.rb.tt" => "test/minitest_helper.rb",
+          "test/test_newgem.rb.tt" => "test/test_#{namespaced_path}.rb"
+        )
       end
-      if options[:test]
-        thor.template(File.join("newgem/.travis.yml.tt"),         File.join(target, ".travis.yml"),            opts)
-      end
+
       if options[:ext]
-        thor.template(File.join("newgem/ext/newgem/extconf.rb.tt"), File.join(target, "ext/#{name}/extconf.rb"), opts)
-        thor.template(File.join("newgem/ext/newgem/newgem.h.tt"), File.join(target, "ext/#{name}/#{underscored_name}.h"), opts)
-        thor.template(File.join("newgem/ext/newgem/newgem.c.tt"), File.join(target, "ext/#{name}/#{underscored_name}.c"), opts)
+        templates.merge!(
+          "ext/newgem/extconf.rb.tt" => "ext/#{name}/extconf.rb",
+          "ext/newgem/newgem.h.tt" => "ext/#{name}/#{underscored_name}.h",
+          "ext/newgem/newgem.c.tt" => "ext/#{name}/#{underscored_name}.c"
+        )
       end
+
+      templates.each do |src, dst|
+        thor.template("newgem/#{src}", target.join(dst), opts)
+      end
+
       Bundler.ui.info "Initializing git repo in #{target}"
       Dir.chdir(target) { `git init`; `git add .` }
 
       if options[:edit]
-        thor.run("#{options["edit"]} \"#{gemspec_dest}\"")  # Open gemspec in editor
+        # Open gemspec in editor
+        thor.run("#{options["edit"]} \"#{target.join("#{name}.gemspec")}\"")
       end
     end
 
