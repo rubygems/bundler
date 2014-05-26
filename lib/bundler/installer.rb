a26e7f31f145491791150f8d2c2385428ae0fde5
juanxo <juan.guerrero.lozano@gmail.com>
2014-05-26 03:14:17 +0200

    Extract standalone path expansion to do it only once

--- lib/bundler/installer.rb
+++ lib/bundler/installer.rb
@@ -252,7 +252,7 @@ module Bundler
         file.puts "ruby_version = RbConfig::CONFIG[\"ruby_version\"]"
         file.puts "path = File.expand_path('..', __FILE__)"
         paths.each do |path|
-          file.puts %{$:.unshift File.expand_path("\#{path}/#{path}")}
+          file.puts %{$:.unshift "\#{path}/#{path}"}
         end
       end
     end
