9c5838de101713f1cda34519bd60a15b8eb510e8
Smit Shah <who828@gmail.com>
2014-06-27 13:13:49 +0530

    Remove uneeded autoload for unix worker

--- lib/bundler/parallel_workers.rb
+++ lib/bundler/parallel_workers.rb
@@ -4,7 +4,6 @@ require "bundler/parallel_workers/worker"
 
 module Bundler
   module ParallelWorkers
-    autoload :UnixWorker, "bundler/parallel_workers/unix_worker"
     autoload :ThreadWorker, "bundler/parallel_workers/thread_worker"
 
     def self.worker_pool(size, job)
