37dc9b942b99f1ae9ff1877d10eaca8c2620adf0
Andre Arko <andre@arko.net>
2014-08-15 22:23:50 -0700

    remove fetch_specs

--- lib/bundler/source/rubygems.rb
+++ lib/bundler/source/rubygems.rb
@@ -66,7 +66,15 @@ module Bundler
       alias_method :name, :to_s
 
       def specs
-        @specs ||= fetch_specs
+        @specs ||= begin
+          # remote_specs usually generates a way larger Index than the other
+          # sources, and large_idx.use small_idx is way faster than
+          # small_idx.use large_idx.
+          idx = @allow_remote ? remote_specs.dup : Index.new
+          idx.use(cached_specs, :override_dupes) if @allow_cached || @allow_remote
+          idx.use(installed_specs, :override_dupes)
+          idx
+        end
       end
 
       def install(spec)
@@ -207,20 +215,6 @@ module Bundler
         else
           remote
         end
-      end
-
-      def fetch_specs
-        # remote_specs usually generates a way larger Index than the other
-        # sources, and large_idx.use small_idx is way faster than
-        # small_idx.use large_idx.
-        if @allow_remote
-          idx = remote_specs.dup
-        else
-          idx = Index.new
-        end
-        idx.use(cached_specs, :override_dupes) if @allow_cached || @allow_remote
-        idx.use(installed_specs, :override_dupes)
-        idx
       end
 
       def installed_specs
