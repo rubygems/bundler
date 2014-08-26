e936bcce4116f579531bf758d8276c2d6e7301a6
Karol Bucek <kares@users.noreply.github.com>
2014-08-26 09:27:08 +0200

    source 'https://rubygems.org'
    in Gem::Specification's `#to_gemfile`

--- lib/bundler/rubygems_ext.rb
+++ lib/bundler/rubygems_ext.rb
@@ -73,7 +73,7 @@ module Gem
     end
 
     def to_gemfile(path = nil)
-      gemfile = "source :gemcutter\n"
+      gemfile = "source 'https://rubygems.org'\n"
       gemfile << dependencies_to_gemfile(nondevelopment_dependencies)
       unless development_dependencies.empty?
         gemfile << "\n"
