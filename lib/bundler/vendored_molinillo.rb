02b4a7f4027d58b597ca8436915555b86307f960
Samuel E. Giddins <segiddins@segiddins.me>
2014-11-04 15:50:24 -0800

    [VendoredMolinillo] Remove vendor path from load path after requiring

--- lib/bundler/vendored_molinillo.rb
+++ lib/bundler/vendored_molinillo.rb
@@ -1,3 +1,5 @@
 vendor = File.expand_path('../vendor/Molinillo-0.1.0/lib', __FILE__)
-$:.unshift(vendor) unless $:.include?(vendor)
+loaded = $:.include?(vendor)
+$:.unshift(vendor) unless loaded
 require 'molinillo'
+$:.delete(vendor) unless loaded
