ad7a552fe3cdc4c82c99fd515ce86902db2919ed
Andre Arko <andre@arko.net>
2014-11-17 16:37:24 -0800

    Tentative fix for #3174
    This wraps the generation of git gem binstubs in the same exclusive lock
    that is used by installing threads. Since generating binstubs _also_
    generates the entire gem, it seems like there were install threads
    stepping on each other while writing the gem itself and while writing
    the gem to add the binstubs.
    
    Ultimately, I think we should stop writing out all the files for each
    gem twice, which seems to be what is happening here. It’s more important
    to fix the exceptions first, though. :)

--- lib/bundler/source/git.rb
+++ lib/bundler/source/git.rb
@@ -223,14 +223,16 @@ module Bundler
     private
 
       def serialize_gemspecs_in(destination)
-        expanded_path = destination.expand_path(Bundler.root)
-        Dir["#{expanded_path}/#{@glob}"].each do |spec_path|
-          # Evaluate gemspecs and cache the result. Gemspecs
-          # in git might require git or other dependencies.
-          # The gemspecs we cache should already be evaluated.
-          spec = Bundler.load_gemspec(spec_path)
-          next unless spec
-          File.open(spec_path, 'wb') {|file| file.write(spec.to_ruby) }
+        SharedHelpers.chdir(destination) do
+          expanded_path = destination.expand_path(Bundler.root)
+          Dir["#{expanded_path}/#{@glob}"].each do |spec_path|
+            # Evaluate gemspecs and cache the result. Gemspecs
+            # in git might require git or other dependencies.
+            # The gemspecs we cache should already be evaluated.
+            spec = Bundler.load_gemspec(spec_path)
+            next unless spec
+            File.open(spec_path, 'wb') {|file| file.write(spec.to_ruby) }
+          end
         end
       end
 
