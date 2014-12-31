af0e7f7e747bf59c0b44ff0e6071271aae60b368
Rohit Arondekar <rohit.arondekar@gmail.com>
2014-12-31 15:17:41 +0530

    Realign Source#version_message to reveal intent

--- lib/bundler/source.rb
+++ lib/bundler/source.rb
@@ -22,12 +22,16 @@ module Bundler
     end
 
     def version_message(spec)
-      locked_spec = Bundler.locked_gems.specs.find { |s| s.name == spec.name } if Bundler.locked_gems
-      locked_spec_version = locked_spec.version if locked_spec
       message = "#{spec.name} #{spec.version}"
-      if locked_spec_version && spec.version != locked_spec_version
-        message << " (was #{locked_spec_version})"
+
+      if Bundler.locked_gems
+        locked_spec = Bundler.locked_gems.specs.find { |s| s.name == spec.name }
+        locked_spec_version = locked_spec.version if locked_spec
+        if locked_spec_version && spec.version != locked_spec_version
+          message << " (was #{locked_spec_version})"
+        end
       end
+
       message
     end
 
