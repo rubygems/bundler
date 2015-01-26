4dc8cf6c778d7aed0c020f1564cf93827e3b0403
Andre Arko <andre@arko.net>
2015-01-25 22:24:02 -0800

    reword clean system gems warning

--- lib/bundler/cli/clean.rb
+++ lib/bundler/cli/clean.rb
@@ -16,7 +16,8 @@ module Bundler
     def require_path_or_force
       if !Bundler.settings[:path] && !options[:force]
         Bundler.ui.error "Cleaning all the gems on your system is dangerous! " \
-          "To remove every gem not in this bundle, run `bundle clean --force`."
+          "If you're sure you want to remove every system gem not in this " \
+          "bundle, run `bundle clean --force`."
         exit 1
       end
     end
