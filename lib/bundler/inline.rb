d992a6df25aa383765a542938263708a4440e15e
Samuel E. Giddins <segiddins@segiddins.me>
2015-02-26 20:58:08 -0800

    [Inline] Add bundler/inline to allow using Bundler without a Gemfile

---
+++ lib/bundler/inline.rb
@@ -0,0 +1,20 @@
+def gemfile(install = false, &blk)
+	require 'bundler'
+	old_root = Bundler.method(:root)
+	def Bundler.root
+		Pathname.pwd.expand_path
+	end
+	ENV['BUNDLE_GEMFILE'] ||= 'Gemfile'
+
+	builder = Bundler::Dsl.new
+	builder.instance_eval(&blk)
+	definition = builder.to_definition(nil, true)
+	def definition.lock(*); end
+	definition.validate_ruby!
+	Bundler::Installer.install(Bundler.root, definition, :system => true) if install
+	runtime = Bundler::Runtime.new(nil, definition)
+	runtime.setup_environment
+	runtime.setup.require
+
+	Bundler.send(:define_singleton_method, :root, old_root)
+end
