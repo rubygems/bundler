5b63bcbf587e4c6a87e17f783afe4bcd625b0f7c
Petr Skocik <pskocik@gmail.com>
2015-03-04 22:04:11 +0100

    gemspec should accept a glob option in its argument hash

--- lib/bundler/dsl.rb
+++ lib/bundler/dsl.rb
@@ -52,7 +52,8 @@ module Bundler
       when 1
         spec = Bundler.load_gemspec(gemspecs.first)
         raise InvalidOption, "There was an error loading the gemspec at #{gemspecs.first}." unless spec
-        gem spec.name, :path => path
+        opts ||= {}
+        gem spec.name, opts.merge(:path => path)
         group(development_group) do
           spec.development_dependencies.each do |dep|
             gem dep.name, *(dep.requirement.as_list + [:type => :development])
@@ -224,7 +225,7 @@ module Bundler
     end
 
     def valid_keys
-      @valid_keys ||= %w(group groups git path name branch ref tag require submodules platform platforms type source)
+      @valid_keys ||= %w(group groups git path glob name branch ref tag require submodules platform platforms type source)
     end
 
     def normalize_options(name, version, opts)
