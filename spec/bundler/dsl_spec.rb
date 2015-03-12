36af444a4806ddb34e05a4180008cd431c36e529
Samuel E. Giddins <segiddins@segiddins.me>
2015-03-12 09:46:30 -0700

    [DSL] Add prettier DSL error

--- spec/bundler/dsl_spec.rb
+++ spec/bundler/dsl_spec.rb
@@ -69,8 +69,7 @@ describe Bundler::Dsl do
       expect(Bundler).to receive(:read_file).with("Gemfile").
         and_return("unknown")
 
-      error_msg = "Undefined local variable or method `unknown'" \
-        " for Gemfile\\s+from Gemfile:1"
+      error_msg = "Invalid `Gemfile` file: Undefined local variable or method `unknown' for Gemfile. Bundler cannot continue."
       expect { subject.eval_gemfile("Gemfile") }.
         to raise_error(Bundler::GemfileError, Regexp.new(error_msg))
     end
@@ -80,7 +79,7 @@ describe Bundler::Dsl do
     it "handles syntax errors with a useful message" do
       expect(Bundler).to receive(:read_file).with("Gemfile").and_return("}")
       expect { subject.eval_gemfile("Gemfile") }.
-        to raise_error(Bundler::GemfileError, /Gemfile syntax error/)
+        to raise_error(Bundler::GemfileError, /Invalid `Gemfile` file: syntax error, unexpected tSTRING_DEND. Bundler cannot continue./)
     end
   end
 
@@ -177,7 +176,7 @@ describe Bundler::Dsl do
     it "will raise a Bundler::GemfileError" do
       gemfile "gem 'foo', :path => /unquoted/string/syntax/error"
       expect { Bundler::Dsl.evaluate(bundled_app("Gemfile"), nil, true) }.
-        to raise_error(Bundler::GemfileError, /Gemfile syntax error/)
+        to raise_error(Bundler::GemfileError, /Invalid `Gemfile` file: unknown regexp options - trg. Bundler cannot continue./)
     end
   end
 
@@ -185,7 +184,7 @@ describe Bundler::Dsl do
     it "will raise a Bundler::GemfileError" do
       gemfile "s = 'foo'.freeze; s.strip!"
       expect { Bundler::Dsl.evaluate(bundled_app("Gemfile"), nil, true) }.
-        to raise_error(Bundler::GemfileError, /There was an error in your Gemfile/)
+        to raise_error(Bundler::GemfileError, /Invalid `Gemfile` file: can't modify frozen String. Bundler cannot continue./)
     end
   end
 end
