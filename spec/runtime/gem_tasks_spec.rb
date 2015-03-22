61ec4e6461420e97a0b3c4e0bbe0a8fe2e2aff85
Samuel E. Giddins <segiddins@segiddins.me>
2015-03-22 00:06:08 -0700

    [GemTasks] Add a basic spec that the relevant tasks are installed

---
+++ spec/runtime/gem_tasks_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+describe "require 'bundler/gem_tasks'" do
+  before :each do
+    bundled_app("foo.gemspec").open("w") do |f|
+      f.write <<-GEMSPEC
+        Gem::Specification.new do |s|
+          s.name = "foo"
+        end
+      GEMSPEC
+    end
+    bundled_app("Rakefile").open("w") do |f|
+      f.write <<-RAKEFILE
+        $:.unshift("#{bundler_path}")
+        require "bundler/gem_tasks"
+      RAKEFILE
+    end
+  end
+
+  it "includes the relevant tasks" do
+    sys_exec "rake -T"
+    expect(err).to eq("")
+    expected_tasks = [
+      "rake build",
+      "rake install",
+      "rake release",
+    ]
+    tasks = out.lines.to_a.map { |s| s.split('#').first.strip }
+    expect(tasks & expected_tasks).to eq(expected_tasks)
+    expect(exitstatus).to eq(0) if exitstatus
+  end
+end
