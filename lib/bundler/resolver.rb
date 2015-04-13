80520abe6453b02ff3a20ceeaa91654cb1256265
sealocal <local.mat@gmail.com>
2015-04-13 13:37:00 -0700

    remove source_types conditional in Resolver

--- lib/bundler/resolver.rb
+++ lib/bundler/resolver.rb
@@ -323,12 +323,7 @@ module Bundler
               message << "Source does not contain any versions of '#{requirement}'"
             end
           else
-            message = "Could not find gem '#{requirement}' "
-            if Bundler.definition.has_rubygems_remotes?
-              message << "in any of the gem sources listed in your Gemfile or installed on this machine."
-            else
-              message << "in the gems available on this machine."
-            end
+            message = "Could not find gem '#{requirement}' in any of the gem sources listed in your Gemfile or installed on this machine."
           end
           raise GemNotFound, message
         end
