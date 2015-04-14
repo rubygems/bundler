1b7fe379d21b1c92e1490c9d5e74efc87dba9a93
Andre Arko <andre@arko.net>
2015-04-13 22:50:58 -0700

    gems can be available without being installed
    as in vendor/cache

--- lib/bundler/resolver.rb
+++ lib/bundler/resolver.rb
@@ -323,7 +323,8 @@ module Bundler
               message << "Source does not contain any versions of '#{requirement}'"
             end
           else
-            message = "Could not find gem '#{requirement}' in any of the gem sources listed in your Gemfile or installed on this machine."
+            message = "Could not find gem '#{requirement}' in any of the gem sources " \
+              "listed in your Gemfile or available on this machine."
           end
           raise GemNotFound, message
         end
