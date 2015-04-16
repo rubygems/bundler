159dc8eaa392995f326dc62ee8d3e239818df6fb
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-04-15 20:40:20 -0500

    updated tests

--- spec/install/parallel/spec_installation_spec.rb
+++ spec/install/parallel/spec_installation_spec.rb
@@ -3,9 +3,18 @@ require 'bundler/installer/parallel_installer'
 
 describe ParallelInstaller::SpecInstallation do
   describe "#ready_to_enqueue?" do
+
+    let!(:dep) do
+      a_spec = Object.new
+      def a_spec.name
+        "I like tests"
+      end
+      a_spec
+    end
+
     context "when in enqueued state" do
       it "is falsey" do
-        spec = ParallelInstaller::SpecInstallation.new
+        spec = ParallelInstaller::SpecInstallation.new(dep)
         spec.state = :enqueued
         expect(spec.ready_to_enqueue?).to be_falsey
       end
@@ -13,14 +22,14 @@ describe ParallelInstaller::SpecInstallation do
 
     context "when in installed state" do
       it "returns falsey" do
-        spec = ParallelInstaller::SpecInstallation.new
+        spec = ParallelInstaller::SpecInstallation.new(dep)
         spec.state = :installed
         expect(spec.ready_to_enqueue?).to be_falsey
       end
     end
 
     it "returns truthy" do
-      spec = ParallelInstaller::SpecInstallation.new
+      spec = ParallelInstaller::SpecInstallation.new(dep)
       expect(spec.ready_to_enqueue?).to be_truthy
     end
   end
