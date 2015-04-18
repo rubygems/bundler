8346be78d190970ab9832120b71625c8ca1bfdf2
Samuel E. Giddins <segiddins@segiddins.me>
2015-04-17 23:21:16 -0700

    [Setup] Print rescued exceptions to $stderr

--- lib/bundler/setup.rb
+++ lib/bundler/setup.rb
@@ -7,10 +7,10 @@ if Bundler::SharedHelpers.in_bundle?
     begin
       Bundler.setup
     rescue Bundler::BundlerError => e
-      puts "\e[31m#{e.message}\e[0m"
-      puts e.backtrace.join("\n") if ENV["DEBUG"]
+      $stderr.puts "\e[31m#{e.message}\e[0m"
+      $stderr.puts e.backtrace.join("\n") if ENV["DEBUG"]
       if e.is_a?(Bundler::GemNotFound)
-        puts "\e[33mRun `bundle install` to install missing gems.\e[0m"
+        $stderr.puts "\e[33mRun `bundle install` to install missing gems.\e[0m"
       end
       exit e.status_code
     end
