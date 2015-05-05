77bdd1e5126f6c629dbc16f9d73343ee58a57721
Samuel E. Giddins <segiddins@segiddins.me>
2015-05-04 19:54:32 -0700

    [SpecHelper] Reset settings

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -12,6 +12,7 @@ module Spec
       end
       FileUtils.mkdir_p(tmp)
       FileUtils.mkdir_p(home)
+      Bundler.send(:remove_instance_variable, :@settings) if Bundler.send(:instance_variable_defined?, :@settings)
     end
 
     attr_reader :out, :err, :exitstatus
