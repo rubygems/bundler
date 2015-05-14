54e416fd45fc47c10c46a20092af51b65e8eda3d
fotanus@gmail.com <Felipe Tanus>
2015-05-14 07:00:52 -0300

    Distinguish CI command runs
    This patch add CI information on the user_agent in order for us to track
    if a given command was run by a CI, as discussed in #3233.

--- spec/bundler/fetcher_spec.rb
+++ spec/bundler/fetcher_spec.rb
@@ -16,5 +16,21 @@ describe Bundler::Fetcher do
       expect(fetcher.user_agent).to match(/ruby\/(\d.)/)
       expect(fetcher.user_agent).to match(/options\/foo,bar/)
     end
+
+    describe "include CI information" do
+      it "from one CI" do
+        ENV["JENKINS_URL"] = "foo"
+        ci_part = fetcher.user_agent.split(' ').find{|x| x.match(/\Aci\//)}
+        expect(ci_part).to match("jenkins")
+      end
+
+      it "from many CI" do
+        ENV["TRAVIS"] = "foo"
+        ENV["CI_NAME"] = "my_ci"
+        ci_part = fetcher.user_agent.split(' ').find{|x| x.match(/\Aci\//)}
+        expect(ci_part).to match("travis")
+        expect(ci_part).to match("my_ci")
+      end
+    end
   end
 end
