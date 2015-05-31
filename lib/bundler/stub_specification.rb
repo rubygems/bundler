d46c78455b358aef295642d2421f3696fb365721
Andre Arko <andre@arko.net>
2015-05-30 18:23:13 -0700

    oh man :stuck_out_tongue_closed_eyes:

--- lib/bundler/stub_specification.rb
+++ lib/bundler/stub_specification.rb
@@ -1,21 +1,23 @@
 require 'bundler/remote_specification'
 
-class StubSpecification < RemoteSpecification
-  def self.from_stub(stub)
-    spec = new(stub.name, stub.version, stub.platform, nil)
-    spec.stub = stub
-    spec
-  end
+module Bundler
+  class StubSpecification < RemoteSpecification
+    def self.from_stub(stub)
+      spec = new(stub.name, stub.version, stub.platform, nil)
+      spec.stub = stub
+      spec
+    end
 
-  attr_accessor :stub
+    attr_accessor :stub
 
-  def to_yaml
-    _remote_specification.to_yaml
-  end
+    def to_yaml
+      _remote_specification.to_yaml
+    end
 
-private
+  private
 
-  def _remote_specification
-    stub.to_spec
+    def _remote_specification
+      stub.to_spec
+    end
   end
 end
