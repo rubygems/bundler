d96574b00922566fc2dffecf460393fb203e598b
Habib Alamin <ha.alamin@gmail.com>
2015-06-20 18:50:10 +0100

    Test that Bundler.setup_friendly prints friendly errors.

--- spec/runtime/setup_friendly_spec.rb
+++ spec/runtime/setup_friendly_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 
 describe "Bundler.setup_friendly" do
+  # install_gemfile in before(:each) block won't take an RSpec let argument,
+  # which I need, because the last example's Gemfile is different
   describe "with no arguments" do
     it "makes all groups available" do
       install_gemfile <<-G
@@ -32,6 +34,25 @@ describe "Bundler.setup_friendly" do
         Bundler.setup_friendly
       RUBY
       expect($LOAD_PATH.first).to eq(File.expand_path '../../../lib', __FILE__) # dunno
+    end
+
+    it "prints friendly errors" do
+      Dir.mktmpdir do |tmp_dir|
+        with_gem_path_as tmp_dir do
+          gemfile <<-G
+            source "file://#{gem_repo1}"
+            gem "rack", :group => :test
+          G
+
+          ruby <<-RUBY
+            require 'rubygems'
+            require 'bundler/setup_friendly'
+            Bundler.setup_friendly
+          RUBY
+          expect(out).to include('Could not find rack')
+          expect(out).to include('Run `bundle install` to install missing gems.')
+        end
+      end
     end
   end
 end
