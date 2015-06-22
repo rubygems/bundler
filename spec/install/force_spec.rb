30e6f8497c5ae83d5c1ab1e3e0cc1d525d67249a
Karlo <karlo@aelogica.com>
2015-06-22 18:23:17 +0800

    Add test to ensure that bundle install --force exits without error

--- spec/install/force_spec.rb
+++ spec/install/force_spec.rb
@@ -7,14 +7,15 @@ describe "bundle install" do
         source "file://#{gem_repo1}"
         gem "rack"
       G
-
-      bundle "install"
     end
 
     it "re-installs installed gems" do
+      bundle "install"
       bundle "install --force"
+
       expect(out).to include "Installing rack 1.0.0"
       should_be_installed "rack 1.0.0"
+      expect(exitstatus).to eq(0) if exitstatus
     end
   end
 end
