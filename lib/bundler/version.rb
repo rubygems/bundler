bb40b1d8a73dfa4d8b36de93f8ffcf63b0c135bf
Stefan Lance <stefan@lances.net>
2015-07-09 13:17:42 -0500

    Change version number to 1.99.0.dev

--- lib/bundler/version.rb
+++ lib/bundler/version.rb
@@ -2,5 +2,5 @@ module Bundler
   # We're doing this because we might write tests that deal
   # with other versions of bundler and we are unsure how to
   # handle this better.
-  VERSION = "1.10.4" unless defined?(::Bundler::VERSION)
+  VERSION = "1.99.0.dev" unless defined?(::Bundler::VERSION)
 end
