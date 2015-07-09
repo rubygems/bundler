29be03a8c77c127fe8693467c49f89c9814fc393
Daniel Teixeira <daniel.t.dt@gmail.com>
2015-07-09 17:42:52 -0300

    Fix install_if not clearing install conditions

--- spec/install/gems/install_if.rb
+++ spec/install/gems/install_if.rb
@@ -4,21 +4,26 @@ describe "bundle install with install_if conditionals" do
   it "follows the install_if DSL" do
     install_gemfile <<-G
       source "file://#{gem_repo1}"
-      gem "rack"
       install_if(lambda { true }) do
         gem "activesupport", "2.3.5"
       end
       gem "thin", :install_if => false
+      install_if(lambda { false }) do
+        gem "foo"
+      end
+      gem "rack"
     G
 
     should_be_installed("rack 1.0", "activesupport 2.3.5")
     should_not_be_installed("thin")
+    should_not_be_installed("foo")
 
     lockfile_should_be <<-L
       GEM
         remote: file:#{gem_repo1}/
         specs:
           activesupport (2.3.5)
+          foo (1.0)
           rack (1.0.0)
           thin (1.0)
             rack
@@ -28,6 +33,7 @@ describe "bundle install with install_if conditionals" do
 
       DEPENDENCIES
         activesupport (= 2.3.5)
+        foo
         rack
         thin
 
