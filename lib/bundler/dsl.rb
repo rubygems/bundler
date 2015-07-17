d535d398862376575c993b5201c61865d210df3f
Stefan Lance <stefan@lances.net>
2015-07-17 15:24:28 -0500

    Fix more conflict resolution errors

--- lib/bundler/dsl.rb
+++ lib/bundler/dsl.rb
@@ -224,9 +224,9 @@ module Bundler
 
       # TODO 2.0 remove this deprecated git source
       git_source(:bitbucket) do |repo_name|
-        warn_deprecated_git_source(:bitbucket, "https://#{user_name}@bitbucket.org/#{user_name}/#{repo_name}.git")
         user_name, repo_name = repo_name.split "/"
         repo_name ||= user_name
+        warn_deprecated_git_source(:bitbucket, "https://#{user_name}@bitbucket.org/#{user_name}/#{repo_name}.git")
         "https://#{user_name}@bitbucket.org/#{user_name}/#{repo_name}.git"
       end
     end
