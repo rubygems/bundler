b3977241e93bfefea0ad8e56c3852bee58bb2fcc
Rose Weixel <rose.weixel@gmail.com>
2015-07-26 14:54:31 -0400

    sanitize config[:test] to be boolean if 'false'

--- lib/bundler/cli/gem.rb
+++ lib/bundler/cli/gem.rb
@@ -162,8 +162,14 @@ module Bundler
       exit 1
     end
 
-    def ask_and_set_test_framework
+    def sanitized_test_framework
       test_framework = options[:test] || Bundler.settings["gem.test"]
+      
+      test_framework == "false" ? false : test_framework
+    end
+
+    def ask_and_set_test_framework
+      test_framework = sanitized_test_framework
 
       if test_framework.nil?
         Bundler.ui.confirm "Do you want to generate tests with your gem?"
