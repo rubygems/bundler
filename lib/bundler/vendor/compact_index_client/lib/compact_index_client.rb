e47768ef105dbfb7a8b0d1ac6fa9a8cfec1ac9d5
Samuel E. Giddins <segiddins@segiddins.me>
2015-08-19 00:31:56 -0600

    [CompactIndex] Use vendored compact_index_client

---
+++ lib/bundler/vendor/compact_index_client/lib/compact_index_client.rb
@@ -0,0 +1,57 @@
+require "pathname"
+require "set"
+
+class Bundler::CompactIndexClient
+  require "bundler/vendor/compact_index_client/lib/compact_index_client/cache"
+  require "bundler/vendor/compact_index_client/lib/compact_index_client/updater"
+  require "bundler/vendor/compact_index_client/lib/compact_index_client/version"
+
+  def initialize(directory, fetcher)
+    @directory = Pathname.new(directory)
+    @updater = Updater.new(fetcher)
+    @cache = Cache.new(@directory)
+    @endpoints = Set.new
+    @info_checksums_by_name = {}
+  end
+
+  def names
+    update(@cache.names_path, "names")
+    @cache.names
+  end
+
+  def versions
+    update(@cache.versions_path, "versions")
+    versions, @info_checksums_by_name = @cache.versions
+    versions
+  end
+
+  def dependencies(names)
+    names.each {|n| update_info(n) }
+    names.map do |name|
+      @cache.dependencies(name).map {|d| d.unshift(name) }
+    end.flatten(1)
+  end
+
+  def spec(name, version, platform = nil)
+    update_info(name)
+    @cache.specific_dependency(name, version, platform)
+  end
+
+private
+
+  def update(local_path, remote_path)
+    return if @endpoints.include?(remote_path)
+    @updater.update(local_path, url(remote_path))
+    @endpoints << remote_path
+  end
+
+  def update_info(name)
+    path = @cache.dependencies_path(name)
+    return if @info_checksums_by_name[name] == @updater.checksum_for_file(path)
+    update(path, "info/#{name}")
+  end
+
+  def url(path)
+    path
+  end
+end
---
+++ lib/bundler/vendor/compact_index_client/lib/compact_index_client/cache.rb
@@ -0,0 +1,80 @@
+class Bundler::CompactIndexClient
+  class Cache
+    attr_reader :directory
+
+    def initialize(directory)
+      @directory = Pathname.new(directory).expand_path
+      FileUtils.mkdir_p dependencies_path(nil)
+    end
+
+    def names
+      lines(names_path)
+    end
+
+    def names_path
+      directory + "names"
+    end
+
+    def versions
+      versions_by_name = Hash.new {|hash, key| hash[key] = [] }
+      info_checksums_by_name = {}
+      lines(versions_path).map do |line|
+        next if line == "-1"
+        name, versions_string, info_checksum = line.split(" ", 3)
+        info_checksums_by_name[name] = info_checksum || ""
+        versions_by_name[name].concat(versions_string.split(",").map! do |version|
+          version.split("-", 2).unshift(name)
+        end)
+      end
+      [versions_by_name, info_checksums_by_name]
+    end
+
+    def versions_path
+      directory + "versions"
+    end
+
+    def dependencies(name)
+      lines(dependencies_path(name)).map do |line|
+        parse_gem(line)
+      end
+    end
+
+    def dependencies_path(name)
+      directory + "dependencies" + name.to_s
+    end
+
+    def specific_dependency(name, version, platform)
+      pattern = [version, platform].compact.join("-")
+      matcher = /\A#{Regexp.escape(pattern)}\b/ unless pattern.empty?
+      lines(dependencies_path(name)).each do |line|
+        return parse_gem(line) if line =~ matcher
+      end if matcher
+      nil
+    end
+
+  private
+
+    def lines(path)
+      return [] unless path.file?
+      lines = path.read.lines
+      header = lines.index("---\n")
+      lines = header ? lines[header + 1..-1] : lines
+      lines.each!(&:strip!)
+    end
+
+    def parse_gem(string)
+      version_and_platform, rest = string.split(" ", 2)
+      version, platform = version_and_platform.split("-", 2)
+      dependencies, requirements = rest.split("|", 2).map {|s| s.split(",") } if rest
+      dependencies = dependencies ? dependencies.map {|d| parse_dependency(d) } : []
+      requirements = requirements ? requirements.map {|r| parse_dependency(r) } : []
+      [version, platform, dependencies, requirements]
+    end
+
+    def parse_dependency(string)
+      dependency = string.split(":")
+      dependency[-1] = dependency[-1].split("&")
+      dependency
+    end
+  end
+end
