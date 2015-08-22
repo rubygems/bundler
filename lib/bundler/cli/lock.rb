8d1a546957631a73e64e16da7c7678c8df7f892d
Victor Koronen <koronen@kth.se>
2015-08-22 14:24:21 +0200

    Fix Style/ClassAndModuleChildren

--- lib/bundler/cli/lock.rb
+++ lib/bundler/cli/lock.rb
@@ -1,35 +1,37 @@
 module Bundler
-  class CLI::Lock
-    attr_reader :options
+  class CLI
+    class Lock
+      attr_reader :options
 
-    def initialize(options)
-      @options = options
-    end
-
-    def run
-      unless Bundler.default_gemfile
-        Bundler.ui.error "Unable to find a Gemfile to lock"
-        exit 1
+      def initialize(options)
+        @options = options
       end
 
-      print = options[:print]
-      ui = Bundler.ui
-      Bundler.ui = UI::Silent.new if print
+      def run
+        unless Bundler.default_gemfile
+          Bundler.ui.error "Unable to find a Gemfile to lock"
+          exit 1
+        end
 
-      unlock = options[:update]
-      definition = Bundler.definition(unlock)
-      definition.resolve_remotely! unless options[:local]
+        print = options[:print]
+        ui = Bundler.ui
+        Bundler.ui = UI::Silent.new if print
 
-      if print
-        puts definition.to_lock
-      else
-        file = options[:lockfile]
-        file = file ? File.expand_path(file) : Bundler.default_lockfile
-        puts "Writing lockfile to #{file}"
-        definition.lock(file)
+        unlock = options[:update]
+        definition = Bundler.definition(unlock)
+        definition.resolve_remotely! unless options[:local]
+
+        if print
+          puts definition.to_lock
+        else
+          file = options[:lockfile]
+          file = file ? File.expand_path(file) : Bundler.default_lockfile
+          puts "Writing lockfile to #{file}"
+          definition.lock(file)
+        end
+
+        Bundler.ui = ui
       end
-
-      Bundler.ui = ui
     end
   end
 end
--- spec/support/hax.rb
+++ spec/support/hax.rb
@@ -1,7 +1,9 @@
 require "rubygems"
 
-class Gem::Platform
-  @local = new(ENV["BUNDLER_SPEC_PLATFORM"]) if ENV["BUNDLER_SPEC_PLATFORM"]
+module Gem
+  class Platform
+    @local = new(ENV["BUNDLER_SPEC_PLATFORM"]) if ENV["BUNDLER_SPEC_PLATFORM"]
+  end
 end
 
 if ENV["BUNDLER_SPEC_VERSION"]
