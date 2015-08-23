262df043970edbde8425e8e98a3de5c6757f25b6
Andre Arko <andre@arko.net>
2015-08-23 15:14:00 -0700

    stop requesting gzip with range, it breaks fastly
    :sob:

--- lib/bundler/vendor/compact_index_client/lib/compact_index_client/updater.rb
+++ lib/bundler/vendor/compact_index_client/lib/compact_index_client/updater.rb
@@ -5,11 +5,14 @@ class Bundler::CompactIndexClient
     end
 
     def update(local_path, remote_path)
-      headers = {"Accept-Encoding" => "gzip"}
+      headers = {}
 
       if local_path.file?
         headers["If-None-Match"] = checksum_for_file(local_path)
         headers["Range"] = "bytes=#{local_path.size}-"
+      else
+        # Fastly ignores Range when Accept-Encoding: gzip is set
+        headers["Accept-Encoding"] = "gzip"
       end
 
       response = @fetcher.call(remote_path, headers)
