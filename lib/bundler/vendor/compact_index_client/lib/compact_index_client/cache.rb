80d7d02cc7ed5af620ae89ad62cfd08349b4bf20
Andre Arko <andre@arko.net>
2015-08-23 03:51:16 -0700

    just pull out the map! into a variable

--- lib/bundler/vendor/compact_index_client/lib/compact_index_client/cache.rb
+++ lib/bundler/vendor/compact_index_client/lib/compact_index_client/cache.rb
@@ -19,12 +19,14 @@ class Bundler::CompactIndexClient
       versions_by_name = Hash.new {|hash, key| hash[key] = [] }
       info_checksums_by_name = {}
       lines(versions_path).each do |line|
-        next if line == "-1"
         name, versions_string, info_checksum = line.split(" ", 3)
+
         info_checksums_by_name[name] = info_checksum || ""
-        versions_by_name[name].concat(versions_string.split(",").map! do |version|
+
+        versions = versions_string.split(",").map! do |version|
           version.split("-", 2).unshift(name)
-        end)
+        end
+        versions_by_name[name].concat(versions)
       end
       [versions_by_name, info_checksums_by_name]
     end
