9f13bfa268287d1d5a61bc3e40a28c74fb3c038d
Andre Arko <andre@arko.net>
2015-08-23 15:13:37 -0700

    request gzipped gem info, ungzip if needed

--- lib/bundler/vendor/compact_index_client/lib/compact_index_client/updater.rb
+++ lib/bundler/vendor/compact_index_client/lib/compact_index_client/updater.rb
@@ -5,15 +5,24 @@ class Bundler::CompactIndexClient
     end
 
     def update(local_path, remote_path)
-      headers = {}
+      headers = {"Accept-Encoding" => "gzip"}
+
       if local_path.file?
         headers["If-None-Match"] = checksum_for_file(local_path)
         headers["Range"] = "bytes=#{local_path.size}-"
       end
+
       response = @fetcher.call(remote_path, headers)
       return if response.is_a?(Net::HTTPNotModified)
+
+      if response["Content-Encoding"] == "gzip"
+        content = Zlib::GzipReader.new(StringIO.new(response.body)).read
+      else
+        content = response.body
+      end
+
       mode = response.is_a?(Net::HTTPPartialContent) ? "a" : "w"
-      local_path.open(mode) {|f| f << response.body }
+      local_path.open(mode) {|f| f << content }
 
       return if checksum_for_file(local_path) == response["ETag"]
       local_path.delete
