128c0cd4f212511365cfd8290172f8c6871d5768
Lorenzo Manacorda <lorenzo.manacorda@gmail.com>
2015-09-16 12:05:32 +0200

    use `install_gemfile` helper in clean_spec

--- spec/commands/clean_spec.rb
+++ spec/commands/clean_spec.rb
@@ -333,24 +333,25 @@ describe "bundle clean" do
   end
 
   it "does not call clean automatically when using system gems" do
-    gemfile <<-G
+    bundle "config path.system true"
+
+    install_gemfile <<-G
       source "file://#{gem_repo1}"
 
       gem "thin"
       gem "rack"
     G
-    bundle "config path.system true"
-    bundle :install
+
     sys_exec "gem list"
     expect(out).to include("rack (1.0.0)")
     expect(out).to include("thin (1.0)")
 
-    gemfile <<-G
+    install_gemfile <<-G
       source "file://#{gem_repo1}"
 
       gem "rack"
     G
-    bundle :install
+
     sys_exec "gem list"
     expect(out).to include("rack (1.0.0)")
     expect(out).to include("thin (1.0)")
@@ -460,22 +461,21 @@ describe "bundle clean" do
   end
 
   it "cleans system gems when --force is used" do
-    gemfile <<-G
+    bundle "config path.system true"
+
+    install_gemfile <<-G
       source "file://#{gem_repo1}"
 
       gem "foo"
       gem "rack"
     G
 
-    bundle "config path.system true"
-    bundle "install"
-
-    gemfile <<-G
+    install_gemfile <<-G
       source "file://#{gem_repo1}"
 
       gem "rack"
     G
-    bundle "install"
+
     bundle "clean --force"
 
     expect(out).to include("Removing foo (1.0)")
@@ -525,13 +525,13 @@ describe "bundle clean" do
       end
     end
 
-    gemfile <<-G
+    bundle "config path.system true"
+
+    install_gemfile <<-G
       source "file://#{gem_repo2}"
 
       gem "bindir"
     G
-    bundle "config path.system true"
-    bundle :install
 
     bundle "clean --force"
 
