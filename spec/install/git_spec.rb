80cf688bab7ee3ced619c12ac5e2d6dfb0c78c38
Agis Anastasopoulos <agis.anast@gmail.com>
2015-10-25 12:19:56 +0200

    Include revision hash in Source::Git#to_s
    This effectively changes the output of commands that use Source::Git#to_s
    (ie. `bundle install`). For example:
    
        Using rack 1.0.0 from git@github.com:rack/rack.git (at master@574b147)
    
    Closes #3433.

---
+++ spec/install/git_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe "bundle install" do
+  context "git sources" do
+    it "displays the revision hash of the gem repository" do
+      build_git "foo", "1.0", :path => lib_path("foo")
+
+      install_gemfile <<-G
+        gem "foo", :git => "#{lib_path("foo")}"
+      G
+
+      bundle :install
+      expect(out).to include("Using foo 1.0 from #{lib_path("foo")} (at master@#{revision_for(lib_path("foo"))[0..6]})")
+      should_be_installed "foo 1.0"
+    end
+  end
+end
