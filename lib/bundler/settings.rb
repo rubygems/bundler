3c748989b2be50e97655d5af855b46b44699e6b4
Samuel Giddins <segiddins@segiddins.me>
2015-12-15 17:51:10 -0800

    [Settings] Use filesystem_access block to read config file

--- lib/bundler/settings.rb
+++ lib/bundler/settings.rb
@@ -224,17 +224,15 @@ module Bundler
     end
 
     def load_config(config_file)
-      valid_file = config_file && config_file.exist? && !config_file.size.zero?
-      if !ignore_config? && valid_file
+      SharedHelpers.filesystem_access(config_file, :read) do
+        valid_file = config_file && config_file.exist? && !config_file.size.zero?
+        return {} if ignore_config? || !valid_file
         config_regex = /^(BUNDLE_.+): (['"]?)(.*(?:\n(?!BUNDLE).+)?)\2$/
-        raise PermissionError.new(config_file, :read) unless config_file.readable?
         config_pairs = config_file.read.scan(config_regex).map do |m|
           key, _, value = m
           [convert_to_backward_compatible_key(key), value.gsub(/\s+/, " ").tr('"', "'")]
         end
         Hash[config_pairs]
-      else
-        {}
       end
     end
 
