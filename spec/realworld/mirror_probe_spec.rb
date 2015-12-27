3a015c685233ec765551934bd9f1f9504c56fa46
Pablo Carranza <pcarranza@gmail.com>
2015-12-27 22:47:20 +0100

    Add realworld specs for probing mirrors failing and falling back, all and specific

--- spec/realworld/mirror_probe_spec.rb
+++ spec/realworld/mirror_probe_spec.rb
@@ -9,25 +9,92 @@ describe "fetching dependencies with a not available mirror" do
     require_rack
     setup_mirror
     setup_server
-    global_config("BUNDLE_MIRROR__HTTP://127__0__0__1:#{@server_port}/__FALLBACK_TIMEOUT/" => "true",
-                  "BUNDLE_MIRROR__HTTP://127__0__0__1:#{@server_port}/" => @mirror_uri)
   end
 
   after do
     @server_thread.kill
   end
 
-  it "install a gem using the original uri when the mirror is not responding" do
-    gemfile <<-G
-      source "#{original}"
-      gem 'weakling'
-    G
+  context "with a specific fallback timeout" do
+    before do
+      global_config("BUNDLE_MIRROR__HTTP://127__0__0__1:#{@server_port}/__FALLBACK_TIMEOUT/" => "true",
+                    "BUNDLE_MIRROR__HTTP://127__0__0__1:#{@server_port}/" => @mirror_uri)
+    end
 
-    bundle :install
+    it "install a gem using the original uri when the mirror is not responding" do
+      gemfile <<-G
+        source "#{original}"
+        gem 'weakling'
+      G
 
-    expect(out).to include("Installing weakling")
-    expect(out).to include("Bundle complete")
-    should_be_installed "weakling 0.0.3"
+      bundle :install
+
+      expect(out).to include("Installing weakling")
+      expect(out).to include("Bundle complete")
+      should_be_installed "weakling 0.0.3"
+    end
+  end
+
+  context "with a global fallback timeout" do
+    before do
+      global_config("BUNDLE_MIRROR__ALL__FALLBACK_TIMEOUT/" => "1",
+                    "BUNDLE_MIRROR__ALL" => @mirror_uri)
+    end
+
+    it "install a gem using the original uri when the mirror is not responding" do
+      gemfile <<-G
+        source "#{original}"
+        gem 'weakling'
+      G
+
+      bundle :install
+
+      expect(out).to include("Installing weakling")
+      expect(out).to include("Bundle complete")
+      should_be_installed "weakling 0.0.3"
+    end
+  end
+
+  context "with a specific mirror without a fallback timeout" do
+    before do
+      global_config("BUNDLE_MIRROR__HTTP://127__0__0__1:#{@server_port}/" => @mirror_uri)
+    end
+
+    it "fails to install the gem with a timeout error" do
+      gemfile <<-G
+        source "#{original}"
+        gem 'weakling'
+      G
+
+      bundle :install
+
+      expect(out).to include("Fetching source index from #{@mirror_uri}")
+      expect(out).to include("Retrying fetcher due to error (2/4): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}")
+      expect(out).to include("Retrying fetcher due to error (3/4): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}")
+      expect(out).to include("Retrying fetcher due to error (4/4): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}")
+      expect(out).to include("Could not fetch specs from #{@mirror_uri}")
+    end
+  end
+
+  context "with a global mirror without a fallback timeout" do
+    before do
+      global_config("BUNDLE_MIRROR__ALL" => @mirror_uri)
+    end
+
+    it "fails to install the gem with a timeout error" do
+      gemfile <<-G
+        source "#{original}"
+        gem 'weakling'
+      G
+
+      bundle :install
+
+      expect(out).to include("Fetching source index from #{@mirror_uri}")
+      expect(out).to include("Retrying fetcher due to error (2/4): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}")
+      expect(out).to include("Retrying fetcher due to error (3/4): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}")
+      expect(out).to include("Retrying fetcher due to error (4/4): Bundler::HTTPError Could not fetch specs from #{@mirror_uri}")
+      expect(out).to include("Could not fetch specs from #{@mirror_uri}")
+    end
   end
 
   def setup_server
