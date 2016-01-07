acddc552742ffd3f06a2191c58e90fbadb9c2e98
James Wen <jrw2175@columbia.edu>
2016-01-06 23:41:06 -0500

    Add unit tests for `Bundler::SharedHelpers#default_gemfile`

--- spec/bundler/shared_helpers_spec.rb
+++ spec/bundler/shared_helpers_spec.rb
@@ -6,6 +6,26 @@ module TargetNamespace
 end
 
 describe Bundler::SharedHelpers do
+  describe "#default_gemfile" do
+    subject { Bundler::SharedHelpers.default_gemfile }
+    before do
+      ENV["BUNDLE_GEMFILE"] = "/path/Gemfile"
+    end
+    context "Gemfile is present" do
+      it "returns the Gemfile path" do
+        expected_gemfile_path = Pathname.new("/path/Gemfile")
+        expect(subject).to eq(expected_gemfile_path)
+      end
+    end
+    context "Gemfile is not present" do
+      before do
+        ENV["BUNDLE_GEMFILE"] = nil
+      end
+      it "raises a GemfileNotFound error" do
+        expect { subject }.to raise_error(Bundler::GemfileNotFound, "Could not locate Gemfile")
+      end
+    end
+  end
   describe "#const_get_safely" do
     context "when the namespace does have the requested constant" do
       subject { Bundler::SharedHelpers.const_get_safely(:VALID_CONSTANT, TargetNamespace) }
