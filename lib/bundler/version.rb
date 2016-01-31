8910f4ba65628dbfe97387662468dc20ec1521e0
Andre Arko <andre@arko.net>
2016-01-31 14:09:33 -0800

    un-freeze the version string for Ruby 1.9.3
    turns out that frozen versions cause RubyGems to explode, but only on
    Ruby 1.9.3. ¯\_(ツ)_/¯

--- lib/bundler/version.rb
+++ lib/bundler/version.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
+
+# Ruby 1.9.3 and old RubyGems don't play nice with frozen version strings
+# rubocop:disable MutableConstant
+
 module Bundler
   # We're doing this because we might write tests that deal
   # with other versions of bundler and we are unsure how to
   # handle this better.
-  VERSION = "1.12.0.pre".freeze unless defined?(::Bundler::VERSION)
+  VERSION = "1.12.0.pre" unless defined?(::Bundler::VERSION)
 end
