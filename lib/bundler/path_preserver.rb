6f2e4461f7addaf1044df7ed87b2f1ad7092a35d
Samuel Giddins <segiddins@segiddins.me>
2016-01-31 20:25:49 -0600

    [PathPreserver] Use the passed-in env

--- lib/bundler/path_preserver.rb
+++ lib/bundler/path_preserver.rb
@@ -1,11 +1,11 @@
 module Bundler
   module PathPreserver
-    def self.preserve_path_in_environment(env_var, env = ENV)
+    def self.preserve_path_in_environment(env_var, env)
       original_env_var      = "_ORIGINAL_#{env_var}"
-      original_path         = ENV[original_env_var]
-      path                  = ENV[env_var]
-      ENV[original_env_var] = path if original_path.nil? || original_path.empty?
-      ENV[env_var]          = original_path if path.nil? || path.empty?
+      original_path         = env[original_env_var]
+      path                  = env[env_var]
+      env[original_env_var] = path if original_path.nil? || original_path.empty?
+      env[env_var]          = original_path if path.nil? || path.empty?
     end
   end
 end
