34d896710a9445ebeb793185acf218f369c76db6
Samuel Giddins <segiddins@segiddins.me>
2016-02-03 22:35:05 -0600

    [Exec] Load instead of exec-ing

--- lib/bundler/cli/exec.rb
+++ lib/bundler/cli/exec.rb
@@ -18,16 +18,30 @@ module Bundler
     end
 
     def run
-      ui = Bundler.ui
-      raise ArgumentError if cmd.nil?
-
+      validate_cmd!
       SharedHelpers.set_bundle_environment
-      bin_path = Bundler.which(@cmd)
+      if bin_path = Bundler.which(cmd)
+        kernel_load(bin_path, *args) && return if ruby_shebang?(bin_path)
+        # First, try to exec directly to something in PATH
+        kernel_exec([bin_path, cmd], *args)
+      else
+        # Just exec using the given command
+        kernel_exec(cmd, *args)
+      end
+    end
+
+  private
+
+    def validate_cmd!
+      return unless cmd.nil?
+      Bundler.ui.error "bundler: exec needs a command to run"
+      exit 128
+    end
+
+    def kernel_exec(*args)
+      ui = Bundler.ui
       Bundler.ui = nil
-      # First, try to exec directly to something in PATH
-      Kernel.exec([bin_path, @cmd], *args) if bin_path
-      # Just exec using the given command
-      Kernel.exec(@cmd, *args)
+      Kernel.exec(*args)
     rescue Errno::EACCES, Errno::ENOEXEC
       Bundler.ui = ui
       Bundler.ui.error "bundler: not executable: #{cmd}"
@@ -37,10 +51,27 @@ module Bundler
       Bundler.ui.error "bundler: command not found: #{cmd}"
       Bundler.ui.warn "Install missing gem executables with `bundle install`"
       exit 127
-    rescue ArgumentError
+    end
+
+    def kernel_load(file, *args)
+      args.pop if args.last.is_a?(Hash)
+      ARGV.replace(args)
+      ui = Bundler.ui
+      Bundler.ui = nil
+      require "bundler/setup"
+      Kernel.load(file)
+    rescue SystemExit
+      raise
+    rescue Exception => e # rubocop:disable Lint/RescueException
       Bundler.ui = ui
-      Bundler.ui.error "bundler: exec needs a command to run"
-      exit 128
+      Bundler.ui.error "bundler: failed to load command: #{cmd} (#{file})"
+      Bundler.ui.trace(e, nil, true)
+      exit 125
+    end
+
+    def ruby_shebang?(file)
+      first_line = File.open(file, "rb", &:readline)
+      first_line == "#!/usr/bin/env ruby\n" || first_line == "#!#{Gem.ruby}\n"
     end
   end
 end
