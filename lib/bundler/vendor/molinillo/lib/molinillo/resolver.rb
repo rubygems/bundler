3e542b92cedbbcb4f3eb9453eece0811cf82bf21
Samuel Giddins <segiddins@segiddins.me>
2016-02-17 23:34:14 -0600

    [Resolver] Update Molinillo to 0.4.3

--- lib/bundler/vendor/molinillo/lib/molinillo/resolver.rb
+++ lib/bundler/vendor/molinillo/lib/molinillo/resolver.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'bundler/vendor/molinillo/lib/molinillo/dependency_graph'
 
 module Bundler::Molinillo
--- lib/bundler/vendor/molinillo/lib/molinillo/state.rb
+++ lib/bundler/vendor/molinillo/lib/molinillo/state.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Bundler::Molinillo
   # A state that a {Resolution} can be in
   # @attr [String] name the name of the current requirement
