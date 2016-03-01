f8d07b1446a8e2e42468a2676b9fe47d9f3d1089
Samuel Giddins <segiddins@segiddins.me>
2016-02-29 20:56:51 -0600

    Fix which to compare quotes correctly on 1.8.7

--- lib/bundler.rb
+++ lib/bundler.rb
@@ -304,8 +304,9 @@ module Bundler
       if File.file?(executable) && File.executable?(executable)
         executable
       elsif paths = ENV["PATH"]
+        quote = '"'.freeze
         paths.split(File::PATH_SEPARATOR).find do |path|
-          path = path[1..-2] if path[0] == '"' && path[-1] == '"'
+          path = path[1..-2] if path.start_with?(quote) && path.end_with?(quote)
           executable_path = File.expand_path(executable, path)
           return executable_path if File.file?(executable_path) && File.executable?(executable_path)
         end
