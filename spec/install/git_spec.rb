4114f81b3148c9c106e8c164acd7f6c990cdb1ce
Asutosh Palai <asupalai@gmail.com>
2016-03-11 17:53:24 +0530

    Added failing test for #3981

--- spec/install/git_spec.rb
+++ spec/install/git_spec.rb
@@ -14,5 +14,30 @@ describe "bundle install" do
       expect(out).to include("Using foo 1.0 from #{lib_path("foo")} (at master@#{revision_for(lib_path("foo"))[0..6]})")
       should_be_installed "foo 1.0"
     end
+
+    it "should check out git repos that are missing but not being installed" do
+      build_git "foo"
+
+      gemfile <<-G
+        gem "foo", :git =>"file://#{lib_path("foo-1.0")}", group: :development
+      G
+
+      lockfile <<-L
+        GIT
+          remote: file://#{lib_path("foo-1.0")}
+          specs:
+            foo (1.0)
+
+        PLATFORMS
+          ruby
+
+        DEPENDENCIES
+          foo!
+      L
+
+      bundle "install --path=vendor/bundle --without development"
+
+      expect(out).to include("Bundle complete!")
+    end
   end
 end
