37766ab7e9683fa4a18bad601d84d0e1beb1235c
Andre Arko <andre@arko.net>
2016-03-17 21:49:00 -0700

    stop requiring that we have every rubygems cert
    RubyGems seems to be full of legacy certs, we should clean them up
    sometime.

--- spec/other/ssl_cert_spec.rb
+++ spec/other/ssl_cert_spec.rb
@@ -3,12 +3,6 @@ require "spec_helper"
 require "bundler/ssl_certs/certificate_manager"
 
 describe "SSL Certificates", :rubygems_master do
-  it "are up to date with Rubygems" do
-    rubygems = File.expand_path("../../../tmp/rubygems", __FILE__)
-    manager = Bundler::SSLCerts::CertificateManager.new(rubygems)
-    expect(manager).to be_up_to_date
-  end
-
   hosts = %w(
     rubygems.org
     index.rubygems.org
