5f5b8e864893aec3ceeb9e340334abedd79f6cff
Homu <homu@barosl.com>
2016-04-29 14:06:10 +0900

    Auto merge of #4477 - bundler:seg-checksum-mismatch-error, r=indirect
    [Updater] Raise a more helpful error on checksum mismatch
    
    @indirect this will make diagnosing https://github.com/bundler/bundler/issues/4472 much easier.

--- lib/bundler/vendor/compact_index_client/lib/compact_index_client/updater.rb
+++ lib/bundler/vendor/compact_index_client/lib/compact_index_client/updater.rb
@@ -1,9 +1,21 @@
+# frozen_string_literal: true
 require "stringio"
 require "zlib"
 
 class Bundler::CompactIndexClient
   class Updater
-    class MisMatchedChecksumError < Error; end
+    class MisMatchedChecksumError < Error
+      def initialize(path, server_checksum, local_checksum)
+        @path = path
+        @server_checksum = server_checksum
+        @local_checksum = local_checksum
+      end
+
+      def message
+        "The checksum of /#{@path} does not match the checksum provided by the server! Something is wrong " \
+          "(local checksum is #{@local_checksum.inspect}, was expecting #{@server_checksum.inspect})."
+      end
+    end
 
     def initialize(fetcher)
       @fetcher = fetcher
@@ -31,20 +43,20 @@ class Bundler::CompactIndexClient
       mode = response.is_a?(Net::HTTPPartialContent) ? "a" : "w"
       local_path.open(mode) {|f| f << content }
 
-      return if etag_for(local_path) == response["ETag"]
+      response_etag = response["ETag"]
+      return if etag_for(local_path) == response_etag
 
       if retrying.nil?
         local_path.delete
         update(local_path, remote_path, :retrying)
       else
-        raise MisMatchedChecksumError, "Checksum of /#{remote_path} " \
-          "does not match the checksum provided by server! Something is wrong."
+        raise MisMatchedChecksumError.new(remote_path, response_etag, etag_for(local_path))
       end
     end
 
     def etag_for(path)
       sum = checksum_for_file(path)
-      sum ? '"' << sum << '"' : nil
+      sum ? %("#{sum}") : nil
     end
 
     def checksum_for_file(path)
