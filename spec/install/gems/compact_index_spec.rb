60463698918eb63d45dd21cabe50864ac4a9f142
Andre Arko <andre@arko.net>
2016-05-31 13:54:13 -0700

    ambiguous gems are no longer possible (we hope)

--- spec/install/gems/compact_index_spec.rb
+++ spec/install/gems/compact_index_spec.rb
@@ -448,19 +448,6 @@ The checksum of /versions does not match the checksum provided by the server! So
       expect(out).not_to include("#{user}:#{password}")
     end
 
-    it "strips http basic auth creds when warning about ambiguous sources" do
-      gemfile <<-G
-        source "#{basic_auth_source_uri}"
-        source "file://#{gem_repo1}"
-        gem "rack"
-      G
-
-      bundle! :install, :artifice => "compact_index_basic_authentication"
-      expect(out).to include("Warning: the gem 'rack' was found in multiple sources.")
-      expect(out).not_to include("#{user}:#{password}")
-      should_be_installed "rack 1.0.0"
-    end
-
     it "does not pass the user / password to different hosts on redirect" do
       gemfile <<-G
         source "#{basic_auth_source_uri}"
