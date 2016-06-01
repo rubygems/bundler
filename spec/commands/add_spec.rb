7b4fa7bfd2798361c11b8dd9699008d408672330
Boggs <hello.boggs@gmail.com>
2016-06-01 16:46:34 +0800

    Fix rubocop offenses

--- spec/commands/add_spec.rb
+++ spec/commands/add_spec.rb
@@ -1,21 +1,22 @@
-require 'spec_helper'
+# frozen_string_literal: true
+require "spec_helper"
 
 describe "bundle add" do
   before :each do
     gemfile
   end
 
-  context 'when version number is set' do
-    it 'adds gem with provided version' do
-      bundle 'add foo 1.2.3'
-      expect(bundled_app('Gemfile').read).to match(/gem 'foo', '~> 1.2'/)
+  context "when version number is set" do
+    it "adds gem with provided version" do
+      bundle "add foo 1.2.3"
+      expect(bundled_app("Gemfile").read).to match(/gem "foo", "~> 1.2"/)
     end
   end
 
-  context 'when version number is not set' do
-    it 'adds gem with last stable version' do
-      bundle 'add foobar', fakeweb: 'rubygems_api'
-      expect(bundled_app('Gemfile').read).to match(/gem 'foobar', '~> 1.2'/)
+  context "when version number is not set" do
+    it "adds gem with last stable version" do
+      bundle "add foobar", :fakeweb => "rubygems_api"
+      expect(bundled_app("Gemfile").read).to match(/gem "foobar", "~> 1.2"/)
     end
   end
 end
--- spec/support/fakeweb/rubygems_api.rb
+++ spec/support/fakeweb/rubygems_api.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
 require File.expand_path("../../path.rb", __FILE__)
 include Spec::Path
 
-$LOAD_PATH.unshift "#{Dir[base_system_gems.join("gems/fakeweb*/lib")].first}"
-require 'fakeweb'
+$LOAD_PATH.unshift Dir[base_system_gems.join("gems/fakeweb*/lib")].first.to_s
+require "fakeweb"
 
 FakeWeb.allow_net_connect = false
 
-FakeWeb.register_uri(:get, 'https://rubygems.org/api/v1/gems/foobar.yaml',
+FakeWeb.register_uri(:get, "https://rubygems.org/api/v1/gems/foobar.yaml",
   :body => "---\nversion: 1.2.3")
