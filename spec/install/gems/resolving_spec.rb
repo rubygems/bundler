b5c038822229813f01f685afc2e51e7c9ad643be
Andre Arko <andre@arko.net>
2016-06-06 23:21:44 -0700

    test for a diagnostic error on ruby version fail

--- spec/install/gems/resolving_spec.rb
+++ spec/install/gems/resolving_spec.rb
@@ -119,7 +119,7 @@ describe "bundle install with install-time dependencies" do
     end
 
     context "allows no gems" do
-      it "does not try to install those gems" do
+      it "raises an error during resolution" do
         build_repo2 do
           build_gem "require_ruby" do |s|
             s.required_ruby_version = "> 9000"
@@ -128,11 +128,23 @@ describe "bundle install with install-time dependencies" do
 
         install_gemfile <<-G
           source "file://#{gem_repo2}"
+          ruby "#{RUBY_VERSION}"
           gem 'require_ruby'
         G
 
         expect(out).to_not include("Gem::InstallError: require_ruby requires Ruby version > 9000")
-        expect(out).to include("require_ruby-1.0 requires ruby version > 9000, which is incompatible with the current version, #{Bundler::RubyVersion.system}")
+
+        nice_error = <<-E.strip.gsub(/^ {8}/, "")
+          Fetching source index from file:#{gem_repo1}/
+          Resolving dependencies...
+          Bundler could not find compatible versions for gem "require_ruby":
+            In Gemfile:
+              ruby (= #{RUBY_VERSION})
+
+              require_ruby was resolved to 1.0, which depends on
+                ruby (> 9000)
+        E
+        expect(out).to eq(nice_error)
       end
     end
   end
