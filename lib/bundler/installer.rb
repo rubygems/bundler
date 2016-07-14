213704cb13b9b6c1a3029df31edd58777ef7f4ef
Colby Swandale <colby@taplaboratories.com.au>
2016-07-15 08:40:50 +1000

    refactor file conflict error message, removing `same`

--- lib/bundler/installer.rb
+++ lib/bundler/installer.rb
@@ -196,7 +196,7 @@ module Bundler
       end unless Bundler.bundle_path.exist?
     rescue Errno::EEXIST
       raise PathError, "Could not install to path `#{Bundler.settings[:path]}` " \
-        "because a file already exists at that same path. Either remove or rename the file so the directory can be created."
+        "because a file already exists at that path. Either remove or rename the file so the directory can be created."
     end
 
     def resolve_if_need(options)
