70bc65cf41f2db1e9477e5e49dc265ab8f4f6422
Samuel Giddins <segiddins@segiddins.me>
2016-07-27 12:08:37 -0500

    Remove tabs from the dot output in the viz spec

--- spec/commands/viz_spec.rb
+++ spec/commands/viz_spec.rb
@@ -24,43 +24,43 @@ describe "bundle viz", :ruby => "1.9.3", :if => Bundler.which("dot") do
     bundle! "viz", :format => "dot"
     expect(bundled_app("gem_graph.dot")).to read_as(strip_whitespace(<<-DOT))
       digraph Gemfile {
-      	graph [bb="0,0,217.82,108",
-      		concentrate=true,
-      		nodesep=0.55,
-      		normalize=true
-      	];
-      	node [fontname="Arial, Helvetica, SansSerif",
-      		label="\\N"
-      	];
-      	edge [fontname="Arial, Helvetica, SansSerif",
-      		fontsize=12,
-      		weight=2
-      	];
-      	default	 [fillcolor="#B9B9D5",
-      		fontsize=16,
-      		height=0.5,
-      		label=default,
-      		pos="32.02,90",
-      		shape=box3d,
-      		style=filled,
-      		width=0.88943];
-      	rack	 [fillcolor="#B9B9D5",
-      		height=0.5,
-      		label=rack,
-      		pos="161.02,18",
-      		style=filled,
-      		width=0.75];
-      	default -> rack	 [constraint=false,
-      		pos="e,140.64,30.056 63.576,71.876 84.405,60.574 111.5,45.872 131.83,34.841"];
-      	"rack-obama"	 [fillcolor="#B9B9D5",
-      		height=0.5,
-      		label="rack-obama",
-      		pos="161.02,90",
-      		style=filled,
-      		width=1.5777];
-      	default -> "rack-obama"	 [constraint=false,
-      		pos="e,103.95,90 64.27,90 74.087,90 83.904,90 93.721,90"];
-      	"rack-obama" -> rack	 [pos="e,161.02,36.104 161.02,71.697 161.02,63.983 161.02,54.712 161.02,46.112"];
+      \tgraph [bb="0,0,217.82,108",
+      \t\tconcentrate=true,
+      \t\tnodesep=0.55,
+      \t\tnormalize=true
+      \t];
+      \tnode [fontname="Arial, Helvetica, SansSerif",
+      \t\tlabel="\\N"
+      \t];
+      \tedge [fontname="Arial, Helvetica, SansSerif",
+      \t\tfontsize=12,
+      \t\tweight=2
+      \t];
+      \tdefault\t [fillcolor="#B9B9D5",
+      \t\tfontsize=16,
+      \t\theight=0.5,
+      \t\tlabel=default,
+      \t\tpos="32.02,90",
+      \t\tshape=box3d,
+      \t\tstyle=filled,
+      \t\twidth=0.88943];
+      \track\t [fillcolor="#B9B9D5",
+      \t\theight=0.5,
+      \t\tlabel=rack,
+      \t\tpos="161.02,18",
+      \t\tstyle=filled,
+      \t\twidth=0.75];
+      \tdefault -> rack\t [constraint=false,
+      \t\tpos="e,140.64,30.056 63.576,71.876 84.405,60.574 111.5,45.872 131.83,34.841"];
+      \t"rack-obama"\t [fillcolor="#B9B9D5",
+      \t\theight=0.5,
+      \t\tlabel="rack-obama",
+      \t\tpos="161.02,90",
+      \t\tstyle=filled,
+      \t\twidth=1.5777];
+      \tdefault -> "rack-obama"\t [constraint=false,
+      \t\tpos="e,103.95,90 64.27,90 74.087,90 83.904,90 93.721,90"];
+      \t"rack-obama" -> rack\t [pos="e,161.02,36.104 161.02,71.697 161.02,63.983 161.02,54.712 161.02,46.112"];
       }
     DOT
   end
