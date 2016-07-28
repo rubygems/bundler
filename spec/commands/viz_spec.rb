ac494b51e21cab23eac08b1b24b25a116a9214bc
Samuel Giddins <segiddins@segiddins.me>
2016-07-28 09:32:33 -0500

    Change viz spec to plain output
    Hopefully this will be consistent between machines

--- spec/commands/viz_spec.rb
+++ spec/commands/viz_spec.rb
@@ -21,47 +21,23 @@ describe "bundle viz", :ruby => "1.9.3", :if => Bundler.which("dot") do
     bundle! "viz"
     expect(out).to include("gem_graph.png")
 
-    bundle! "viz", :format => "dot"
-    expect(bundled_app("gem_graph.dot")).to read_as(strip_whitespace(<<-DOT))
+    bundle! "viz", :format => "debug"
+    expect(out).to eq(strip_whitespace(<<-DOT).strip)
       digraph Gemfile {
-      \tgraph [bb="0,0,217.82,108",
-      \t\tconcentrate=true,
-      \t\tnodesep=0.55,
-      \t\tnormalize=true
-      \t];
-      \tnode [fontname="Arial, Helvetica, SansSerif",
-      \t\tlabel="\\N"
-      \t];
-      \tedge [fontname="Arial, Helvetica, SansSerif",
-      \t\tfontsize=12,
-      \t\tweight=2
-      \t];
-      \tdefault\t [fillcolor="#B9B9D5",
-      \t\tfontsize=16,
-      \t\theight=0.5,
-      \t\tlabel=default,
-      \t\tpos="32.02,90",
-      \t\tshape=box3d,
-      \t\tstyle=filled,
-      \t\twidth=0.88943];
-      \track\t [fillcolor="#B9B9D5",
-      \t\theight=0.5,
-      \t\tlabel=rack,
-      \t\tpos="161.02,18",
-      \t\tstyle=filled,
-      \t\twidth=0.75];
-      \tdefault -> rack\t [constraint=false,
-      \t\tpos="e,140.64,30.056 63.576,71.876 84.405,60.574 111.5,45.872 131.83,34.841"];
-      \t"rack-obama"\t [fillcolor="#B9B9D5",
-      \t\theight=0.5,
-      \t\tlabel="rack-obama",
-      \t\tpos="161.02,90",
-      \t\tstyle=filled,
-      \t\twidth=1.5777];
-      \tdefault -> "rack-obama"\t [constraint=false,
-      \t\tpos="e,103.95,90 64.27,90 74.087,90 83.904,90 93.721,90"];
-      \t"rack-obama" -> rack\t [pos="e,161.02,36.104 161.02,71.697 161.02,63.983 161.02,54.712 161.02,46.112"];
+      concentrate = "true";
+      normalize = "true";
+      nodesep = "0.55";
+      edge[ weight  =  "2"];
+      node[ fontname  =  "Arial, Helvetica, SansSerif"];
+      edge[ fontname  =  "Arial, Helvetica, SansSerif" , fontsize  =  "12"];
+      default [style = "filled", fillcolor = "#B9B9D5", shape = "box3d", fontsize = "16", label = "default"];
+      rack [style = "filled", fillcolor = "#B9B9D5", label = "rack"];
+        default -> rack [constraint = "false"];
+      "rack-obama" [style = "filled", fillcolor = "#B9B9D5", label = "rack-obama"];
+        default -> "rack-obama" [constraint = "false"];
+        "rack-obama" -> rack;
       }
+      debugging bundle viz...
     DOT
   end
 
