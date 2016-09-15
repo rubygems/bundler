718a6d8a54127f05bb5128280209cfcded2a1390
JuanitoFatas <katehuang0320@gmail.com>
2016-09-15 17:20:00 +0800

    Use Hashrocket => because we support Ruby 1.8.7

--- spec/install/gemfile_spec.rb
+++ spec/install/gemfile_spec.rb
@@ -73,7 +73,7 @@ describe "bundle install" do
         simulate_ruby_engine "jruby", "9.1.2.0" do
           install_gemfile! <<-G
             source "https://rubygems.org"
-            ruby "2.3.0", :engine => :jruby, engine_version: "9.1.2.0"
+            ruby "2.3.0", :engine => :jruby, :engine_version => "9.1.2.0"
           G
 
           expect(out).to match(/Bundle complete!/)
@@ -86,7 +86,7 @@ describe "bundle install" do
         simulate_ruby_engine "jruby", "9.1.2.0" do
           install_gemfile! <<-G
             source "file://#{gem_repo1}"
-            ruby "2.3.0", :engine => :jruby, engine_version: "9.1.2.0"
+            ruby "2.3.0", :engine => :jruby, :engine_version => "9.1.2.0"
             gem "rack"
           G
 
