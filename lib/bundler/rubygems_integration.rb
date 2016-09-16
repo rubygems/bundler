c45389622ed9a893a5851c251d5a5e97e6d499bc
Homu <homu@barosl.com>
2016-09-16 21:29:36 +0900

    Auto merge of #4983 - bundler:seg-redefine-method-visibility, r=indirect
    [RubygemsIntegration] Ensure redefined methods have the same visibility
    
    Closes #4975
    
    \c @indirect @headius
    
    I'd love to get this out as 1.13.2 asap, if it works

--- lib/bundler/rubygems_integration.rb
+++ lib/bundler/rubygems_integration.rb
@@ -500,6 +500,7 @@ module Bundler
     end
 
     def redefine_method(klass, method, unbound_method = nil, &block)
+      visibility = method_visibility(klass, method)
       begin
         if (instance_method = klass.instance_method(method)) && method != :initialize
           # doing this to ensure we also get private methods
@@ -512,8 +513,20 @@ module Bundler
       @replaced_methods[[method, klass]] = instance_method
       if unbound_method
         klass.send(:define_method, method, unbound_method)
+        klass.send(visibility, method)
       elsif block
         klass.send(:define_method, method, &block)
+        klass.send(visibility, method)
+      end
+    end
+
+    def method_visibility(klass, method)
+      if klass.private_method_defined?(method)
+        :private
+      elsif klass.protected_method_defined?(method)
+        :protected
+      else
+        :public
       end
     end
 
