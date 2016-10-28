rubygems/bundler

commit cc3558650ed94c110ee1ab50ea75cd556dc188e9

Only report same platform gem updates for `bundle outdated`
- `bundle outdated` will now only show updates for a gem if there are
  available updates for that gem on the same platform
- ex. gem `laduradura` is being used on ruby platform but updates are
  available on gem `laduradura` for java platform = do not display
  potential update versions for java

lib/bundler/cli/outdated.rb
@@ -48,9 +48,9 @@ module Bundler
         dependency = current_dependencies[current_spec.name]
 
         if options["strict"]
-          active_spec = definition.specs.detect {|spec| spec.name == current_spec.name }
+          active_spec = definition.specs.detect {|spec| spec.name == current_spec.name && spec.platform == current_spec.platform }
         else
-          active_specs = definition.index[current_spec.name].sort_by(&:version)
+          active_specs = definition.index[current_spec.name].select {|spec| spec.platform == current_spec.platform }.sort_by(&:version)
           if !current_spec.version.prerelease? && !options[:pre] && active_specs.size > 1
             active_spec = active_specs.delete_if {|b| b.respond_to?(:version) && b.version.prerelease? }
           end

spec/commands/outdated_spec.rb
@@ -243,6 +243,20 @@ describe "bundle outdated" do
     end
   end
 
+  context "update available for a gem on a different platform" do
+    before do
+      install_gemfile <<-G
+        source "file://#{gem_repo2}"
+        gem "laduradura", '= 5.15.2'
+      G
+    end
+
+    it "reports that no updates are available" do
+      bundle "outdated"
+      expect(out).to include("Bundle up to date!")
+    end
+  end
+
   shared_examples_for "version update is detected" do
     it "reports that a gem has a newer version" do
       subject

spec/support/builders.rb
@@ -120,6 +120,16 @@ module Spec
           s.add_dependency "weakling", ">= 0.0.3"
         end
 
+        build_gem "laduradura", "5.15.2"
+        build_gem "laduradura", "5.15.2" do |s|
+          s.platform = "java"
+          s.write "lib/laduradura.rb", "LADURADURA = '5.15.2 JAVA'"
+        end
+        build_gem "laduradura", "5.15.3" do |s|
+          s.platform = "java"
+          s.write "lib/laduradura.rb", "LADURADURA = '5.15.2 JAVA'"
+        end
+
         build_gem "weakling", "0.0.3"
 
         build_gem "terranova", "8"

commit 291a807c7d6f441253f15a2fda29b2a52035c55a

Fix `outdated` listing regression from cc355865

lib/bundler/cli/outdated.rb
@@ -54,9 +54,9 @@ module Bundler
         dependency = current_dependencies[current_spec.name]
 
         if strict
-          active_spec = definition.specs.detect {|spec| spec.name == current_spec.name && spec.platform == current_spec.platform }
+          active_spec = definition.specs.detect {|spec| spec.name == current_spec.name && spec.match_platform(current_spec.platform) }
         else
-          active_specs = definition.index[current_spec.name].select {|spec| spec.platform == current_spec.platform }.sort_by(&:version)
+          active_specs = definition.index[current_spec.name].select {|spec| spec.match_platform(current_spec.platform) }.sort_by(&:version)
           if !current_spec.version.prerelease? && !options[:pre] && active_specs.size > 1
             active_spec = active_specs.delete_if {|b| b.respond_to?(:version) && b.version.prerelease? }
           end