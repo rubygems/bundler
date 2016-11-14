c542198aefcaaa9ef0574c0a4b0b889b5d91b0b2
Colby Swandale <colby@taplaboratories.com>
2016-11-14 21:46:06 +1100

    improve error message for bundle install --path --system conflict

--- lib/bundler/cli/install.rb
+++ lib/bundler/cli/install.rb
@@ -167,8 +167,8 @@ module Bundler
     def check_for_options_conflicts
       if (options[:path] || options[:deployment]) && options[:system]
         error_message = String.new
-        error_message << "You have specified both a path to install your gems to as well as --system. Please choose.\n" if options[:path]
-        error_message << "You have specified both --deployment as well as --system. Please choose.\n" if options[:deployment]
+        error_message << "You have specified both --path as well as --system. Please choose only one option.\n" if options[:path]
+        error_message << "You have specified both --deployment as well as --system. Please choose only one option.\n" if options[:deployment]
         raise InvalidOption.new(error_message)
       end
     end
