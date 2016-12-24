9f16e683d56dc12d50bad2b1811b87678ca5cf36
Samuel Giddins <segiddins@segiddins.me>
2016-12-24 19:20:24 +0100

    [Worker] Fail gracefully when creating threads fails

--- lib/bundler/worker.rb
+++ lib/bundler/worker.rb
@@ -25,11 +25,8 @@ module Bundler
       @request_queue = Queue.new
       @response_queue = Queue.new
       @func = func
-      @threads = Array.new(size) do |i|
-        Thread.start { process_queue(i) }.tap do |thread|
-          thread.name = "#{name} Worker ##{i}" if thread.respond_to?(:name=)
-        end
-      end
+      @size = size
+      @threads = nil
       trap("INT") { abort_threads }
     end
 
@@ -37,6 +34,7 @@ module Bundler
     #
     # @param obj [String] mostly it is name of spec that should be downloaded
     def enq(obj)
+      create_threads unless @threads
       @request_queue.enq obj
     end
 
@@ -70,13 +68,37 @@ module Bundler
     # Stop the worker threads by sending a poison object down the request queue
     # so as worker threads after retrieving it, shut themselves down
     def stop_threads
+      return unless @threads
       @threads.each { @request_queue.enq POISON }
       @threads.each(&:join)
+      @threads = nil
     end
 
     def abort_threads
+      return unless @threads
       @threads.each(&:exit)
       exit 1
+    end
+
+    def create_threads
+      creation_errors = []
+
+      @threads = Array.new(@size) do |i|
+        begin
+          Thread.start { process_queue(i) }.tap do |thread|
+            thread.name = "#{name} Worker ##{i}" if thread.respond_to?(:name=)
+          end
+        rescue ThreadError => e
+          creation_errors << e
+          nil
+        end
+      end.compact
+
+      return if creation_errors.empty?
+
+      message = "Failed to create threads for the #{name} worker: #{creation_errors.map(&:to_s).uniq.join(", ")}"
+      raise ThreadCreationError, message if @threads.empty?
+      Bundler.ui.info message
     end
   end
 end
