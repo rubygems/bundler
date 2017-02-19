5d664465e2602133291ce753349d57310b41188c
Colby Swandale <colby@taplaboratories.com>
2017-02-19 16:04:48 +1100

    check if gem_spec responds to default_gem

--- lib/bundler/cli/info.rb
+++ lib/bundler/cli/info.rb
@@ -25,9 +25,9 @@ module Bundler
     end
 
     def default_gem_spec(gem_name)
-      return nil unless Gem::Specification.respond_to?(:find_all_by_name)
+      return unless Gem::Specification.respond_to?(:find_all_by_name)
       gem_spec = Gem::Specification.find_all_by_name(gem_name).last
-      return gem_spec if gem_spec && gem_spec.default_gem?
+      return gem_spec if gem_spec && gem_spec.respond_to?(:default_gem?) && gem_spec.default_gem?
     end
 
     def spec_not_found(gem_name)
