2ef1e3e6c4991a25f9bf4fc1c3190e7270ccc649
Dennis Suratna <dennis.suratna@gmail.com>
2017-03-07 10:33:32 -0800

    Expand test and refactor to re-install using source

--- lib/bundler/cli/pristine.rb
+++ lib/bundler/cli/pristine.rb
@@ -4,34 +4,29 @@ require "bundler/cli/common"
 module Bundler
   class CLI::Pristine
     def run
-
       ::Bundler.load.specs.each do |spec|
-
         gem_name = "#{spec.name} (#{spec.version}#{spec.git_version})"
 
         if spec.source.instance_of? Source::Path
-          ::Bundler.ui.warn("Cannot pristine #{gem_name} Gem is sourced from path.")
+          ::Bundler.ui.warn("Cannot pristine #{gem_name}. Gem is sourced from local path.")
           next
         end
 
         if spec.source.instance_of? Source::Rubygems
           cached_gem = spec.cache_file
-          unless File.exists?(cached_gem)
+          unless File.exist?(cached_gem)
             # TODO: Refetch from ruby gem?
             ::Bundler.ui.error("Failed to pristine #{gem_name}. Cached gem #{cached_gem} does not exist.")
             next
           end
 
-          Gem::Installer.at(cached_gem,
-                            :wrappers => true,
-                            :force => true,
-                            :install_dir => spec.base_dir,
-                            :build_args => spec.build_args).install
+          spec.source.install(spec, :force => true)
 
         elsif spec.source.instance_of? Source::Git
-          `git -C "#{spec.full_gem_path}" checkout --force`
+          git_source = spec.source
+          git_source.remote!
+          git_source.install(spec, :force => true)
         end
-
       end
     end
   end
--- spec/commands/pristine_spec.rb
+++ spec/commands/pristine_spec.rb
@@ -22,7 +22,6 @@ RSpec.describe "bundle pristine" do
 
   context "when sourced from Rubygems" do
     it "reverts using cached .gem file" do
-
       spec = Bundler.definition.specs["weakling"].first
       changes_txt = "#{spec.full_gem_path}/lib/changes.txt"
 
@@ -31,33 +30,32 @@ RSpec.describe "bundle pristine" do
 
       bundle "pristine"
       expect(File.exist?(changes_txt)).to be_falsey
-
     end
   end
 
   context "when sourced from git repo" do
-    it "reverts by issuing `git checkout --force`" do
-
+    it "reverts by resetting to current revision`" do
       spec = Bundler.definition.specs["foo"].first
       changed_file = "#{spec.full_gem_path}/lib/foo.rb"
+      diff = "#Pristine spec changes"
 
       `echo '#Pristine spec changes' >> #{changed_file}`
-      expect(File.read(changed_file)).to include('#Pristine spec changes')
+      expect(File.read(changed_file)).to include(diff)
 
       bundle "pristine"
-      expect(File.read(changed_file)).to_not include('#Pristine spec changes')
-
+      expect(File.read(changed_file)).to_not include(diff)
     end
   end
 
-  # context "when sourced from path" do
-  #   it "ignores and warns sourced from local path" do
-  #
-  #     spec = Bundler.definition.specs["bar"].first
-  #     expect(Bundler.ui).to receive(:warn).with("Cannot pristine #{spec.name} (#{spec.version}#{spec.git_version}) Gem is sourced from path.")
-  #     bundle "pristine"
-  #
-  #   end
-  # end
-
+  context "when sourced from path" do
+    it "displays warning and ignores changes sourced from local path" do
+      spec = Bundler.definition.specs["bar"].first
+      changes_txt = "#{spec.full_gem_path}/lib/changes.txt"
+      FileUtils.touch(changes_txt)
+      expect(File.exist?(changes_txt)).to be_truthy
+      bundle "pristine"
+      expect(out).to include("Cannot pristine #{spec.name} (#{spec.version}#{spec.git_version}). Gem is sourced from local path.")
+      expect(File.exist?(changes_txt)).to be_truthy
+    end
+  end
 end
