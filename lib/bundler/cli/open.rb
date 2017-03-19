7e748143e410f615f96d40be79b0bf42c8e8d0a9
Colby Swandale <colby@taplaboratories.com>
2017-03-19 22:06:53 +1100

    Bundler::CLI::Common.select_spec will return a default gem spec & bundle open command opens default gems

--- lib/bundler/cli/open.rb
+++ lib/bundler/cli/open.rb
@@ -13,7 +13,7 @@ module Bundler
     def run
       editor = [ENV["BUNDLER_EDITOR"], ENV["VISUAL"], ENV["EDITOR"]].find {|e| !e.nil? && !e.empty? }
       return Bundler.ui.info("To open a bundled gem, set $EDITOR or $BUNDLER_EDITOR") unless editor
-      return unless spec = Bundler::CLI::Common.select_spec(name, :regex_match)
+      return unless spec = Bundler::CLI::Common.select_spec(name, :regex_match, :include_default => true)
       path = spec.full_gem_path
       Dir.chdir(path) do
         command = Shellwords.split(editor) + [path]
