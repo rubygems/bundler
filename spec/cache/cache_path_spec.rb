8fce5958b8191080a1aa911fc0136b4b2e8591d5
Mal Graty <mal.graty@googlemail.com>
2017-05-01 00:55:45 +0100

    Restore and update absolute cache_path test

--- spec/cache/cache_path_spec.rb
+++ spec/cache/cache_path_spec.rb
@@ -23,4 +23,11 @@ RSpec.describe "bundle package" do
       expect(bundled_app("vendor/cache-foo/rack-1.0.0.gem")).to exist
     end
   end
+
+  context "with absolute --cache-path" do
+    it "caches gems at given path" do
+      bundle :package, "cache-path" => "/tmp/cache-foo"
+      expect(bundled_app("/tmp/cache-foo/rack-1.0.0.gem")).to exist
+    end
+  end
 end
