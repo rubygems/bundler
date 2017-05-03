e9be76d50623647c31dfcd0f9c9da558cda76f40
Samuel Giddins <segiddins@segiddins.me>
2017-05-03 12:21:19 -0500

    Warn when making an outdated TLS connection to rubygems.org

--- lib/bundler/vendored_persistent.rb
+++ lib/bundler/vendored_persistent.rb
@@ -16,3 +16,33 @@ module Bundler
   end
 end
 require "bundler/vendor/net-http-persistent/lib/net/http/persistent"
+
+module Bundler
+  class PersistentHTTP < Persistent::Net::HTTP::Persistent
+    def connection_for(uri)
+      connection = super
+      warn_old_tls_version_rubygems_connection(uri, connection)
+      connection
+    end
+
+    def warn_old_tls_version_rubygems_connection(uri, connection)
+      return unless connection.use_ssl?
+      return unless (uri.hostname || "").end_with?("rubygems.org")
+
+      socket = connection.instance_variable_get(:@socket)
+      socket_io = socket.io
+      return unless socket_io.respond_to?(:ssl_version)
+      ssl_version = socket_io.ssl_version
+
+      case ssl_version
+      when /TLSv([\d\.]+)/
+        version = Gem::Version.new($1)
+        if version < Gem::Version.new("1.1")
+          Bundler.ui.warn "Your Ruby version does not support TLSv1.1 or newer" \
+            ", which will be required to connect to https://#{uri.hostname}" \
+            " by January 2018."
+        end
+      end
+    end
+  end
+end
