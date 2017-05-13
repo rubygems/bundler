a52076c06ad77f8dd1b2df7c0763e45ff7369ff0
Samuel Giddins <segiddins@segiddins.me>
2017-05-13 16:45:33 -0500

    Avoid re-building manpages each spec run

--- spec/support/manpages.rb
+++ spec/support/manpages.rb
@@ -4,7 +4,7 @@ module Spec
     def self.setup
       man_path = Spec::Path.root.join("man")
       return if man_path.children(false).select {|file| file.extname == ".ronn" }.all? do |man|
-        man_path.join(man).sub_ext(".txt").file?
+        Dir[man_path.join("#{man.to_s[0..-6]}*.txt").to_s].any?
       end
 
       system(Spec::Path.root.join("bin", "rake").to_s, "man:build") || raise("Failed building man pages")
