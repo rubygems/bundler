e35c4d3caa9af3e84f12fb79523065da35c1246d
Samuel Giddins <segiddins@segiddins.me>
2017-06-12 17:02:57 -0500

    Commit blank generated build metadata file

---
+++ lib/bundler/generated/build_metadata.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Bundler
+  BUILD_METADATA = {
+    :built_at => "1999-01-01".freeze,
+    :git_sha => "000000000".freeze,
+    :release => false,
+  }.freeze
+end
