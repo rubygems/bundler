2f952bbbeb056a17b1b7404e57cbf4c74d812001
Samuel Giddins <segiddins@segiddins.me>
2017-06-12 17:37:37 -0500

    [BUILD_METADATA] Make the defaults "dynamic"

--- lib/bundler/generated/build_metadata.rb
+++ lib/bundler/generated/build_metadata.rb
@@ -2,8 +2,8 @@
 
 module Bundler
   BUILD_METADATA = {
-    :built_at => "1999-01-01".freeze,
-    :git_sha => "000000000".freeze,
+    :built_at => Time.now.strftime("%Y-%m-%d").freeze,
+    :git_sha => Dir.chdir(File.expand_path("..", __FILE__)) { `git rev-parse --short HEAD`.strip }.freeze,
     :release => false,
   }.freeze
 end
