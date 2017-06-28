1f422b9aa9899acf3319ef18c59f2985d8e779b5
Samuel Giddins <segiddins@segiddins.me>
2017-06-28 13:48:35 +0200

    Fix expectations to adapt to the fileutils bundler is using

--- spec/bundler/ssl_certs/certificate_manager_spec.rb
+++ spec/bundler/ssl_certs/certificate_manager_spec.rb
@@ -71,17 +71,17 @@ RSpec.describe Bundler::SSLCerts::CertificateManager do
     context "when certificate manager is not up to date" do
       before do
         allow(subject).to receive(:up_to_date?).and_return(false)
-        allow(Bundler::FileUtils).to receive(:rm)
-        allow(Bundler::FileUtils).to receive(:cp)
+        allow(bundler_fileutils).to receive(:rm)
+        allow(bundler_fileutils).to receive(:cp)
       end
 
       it "should remove the current bundler certs" do
-        expect(Bundler::FileUtils).to receive(:rm).with(subject.bundler_certs)
+        expect(bundler_fileutils).to receive(:rm).with(subject.bundler_certs)
         subject.update!
       end
 
       it "should copy the rubygems certs into bundler certs" do
-        expect(Bundler::FileUtils).to receive(:cp).with(subject.rubygems_certs, subject.bundler_cert_path)
+        expect(bundler_fileutils).to receive(:cp).with(subject.rubygems_certs, subject.bundler_cert_path)
         subject.update!
       end
 
