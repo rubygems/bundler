6760af8ac03fd0569e2eee1b2808232d51036cac
Stefan Sedich <stefan.sedich@gmail.com>
2017-07-10 10:40:05 -0700

    Handle case where user does not have write access and add an explict unlock as it seems Ruby 1.8.7 does not unlock on closing of the file

--- lib/bundler/process_lock.rb
+++ lib/bundler/process_lock.rb
@@ -4,18 +4,21 @@ module Bundler
   class ProcessLock
     def self.lock(bundle_path = Bundler.bundle_path)
       lock_file_path = File.join(bundle_path, "bundler.lock")
+      has_lock = false
 
       File.open(lock_file_path, "w") do |f|
         f.flock(File::LOCK_EX)
-
+        has_lock = true
         yield
+        f.flock(File::LOCK_UN)
       end
-    rescue Errno::ENOLCK # NFS
-      raise if Thread.main != Thread.current
-
+    rescue Errno::EACCES, Errno::ENOLCK
+      # In the case the user does not have access to
+      # create the lock file or is using NFS where
+      # locks are not available we skip locking.
       yield
     ensure
-      File.delete(lock_file_path) if File.exist?(lock_file_path)
+      FileUtils.rm_f(lock_file_path) if has_lock
     end
   end
 end
