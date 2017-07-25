5b930d792be4ad26557bb546239eae8a7cf6bed2
Samuel Giddins <segiddins@segiddins.me>
2017-07-25 11:42:42 -0500

    Update specs for thor handling file clashes

--- spec/commands/newgem_spec.rb
+++ spec/commands/newgem_spec.rb
@@ -888,19 +888,19 @@ Usage: "bundle gem GEM [OPTIONS]"
         FileUtils.touch("conflict-foobar")
       end
       bundle "gem conflict-foobar"
-      expect(last_command.bundler_err).to include("Errno::EEXIST")
+      expect(last_command.bundler_err).to include("Errno::ENOTDIR")
       expect(exitstatus).to eql(32) if exitstatus
     end
   end
 
   context "on conflicts with a previously created directory" do
-    it "should fail gracefully" do
+    it "should succeed" do
       in_app_root do
         FileUtils.mkdir_p("conflict-foobar/Gemfile")
       end
-      bundle "gem conflict-foobar"
-      expect(last_command.bundler_err).to include("Errno::EISDIR")
-      expect(exitstatus).to eql(32) if exitstatus
+      bundle! "gem conflict-foobar"
+      expect(last_command.stdout).to include("file_clash  conflict-foobar/Gemfile").
+        and include "Initializing git repo in #{bundled_app("conflict-foobar")}"
     end
   end
 end
