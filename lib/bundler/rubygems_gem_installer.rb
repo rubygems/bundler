e2a8b1735b53c1f7af3cbda84bc031d4c9b84c57
Samuel Giddins <segiddins@segiddins.me>
2017-08-02 15:32:26 -0500

    Just dont cache extensions on super old RG versions

--- lib/bundler/rubygems_gem_installer.rb
+++ lib/bundler/rubygems_gem_installer.rb
@@ -20,9 +20,9 @@ module Bundler
 
     def build_extensions
       extension_cache_path = options[:bundler_extension_cache_path]
-      return super unless extension_cache_path
+      return super unless extension_cache_path && extension_dir = Bundler.rubygems.spec_extension_dir(spec)
 
-      extension_dir = Pathname.new(spec.extension_dir)
+      extension_dir = Pathname.new(extension_dir)
       build_complete = SharedHelpers.filesystem_access(extension_cache_path.join("gem.build_complete"), :read, &:file?)
       if build_complete && !options[:force]
         SharedHelpers.filesystem_access(extension_dir.parent, &:mkpath)
