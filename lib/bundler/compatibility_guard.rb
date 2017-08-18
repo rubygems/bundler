f0bbb6be4f6e699666b44f00c627b81d3184faa3
Samuel Giddins <segiddins@segiddins.me>
2017-08-17 23:44:45 -0400

    Ensure versions are unfrozen for super old RG

--- lib/bundler/compatibility_guard.rb
+++ lib/bundler/compatibility_guard.rb
@@ -4,7 +4,7 @@ require "rubygems"
 require "bundler/version"
 
 if Bundler::VERSION.split(".").first.to_i >= 2
-  if Gem::Version.new(Object::RUBY_VERSION) < Gem::Version.new("2.3")
+  if Gem::Version.new(Object::RUBY_VERSION.dup) < Gem::Version.new("2.3")
     abort "Bundler 2 requires Ruby 2.3 or later. Either install bundler 1 or update to a supported Ruby version."
   end
 
