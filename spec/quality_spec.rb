c7a183c3b0397a16cc21661ffe924874f9b8bf0e
Samuel Giddins <segiddins@segiddins.me>
2017-08-30 20:39:06 -0500

    Auto-configure job count in 2.0
    Also, stop subtracting 1 from the configured job count in 2.0

--- spec/quality_spec.rb
+++ spec/quality_spec.rb
@@ -169,6 +169,7 @@ RSpec.describe "The library itself" do
 
   it "documents all used settings" do
     exemptions = %w[
+      auto_config_jobs
       cache_command_is_package
       console_command
       default_cli_command
