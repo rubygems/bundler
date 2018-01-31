be337de36acf0d97a795b5f2bf5d7c14a910f968
David Rodríguez <deivid.rodriguez@riseup.net>
2018-01-31 08:47:35 -0300

    1.8.7 compatible file read

--- lib/bundler.rb
+++ lib/bundler.rb
@@ -441,7 +441,7 @@ EOF
 
     def load_gemspec_uncached(file, validate = false)
       path = Pathname.new(file)
-      contents = path.binread
+      contents = read_file(file)
       spec = if contents.start_with?("---") # YAML header
         eval_yaml_gemspec(path, contents)
       else
