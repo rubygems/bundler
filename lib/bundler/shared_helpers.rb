94f664fc4c55441736aa173b57b1bad6d7ef761d
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2018-10-16 11:34:40 +0900

    Support file structure of ruby core repository.

--- lib/bundler/shared_helpers.rb
+++ lib/bundler/shared_helpers.rb
@@ -274,8 +274,14 @@ module Bundler
 
       until !File.directory?(current) || current == previous
         if ENV["BUNDLE_SPEC_RUN"]
+          if File.file?(File.join(current, "bundler.gemspec"))
+            gemspec = "bundler.gemspec"
+          else
+            # for Ruby Core
+            gemspec = "lib/bundler.gemspec"
+          end
           # avoid stepping above the tmp directory when testing
-          return nil if File.file?(File.join(current, "bundler.gemspec"))
+          return nil if File.file?(File.join(current, gemspec))
         end
 
         names.each do |name|
@@ -304,10 +310,12 @@ module Bundler
         unless File.exist?(exe_file)
           exe_file = File.expand_path("../../../exe/bundle", __FILE__)
         end
-
-        Bundler::SharedHelpers.set_env "BUNDLE_BIN_PATH", exe_file
       rescue Gem::GemNotFoundException
-        Bundler::SharedHelpers.set_env "BUNDLE_BIN_PATH", File.expand_path("../../../exe/bundle", __FILE__)
+        exe_file = File.expand_path("../../../exe/bundle", __FILE__)
+        # for Ruby core repository
+        exe_file = File.expand_path("../../../../bin/bundle", __FILE__) unless File.exist?(exe_file)
+      ensure
+        Bundler::SharedHelpers.set_env "BUNDLE_BIN_PATH", exe_file
       end
 
       # Set BUNDLE_GEMFILE
