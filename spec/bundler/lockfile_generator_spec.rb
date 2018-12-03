6eb2b9385fc8dd88c14702891951733ebb5bb7be
Gavriel Loria <gtloria@protonmail.com>
2018-12-03 01:25:27 -0500

    now includes tests

--- spec/bundler/lockfile_generator_spec.rb
+++ spec/bundler/lockfile_generator_spec.rb
@@ -2,12 +2,13 @@
 
 RSpec.describe "Bundler::LockfileGenerator" do
   context "Gemfile contains private credentials in source", :bundler => ">= 2" do
+    
     it "filters out private token for lockfile" do
       gemfile <<-G
 source "https://my-secret-token:x-oauth-basic@github.com/foo/bar.git"
       G
       bundle "install"
-      expect("gems.locked").not_to have_file_content "my-secret-token"
+      expect(lockfile).not_to match "my-secret-token"
     end
 
     it "filters out private password for lockfile" do
@@ -15,7 +16,7 @@ source "https://my-secret-token:x-oauth-basic@github.com/foo/bar.git"
 source "https://username:my-secret-password@github.com/foo/bar.git"
       G
       bundle "install"
-      expect("gems.locked").not_to have_file_content "my-secret-password"
+      expect(lockfile).not_to match "my-secret-password"
     end
   end
 end
