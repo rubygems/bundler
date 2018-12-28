21ac802b8c54718687a8682c396fc31e5a215ebc
Colby Swandale <me@colby.fyi>
2018-12-28 21:44:25 +1100

    set `bundle` spec helper to load the system bundler binstub directly instead of depending on the $PATH

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -104,7 +104,7 @@ module Spec
       bundle_bin = options.delete("bundle_bin") || bindir.join("bundle")
 
       if system_bundler = options.delete(:system_bundler)
-        bundle_bin = "-S bundle"
+        bundle_bin = system_gem_path.join("bin/bundler")
       end
 
       env = options.delete(:env) || {}
