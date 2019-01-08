17a3690706c8230fc72a67f0c0935d9afa4c377b
ankitkataria <ankitkataria28@gmail.com>
2019-01-09 00:03:31 +0530

    add dependents to info command

--- lib/bundler/cli/info.rb
+++ lib/bundler/cli/info.rb
@@ -14,6 +14,7 @@ module Bundler
       spec_not_found(gem_name) unless spec
       return print_gem_path(spec) if @options[:path]
       print_gem_info(spec)
+      print_gem_dependencies(gem_name)
     end
 
   private
@@ -45,6 +46,65 @@ module Bundler
       gem_info << "\tPath: #{spec.full_gem_path}\n"
       gem_info << "\tDefault Gem: yes" if spec.respond_to?(:default_gem?) && spec.default_gem?
       Bundler.ui.info gem_info
+    end
+
+    def print_gem_dependencies(spec)
+      top_level_dependencies = summarize(spec)
+
+      stat_info = String.new
+      stat_info << "\tDependents:\n"
+      if top_level_dependencies.count > 0
+        top_level_dependencies.each do |stat_line|
+          stat_info << format("\t\t%s (%s)\n", stat_line[:name], stat_line[:version])
+        end
+      else
+        stat_info << "\t\tNone"
+      end
+
+      Bundler.ui.info stat_info
+    end
+
+    def summarize(spec)
+      definition = Bundler.definition
+      lockfile_contents = definition.to_lock
+      parser = Bundler::LockfileParser.new(lockfile_contents)
+      @tree = specs_as_tree(parser.specs)
+
+      reverse_dependencies_with_versions(spec)
+    end
+
+    def specs_as_tree(specs)
+      specs.each_with_object({}) do |spec, hash|
+        hash[spec.name] = spec
+      end
+    end
+
+    def transitive_dependencies(target)
+      raise ArgumentError, "Unknown gem #{target}" unless @tree.key? target
+
+      top_level = @tree[target].dependencies
+      all_level = top_level + top_level.inject([]) do |arr, dep|
+        next arr if dep.name == "bundler"
+
+        arr + transitive_dependencies(dep.name)
+      end
+
+      all_level.uniq(&:name)
+    end
+
+    def reverse_dependencies_with_versions(target)
+      @tree.map do |name, dep|
+        transitive_dependencies(name).map do |transitive_dependency|
+          next unless transitive_dependency.name == target
+          {
+            :name => dep.name,
+            :version => transitive_dependency.requirement.to_s,
+            :requirement => transitive_dependency.requirement,
+          }
+        end
+      end.flatten.compact.sort do |a, b|
+        a[:requirement].as_list <=> b[:requirement].as_list
+      end
     end
   end
 end
