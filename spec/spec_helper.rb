842303ebd184d58c9f6d1c0df9bf77e3c9a0810e
Jan Piotrowski <piotrowski+git@gmail.com>
2019-01-09 21:40:30 +0100

    do not generate manpages on Windows

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,7 +41,7 @@ end
 
 $debug = false
 
-Spec::Manpages.setup
+Spec::Manpages.setup unless Gem.win_platform?
 Spec::Rubygems.setup
 FileUtils.rm_rf(Spec::Path.gem_repo1)
 ENV["RUBYOPT"] = "#{ENV["RUBYOPT"]} -r#{Spec::Path.spec_dir}/support/hax.rb"
