127f798df641a70130457a9b9d9e642baf2d0920
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2019-03-18 09:14:37 +0900

    Use Gem::Request.get_cert_files instead of hard-coded paths.

--- lib/bundler/fetcher.rb
+++ lib/bundler/fetcher.rb
@@ -4,6 +4,7 @@ require "bundler/vendored_persistent"
 require "cgi"
 require "securerandom"
 require "zlib"
+require "rubygems/request"
 
 module Bundler
   # Handles all the fetching with the rubygems server
@@ -295,8 +296,7 @@ module Bundler
         end
       else
         store.set_default_paths
-        certs = File.join(Gem::RUBYGEMS_DIR, "rubygems", "ssl_certs", "*", "*.pem")
-        Dir.glob(certs).each {|c| store.add_file c }
+        Gem::Request.get_cert_files.each {|c| store.add_file c }
       end
       store
     end
