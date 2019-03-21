0bc4970769f178e80a0ad149c433e25262e47bf5
David Rodríguez <deivid.rodriguez@riseup.net>
2019-03-21 10:50:00 +0100

    Always pass the `--all` flag when needed

--- spec/install/gems/dependency_api_spec.rb
+++ spec/install/gems/dependency_api_spec.rb
@@ -237,7 +237,7 @@ RSpec.describe "gemcutter's dependency API" do
         gem "rack"
       G
 
-      bundle! "update --full-index", :artifice => "endpoint", :all => bundle_update_requires_all?
+      bundle! "update --full-index", :artifice => "endpoint", :all => true
       expect(out).to include("Fetching source index from #{source_uri}")
       expect(the_bundle).to include_gems "rack 1.0.0"
     end
