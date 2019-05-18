08cea1ecb47ece5174762d00769e2445fadadb36
David Rodríguez <deivid.rodriguez@riseup.net>
2019-05-18 09:40:24 -0400

    Revert "Remove unused filter"
    This reverts commit ac282dc9de5e20db85e9cdf23b97763a2a2cc292.
    
    Because the filter is actually used once:
    
    https://github.com/bundler/bundler/blob/3ec8165eec0a1af8c45ee258d3e7cb61fba875a2/spec/update/git_spec.rb#L162
    
    My bad.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -69,8 +69,11 @@ RSpec.configure do |config|
     config.filter_run_excluding :realworld => true
   end
 
+  git_version = Bundler::Source::Git::GitProxy.new(nil, nil, nil).version
+
   config.filter_run_excluding :ruby => RequirementChecker.against(RUBY_VERSION)
   config.filter_run_excluding :rubygems => RequirementChecker.against(Gem::VERSION)
+  config.filter_run_excluding :git => RequirementChecker.against(git_version)
   config.filter_run_excluding :rubygems_master => (ENV["RGV"] != "master")
   config.filter_run_excluding :bundler => RequirementChecker.against(Bundler::VERSION.split(".")[0])
   config.filter_run_excluding :ruby_repo => !(ENV["BUNDLE_RUBY"] && ENV["BUNDLE_GEM"]).nil?
