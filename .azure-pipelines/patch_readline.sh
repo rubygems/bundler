7809393a6d18781ac96f6d009a677af70fa59450
David Rodríguez <deivid.rodriguez@riseup.net>
2019-05-29 18:05:10 +0200

    Quote some strings
    To emphasize that I want `\r` as a literal, but `$ruby_version` evaluated.

--- .azure-pipelines/patch_readline.sh
+++ .azure-pipelines/patch_readline.sh
@@ -1,3 +1,3 @@
 ruby_version=$(ruby -e 'puts RUBY_VERSION')
 
-git apply --ignore-space-change --ignore-whitespace .azure-pipelines\rbreadline.diff --directory=C:/hostedtoolcache/windows/Ruby/$ruby_version/x64/lib/ruby/site_ruby --unsafe-paths
+git apply --ignore-space-change --ignore-whitespace '.azure-pipelines\rbreadline.diff' --directory="C:/hostedtoolcache/windows/Ruby/$ruby_version/x64/lib/ruby/site_ruby" --unsafe-paths
