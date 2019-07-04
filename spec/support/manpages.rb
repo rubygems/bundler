4c90d690fb9d0dec63042c316d763414049ec67c
David Rodríguez <deivid.rodriguez@riseup.net>
2019-07-04 16:21:09 +0200

    Tests don't need to build man pages

--- spec/support/manpages.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Spec
-  module Manpages
-    def self.setup
-      system(Spec::Path.root.join("bin", "rake").to_s, "man:build") || raise("Failed building man pages")
-    end
-  end
-end
