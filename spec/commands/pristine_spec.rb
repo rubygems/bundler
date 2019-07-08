ccb5485c263725252af23a4819e416f6e2e8b6fb
David Rodríguez <deivid.rodriguez@riseup.net>
2019-07-08 17:09:26 +0200

    Remove stuff that I think it's not needed

--- spec/commands/pristine_spec.rb
+++ spec/commands/pristine_spec.rb
@@ -43,7 +43,6 @@ RSpec.describe "bundle pristine", :ruby_repo do
     end
 
     it "does not delete the bundler gem", :rubygems => ">= 2.6.2" do
-      ENV["BUNDLER_SPEC_KEEP_DEFAULT_BUNDLER_GEM"] = "true"
       system_gems :bundler
       bundle! "install"
       bundle! "pristine", :system_bundler => true
