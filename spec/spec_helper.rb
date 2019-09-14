86ebf932aec7efaf6c86c59724e12f1256ab7c5b
David Rodríguez <deivid.rodriguez@riseup.net>
2019-09-14 19:18:31 +0200

    Rename requirement_checker to filters
    And move filter configuration there.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -51,28 +51,6 @@ RSpec.configure do |config|
 
   config.bisect_runner = :shell
 
-  if ENV["BUNDLER_SUDO_TESTS"] && Spec::Sudo.present?
-    config.filter_run :sudo => true
-  else
-    config.filter_run_excluding :sudo => true
-  end
-
-  if ENV["BUNDLER_REALWORLD_TESTS"]
-    config.filter_run :realworld => true
-  else
-    config.filter_run_excluding :realworld => true
-  end
-
-  git_version = Bundler::Source::Git::GitProxy.new(nil, nil, nil).version
-
-  config.filter_run_excluding :rubygems => RequirementChecker.against(Gem::VERSION)
-  config.filter_run_excluding :git => RequirementChecker.against(git_version)
-  config.filter_run_excluding :bundler => RequirementChecker.against(Bundler::VERSION.split(".")[0])
-  config.filter_run_excluding :ruby_repo => !ENV["GEM_COMMAND"].nil?
-  config.filter_run_excluding :no_color_tty => Gem.win_platform? || !ENV["GITHUB_ACTION"].nil?
-
-  config.filter_run_when_matching :focus unless ENV["CI"]
-
   original_wd  = Dir.pwd
   original_env = ENV.to_hash
 
