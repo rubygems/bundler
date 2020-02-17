8f2b8fa010e0bf21699c2dbf9afb4ecbe5569f2e
David Rodríguez <deivid.rodriguez@riseup.net>
2020-02-17 11:45:23 +0100

    Revert "I don't think this is needed?"
    This reverts commit 4ef1fe77cecf730a58f1451b43c31ff037cb85cb.

--- lib/bundler/settings.rb
+++ lib/bundler/settings.rb
@@ -108,6 +108,8 @@ module Bundler
     end
 
     def set_local(key, value)
+      local_config_file || raise(GemfileNotFound, "Could not locate Gemfile")
+
       set_key(key, value, @local_config, local_config_file)
     end
 
@@ -384,7 +386,7 @@ module Bundler
     end
 
     def local_config_file
-      @root.join("config")
+      @root.join("config") if @root
     end
 
     def load_config(config_file)
